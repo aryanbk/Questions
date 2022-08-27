7be02184b368c34ac3fe47e592300b93d06f2e33
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-08-27 20:34:34 +0530

    Time: 799 ms (30.89%), Space: 117.7 MB (26.79%) - LeetHub

---
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return 0;
+        int rows = matrix.length, cols = matrix[0].length;
+        int[][] areas = new int[rows][cols];
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                int area = matrix[r][c];
+                if (r-1 >= 0)
+                    area += areas[r-1][c];
+                if (c-1 >= 0)
+                    area += areas[r][c-1];
+                if (r-1 >= 0 && c-1 >= 0)
+                    area -= areas[r-1][c-1];
+                areas[r][c] = area;
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for (int r1 = 0; r1 < rows; r1++) {
+            for (int r2 = r1; r2 < rows; r2++) {
+                TreeSet<Integer> tree = new TreeSet<>();
+                tree.add(0);    // padding
+                for (int c = 0; c < cols; c++) {
+                    int area = areas[r2][c];
+                    if (r1-1 >= 0)
+                        area -= areas[r1-1][c];
+                    Integer ceiling = tree.ceiling(area - k);
+                    if (ceiling != null)
+                        max = Math.max(max, area - ceiling);
+                    tree.add(area);
+                }
+            }
+        }
+        return max;
+    }
+}
