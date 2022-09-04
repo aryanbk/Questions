d48a2b894c13235a53245c5c202c7e9ba859a4b6
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-09-04 13:44:03 +0530

    Time: 447 ms (90.27%), Space: 19.4 MB (89.66%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.py
+++ 901-online-stock-span/901-online-stock-span.py
@@ -1,6 +1,6 @@
 class StockSpanner:
     def __init__(self):
-        self.stk=[]
+        self.stk=deque()
 
     def next(self, price: int) -> int:
         ans=1
@@ -8,6 +8,16 @@ class StockSpanner:
             ans+=self.stk.pop()[1]
         self.stk.append([price, ans])
         return ans
+    
+#     def __init__(self):
+#         self.stk=[]
+
+#     def next(self, price: int) -> int:
+#         ans=1
+#         while len(self.stk)>0 and self.stk[len(self.stk)-1][0]<=price:
+#             ans+=self.stk.pop()[1]
+#         self.stk.append([price, ans])
+#         return ans
 
 
 # Your StockSpanner object will be instantiated and called as such:
