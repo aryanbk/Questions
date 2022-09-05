61da998b5c5da0e0f0f12e6f97016702b7e73004
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-09-05 12:06:04 +0530

    Time: 1071 ms (6.82%), Space: 15.2 MB (89.73%) - LeetHub

---
+++ 85-maximal-rectangle/85-maximal-rectangle.py
@@ -0,0 +1,47 @@
+class Solution:
+    def maximalRectangle(self, mat: List[List[str]]) -> int:
+        m,n = len(mat), len(mat[0])
+        
+        # maximum area of histerogram
+        def mah(arr):
+            # nearest smallest to the left
+            def nsl():
+                stk=[]
+                res=[-1]*n
+                for i in range(n):
+                    while len(stk)>0 and arr[stk[-1]]>=arr[i]:
+                        stk.pop()
+                    if len(stk)!=0:
+                        res[i]=stk[-1]
+                    stk.append(i)
+                return res
+            
+            # nearest smallest to the left
+            def nsr():
+                stk=[]
+                res=[n]*n
+                for i in range(n-1, -1, -1):
+                    while len(stk)>0 and arr[stk[-1]]>=arr[i]:
+                        stk.pop()
+                    if len(stk)!=0:
+                        res[i]=stk[-1]
+                    stk.append(i)
+                return res
+            
+            left=nsl()
+            right=nsr()
+            mx=0
+            
+            for i in range(n):
+                mx=max(mx, (right[i]-left[i]-1)*arr[i])
+            return mx
+        
+        
+        arr=[0]*n
+        ans=0
+        for i in range(m):
+            for j in range(n):
+                ele = int(mat[i][j])
+                arr[j] = 0 if ele==0 else arr[j]+ele
+            ans=max(ans, mah(arr))
+        return ans
