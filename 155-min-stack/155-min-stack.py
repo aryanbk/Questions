a012bcc22f0016227e727553eae4fcdbefc0dc56
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-09-05 16:06:46 +0530

    Time: 88 ms (62.86%), Space: 17.8 MB (96.03%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -2,24 +2,50 @@ class MinStack:
 
     def __init__(self):
         self.stk=[]
-        self.stk2=[]
+        self.mini=0
 
     def push(self, val: int) -> None:
-        self.stk.append(val)
-        if len(self.stk2)==0 or val<=self.stk2[-1]:
-            self.stk2.append(val)
+        if len(self.stk)==0:
+            self.stk.append(val)
+            self.mini=val
+            return
+        if val>=self.mini:
+            self.stk.append(val)
+            return
+        self.stk.append(2*val - self.mini)
+        self.mini=val
+        return
 
     def pop(self) -> None:
-        if len(self.stk)>0 and self.stk.pop()==self.stk2[-1]:
-            self.stk2.pop()
+        if self.stk[-1]<self.mini:
+            self.mini=2*self.mini - self.stk.pop()
+            return
+        self.stk.pop()
 
     def top(self) -> int:
+        if self.stk[-1]<self.mini:
+            return self.mini
         return self.stk[-1]
         
     def getMin(self) -> int:
-        return self.stk2[-1]
+        return self.mini
 
     
+    
+#     def __init__(self):
+#         self.stk=[]
+#         self.stk2=[]
+#     def push(self, val: int) -> None:
+#         self.stk.append(val)
+#         if len(self.stk2)==0 or val<=self.stk2[-1]:
+#             self.stk2.append(val)
+#     def pop(self) -> None:
+#         if len(self.stk)>0 and self.stk.pop()==self.stk2[-1]:
+#             self.stk2.pop()
+#     def top(self) -> int:
+#         return self.stk[-1]
+#     def getMin(self) -> int:
+#         return self.stk2[-1]
     
     
 #     def __init__(self):
