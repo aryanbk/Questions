043c7435cc5e098737077a322a0827b74c0b980f
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-09-06 23:22:38 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (77.01%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.java
+++ 814-binary-tree-pruning/814-binary-tree-pruning.java
@@ -15,14 +15,23 @@
  */
 class Solution {
     public TreeNode pruneTree(TreeNode root) {
-        return dfs(root) ? root : null;
-    }
-    boolean dfs(TreeNode root){
-        if(root==null) return false;
-        boolean l = dfs(root.left);
-        boolean r = dfs(root.right);
-        if(!l) root.left=null;
-        if(!r) root.right=null;
-        return l || r || (root.val==1)?true:false;
+        if(root==null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        return (root.val==1 || root.left!=null || root.right!=null) ? root : null ;
     }
 }
+
+// class Solution {
+//     public TreeNode pruneTree(TreeNode root) {
+//         return dfs(root) ? root : null;
+//     }
+//     boolean dfs(TreeNode root){
+//         if(root==null) return false;
+//         boolean l = dfs(root.left);
+//         boolean r = dfs(root.right);
+//         if(!l) root.left=null;
+//         if(!r) root.right=null;
+//         return l || r || (root.val==1)?true:false;
+//     }
+// }
