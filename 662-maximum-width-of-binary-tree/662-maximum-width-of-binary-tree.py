f436517e3e4250ef6ee8b2a6470e3f6441c7b58e
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-09-14 21:19:23 +0530

    Time: 97 ms (14.10%), Space: 14.8 MB (75.58%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
@@ -11,7 +11,7 @@ class Solution:
             l, mini, maxi= len(q), q[0][1], -1,
             for i in range(l):
                 node, num = q.pop(0)
-                maxi = max(maxi, num)
+                maxi = num
                 if node.left: q.append([node.left, 2*num - 1])
                 if node.right: q.append([node.right, 2*num])
             ans=max(ans, maxi-mini+1)
