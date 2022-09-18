75b6e610643997f9ec23c2b8af6c93468fd847df
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-09-18 22:04:39 +0530

    Time: 3814 ms (9.09%), Space: 20.6 MB (100.00%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
@@ -5,22 +5,37 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def reverseOddLevels(self, root: Optional[TreeNode], l=0) -> Optional[TreeNode]:
-        q, odd = [root], False
-        while q:
-            lq, sub = len(q), []
-            for _ in range(lq):
-                node=q.pop(0)
-                if odd: sub.append(node)
-                if node.left: q.append(node.left)
-                if node.right: q.append(node.right)
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def swap(r1, r2, odd):
+            if not r1: return
             if odd:
-                i, j = 0, len(sub)-1
-                while i<j:
-                    temp=sub[i].val
-                    sub[i].val = sub[j].val
-                    sub[j].val=temp
-                    i+=1
-                    j-=1
-            odd = not odd
+                temp=r1.val
+                r1.val=r2.val
+                r2.val=temp
+            swap(r1.left, r2.right, not odd)
+            swap(r1.right, r2.left, not odd)
+        swap(root.left, root.right, True)
         return root
+                
+
+# class Solution:
+#     def reverseOddLevels(self, root: Optional[TreeNode], l=0) -> Optional[TreeNode]:
+#         q, odd = [root], False
+#         while q:
+#             lq, sub = len(q), []
+#             for _ in range(lq):
+#                 node=q.pop(0)
+#                 if odd: sub.append(node)
+#                 if node.left: q.append(node.left)
+#                 if node.right: q.append(node.right)
+#             if odd:
+#                 i, j = 0, len(sub)-1
+#                 while i<j:
+#                     temp=sub[i].val
+#                     sub[i].val = sub[j].val
+#                     sub[j].val=temp
+#                     i+=1
+#                     j-=1
+#             odd = not odd
+#         return root
+        
