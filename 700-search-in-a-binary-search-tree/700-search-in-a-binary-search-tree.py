6955b6cfc20c7d7755b4ba5dd1701456c9096856
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-09-19 14:19:04 +0530

    Time: 79 ms (94.86%), Space: 16.5 MB (24.51%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.py
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.py
@@ -6,8 +6,18 @@
 #         self.right = right
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        if not root: return None
-        if root.val==val: return root
-        elif root.val>val: return self.searchBST(root.left, val)
-        else: return self.searchBST(root.right, val)
+        while root:
+            if not root: return None
+            elif root.val==val: return root
+            elif root.val>val: root=root.left
+            else: root=root.right 
+
+
+
+# class Solution:
+#     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+#         if not root: return None
+#         if root.val==val: return root
+#         elif root.val>val: return self.searchBST(root.left, val)
+#         else: return self.searchBST(root.right, val)
         
