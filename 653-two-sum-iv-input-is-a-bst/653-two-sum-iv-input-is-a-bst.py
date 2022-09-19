8b1b17cfa61541775b9d2529e1c3412cc3360f77
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-09-19 21:16:17 +0530

    Time: 206 ms (14.63%), Space: 16.4 MB (74.63%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -6,16 +6,46 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        ans=None
-        def inorder(root):
-            nonlocal ans
-            if not root or ans: return
-            inorder(root.left)
-            if k-root.val in ino:
-                ans=True
-                return
-            ino.add(root.val)
-            inorder(root.right)
-        ino=set()
-        inorder(root)
-        return ans
+        
+        def fillstk(stk, root, left):
+            while root:
+                stk.append(root)
+                if left: root=root.left
+                else: root=root.right
+                    
+        ##########
+        stkl, stkr = [], []
+        fillstk(stkl, root, True)
+        fillstk(stkr, root, False)
+        
+        while stkl and stkr and (stkl[-1]!=stkr[-1]):
+            sums=stkl[-1].val + stkr[-1].val
+            if sums==k: return True
+            elif sums<k:
+                node=stkl.pop()
+                fillstk(stkl, node.right, True)
+            elif sums>k:
+                node=stkr.pop()
+                fillstk(stkr, node.left, False)
+        return False
+
+
+
+
+
+# class Solution:
+#     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+#         ans=None
+#         def inorder(root):
+#             nonlocal ans
+#             if not root or ans: return
+#             inorder(root.left)
+#             if k-root.val in ino:
+#                 ans=True
+#                 return
+#             ino.add(root.val)
+#             inorder(root.right)
+#         ino=set()
+#         inorder(root)
+#         return ans
+        
