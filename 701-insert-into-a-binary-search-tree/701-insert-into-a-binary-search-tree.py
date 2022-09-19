8bc49cb65802f98015bcf9489d35d36872aea9cc
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-09-19 14:56:45 +0530

    Time: 323 ms (8.34%), Space: 16.9 MB (91.01%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.py
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.py
@@ -4,20 +4,29 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
-    def insertIntoBST(self, root: Optional[TreeNode], val: int , prev=None, left=True) -> Optional[TreeNode]:
-        if not root:
-            if not prev: return TreeNode(val)
-            if left:
-                prev.left=TreeNode(val)
-                return prev.left
-            else:
-                prev.right = TreeNode(val)
-                return prev.right
-        if root.val>val:
-            root.left = self.insertIntoBST(root.left, val , root, True)
-            return root
-        elif root.val < val:
-            root.right = self.insertIntoBST(root.right, val , root, False)
-            return root
+    def insertIntoBST(self, root: Optional[TreeNode], val: int , left=True) -> Optional[TreeNode]:
+        if not root: root = TreeNode(val)
+        elif root.val>val: root.left = self.insertIntoBST(root.left, val)
+        elif root.val < val: root.right = self.insertIntoBST(root.right, val)
+        return root
+
+
+# class Solution:
+#     def insertIntoBST(self, root: Optional[TreeNode], val: int , prev=None, left=True) -> Optional[TreeNode]:
+#         if not root:
+#             if not prev: return TreeNode(val)
+#             if left:
+#                 prev.left=TreeNode(val)
+#                 return prev.left
+#             else:
+#                 prev.right = TreeNode(val)
+#                 return prev.right
+#         if root.val>val:
+#             root.left = self.insertIntoBST(root.left, val , root, True)
+#             return root
+#         elif root.val < val:
+#             root.right = self.insertIntoBST(root.right, val , root, False)
+#             return root
         
