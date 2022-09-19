54d1607068cafcb193275d442ad880dc89e3038e
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-09-19 12:37:07 +0530

    Time: 58 ms (31.10%), Space: 14 MB (13.00%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -4,22 +4,48 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Morris Traversal
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        stk,ans=[],[]
-        while True:
-            if root:
-                stk.append(root)
-                root=root.left
+        ans, ptr = [], root
+        while ptr:
+            if not ptr.left:
+                ans.append(ptr.val)
+                ptr=ptr.right
             else:
-                if len(stk) is 0:
-                    return ans
-                root=stk.pop()
-                ans.append(root.val)
-                root=root.right
+                prev=ptr.left
+                while prev.right and prev.right!=ptr:
+                    prev=prev.right
+                if not prev.right:
+                    prev.right=ptr
+                    ptr=ptr.left
+                else:
+                    prev.right=None
+                    ans.append(ptr.val)
+                    ptr=ptr.right
+        return ans
+
+    
+
+# Iterative
+# class Solution:
+#     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         stk,ans=[],[]
+#         while True:
+#             if root:
+#                 stk.append(root)
+#                 root=root.left
+#             else:
+#                 if len(stk) is 0:
+#                     return ans
+#                 root=stk.pop()
+#                 ans.append(root.val)
+#                 root=root.right
         
         
         
+# Recursive
 # class Solution:
 #     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
 #         if not root:
@@ -28,3 +54,4 @@ class Solution:
 #         res.append(root.val)
 #         res.extend(self.inorderTraversal(root.right))
 #         return res
+        
