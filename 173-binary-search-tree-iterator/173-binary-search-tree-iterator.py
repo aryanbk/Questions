b916e1e71c4c9b84cb983e089740cc9e8e5d16c7
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-09-19 20:26:03 +0530

    Time: 176 ms (8.50%), Space: 20.4 MB (34.16%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.py
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.py
@@ -7,25 +7,45 @@
 class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
-        def dfs(root):
-            if not root: return
-            dfs(root.left)
-            self.ino.append(root.val)
-            dfs(root.right)
-        
-        self.ino=[]
-        self.ptr=0
-        dfs(root)
-        
+        self.stk=[]
+        self._fillstk(root)
 
     def next(self) -> int:
-        ans = self.ino[self.ptr]
-        self.ptr+=1
-        return ans
+        node=self.stk.pop()
+        self._fillstk(node.right)
+        return node.val
 
     def hasNext(self) -> bool:
-        return self.ptr<len(self.ino)
+        return len(self.stk)>0
+        
+    def _fillstk(self, root):
+        while root:
+            self.stk.append(root)
+            root=root.left
 
+############################################################################################
+# class BSTIterator:
+# 
+#     def __init__(self, root: Optional[TreeNode]):
+#         def dfs(root):
+#             if not root: return
+#             dfs(root.left)
+#             self.ino.append(root.val)
+#             dfs(root.right)
+        
+#         self.ino=[]
+#         self.ptr=0
+#         dfs(root)
+        
+
+#     def next(self) -> int:
+#         ans = self.ino[self.ptr]
+#         self.ptr+=1
+#         return ans
+
+#     def hasNext(self) -> bool:
+#         return self.ptr<len(self.ino)
+############################################################################################
 
 # Your BSTIterator object will be instantiated and called as such:
 # obj = BSTIterator(root)
