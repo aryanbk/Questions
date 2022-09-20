5346e6bb0c3a7100e38b8b7d1d156fb89d5fa291
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-09-20 22:38:40 +0530

    Time: 64 ms (78.11%), Space: 43.9 MB (97.69%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,12 +1,25 @@
+// class Solution {
+//     public int findLength(int[] a, int[] b) {
+//         int m = a.length, n = b.length;
+//         if (m == 0 || n == 0) return 0;
+//         int[][] dp = new int[m + 1][n + 1];
+//         int max = 0;
+//         for (int i = m - 1; i >= 0; i--)
+//             for (int j = n - 1; j >= 0; j--)
+//                 max = Math.max(max, dp[i][j] = a[i] == b[j] ? 1 + dp[i + 1][j + 1] : 0);
+//         return max;        
+//     }
+// }
+
 class Solution {
     public int findLength(int[] a, int[] b) {
         int m = a.length, n = b.length;
         if (m == 0 || n == 0) return 0;
-        int[][] dp = new int[m + 1][n + 1];
+        int[] dp = new int[n + 1];
         int max = 0;
         for (int i = m - 1; i >= 0; i--)
-            for (int j = n - 1; j >= 0; j--)
-                max = Math.max(max, dp[i][j] = a[i] == b[j] ? 1 + dp[i + 1][j + 1] : 0);
+            for (int j = 0; j < n; j++)
+                max = Math.max(max, dp[j] = a[i] == b[j] ? 1 + dp[j + 1] : 0);
         return max;        
     }
 }
