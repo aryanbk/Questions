b28a54774548e894b2f80b2386320a1210549973
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-09-30 11:11:33 +0530

    Time: 2363 ms (33.69%), Space: 18.1 MB (71.66%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -7,18 +7,15 @@ class Solution:
         while i<l:
             lo, hi = i+1, l-1
             while lo<hi:
-                if nums[lo]+nums[hi]==-1*nums[i]:
+                s=nums[i]+nums[lo]+nums[hi]
+                if s==0:
                     ans.append([nums[i], nums[lo], nums[hi]])
                     p=lo
                     while lo<l and nums[lo]==nums[p]: lo+=1
                     p=hi
                     while hi>=0 and nums[hi]==nums[p]: hi-=1
-                elif nums[lo]+nums[hi]>-1*nums[i]:
-                    p=hi
-                    while hi>=0 and nums[hi]==nums[p]: hi-=1
-                elif nums[lo]+nums[hi]<-1*nums[i]:
-                    p=lo
-                    while lo<l and nums[lo]==nums[p]: lo+=1
+                elif s>0: hi-=1
+                else: lo+=1
             p=i
             while i<l and nums[p]==nums[i]: i+=1
         return ans
