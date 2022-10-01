68809328b758abcb4bdc66f7da30471b96df2d00
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-01 18:13:21 +0530

    Time: 2377 ms (19.03%), Space: 19.7 MB (94.90%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        i, j, ans, mp = 0, 0, 0, {}
+        
+        while j<len(fruits):
+            if fruits[j] in mp: mp[fruits[j]]+=1
+            else: mp[fruits[j]]=1
+            j+=1
+            
+            if len(mp)<=2: ans=max(ans, j-i)
+            else:
+                while len(mp)>2 and i<=j:
+                    mp[fruits[i]]-=1
+                    if mp[fruits[i]]==0: mp.pop(fruits[i])
+                    i+=1
+        
+        return ans
+        
