f46b58151f43e40a089839199bed510d703589f2
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-01 19:13:34 +0530

    Time: 264 ms (34.32%), Space: 14.7 MB (36.58%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        def valid():
+            for i in f:
+                if f[i]<0: return False
+            return True
+        
+        f={}
+        for i in t:
+            if i in f: f[i]-=1
+            else: f[i]=-1
+        
+        i, j, ans= 0, 0, s+"0"
+        while j<len(s):
+            if s[j] in f: f[s[j]]+=1
+            else: f[s[j]]=1
+            
+            if valid():
+                if len(ans)>j-i+1: ans=s[i:j+1]
+                while valid() and i<=j:
+                    if len(ans)>j-i+1: ans=s[i:j+1]
+                    f[s[i]]-=1
+                    i+=1
+            j+=1
+        
+        return ans if ans!=s+"0" else ""
