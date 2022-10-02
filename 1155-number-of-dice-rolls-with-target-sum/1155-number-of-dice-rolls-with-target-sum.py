a3fcbb34b04957d01a42cc2af0c094248323af93
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-02 21:16:35 +0530

    Time: 817 ms (51.35%), Space: 14.6 MB (64.02%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,16 +1,31 @@
 class Solution:
-    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        def solve(n, target):
-            nonlocal k
-            if target==0 and n==0: return 1
-            if target<=0 or n<=0 : return 0
-            if dp[target][n]!=-1: return dp[target][n]
-            
-            ans, lim = 0, min(k, target)+1
-            for i in range(1, lim):
-                ans+=solve(n-1, target-i)
-            dp[target][n]=ans
-            return dp[target][n]
+    def numRollsToTarget(self, n: int, k: int, t: int) -> int:
+        dp=[[0]*(n+1) for _ in range(t+1)]
+        dp[0][0]=1
+        for i in range(1, t+1):
+            for j in range(1, n+1):
+                for a in range(1, min(k, i)+1): dp[i][j] += dp[i-a][j-1]
         
-        dp=[[-1]*(n+1) for i in range(target+1)]
-        return solve(n, target)%(10**9+7)
+        return dp[t][n]%(10**9+7)
+
+
+
+
+
+# class Solution:
+#     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+#         def solve(n, target):
+#             nonlocal k
+#             if target==0 and n==0: return 1
+#             if target<=0 or n<=0 : return 0
+#             if dp[target][n]!=-1: return dp[target][n]
+            
+#             ans, lim = 0, min(k, target)+1
+#             for i in range(1, lim):
+#                 ans+=solve(n-1, target-i)
+#             dp[target][n]=ans
+#             return dp[target][n]
+        
+#         dp=[[-1]*(n+1) for i in range(target+1)]
+#         return solve(n, target)%(10**9+7)
+        
