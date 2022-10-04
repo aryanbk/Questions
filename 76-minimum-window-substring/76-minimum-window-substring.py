6934939b8ad37edc90c50ab69ed22bf0ff35f1b6
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-05 00:41:35 +0530

    Time: 360 ms (23.20%), Space: 14.6 MB (83.76%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,24 +1,52 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        fq=Counter(t)
+        l=0
+        ans = s+t
+        
         def valid():
-            for i in f:
-                if f[i]<0: return False
+            for i in fq.values():
+                if i>0:
+                    return False
             return True
         
-        f={}
-        for i in t:
-            if i in f: f[i]-=1
-            else: f[i]=-1
-        
-        i, j, ans= 0, 0, s+"0"
-        while j<len(s):
-            if s[j] in f: f[s[j]]+=1
-            else: f[s[j]]=1
+        for r in range(len(s)):
+            fq[s[r]] = fq.get(s[r], 0) -1
             
-            while valid() and i<=j:
-                if len(ans)>j-i+1: ans=s[i:j+1]
-                f[s[i]]-=1
-                i+=1
-            j+=1
+            while valid():
+                if len(ans)>r-l+1:
+                    ans=s[l:r+1]
+                fq[s[l]]+=1
+                l+=1
         
-        return ans if ans!=s+"0" else ""
+        return "" if ans==s+t else ans
+        
+
+
+
+
+
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+#         def valid():
+#             for i in f:
+#                 if f[i]<0: return False
+#             return True
+        
+#         f={}
+#         for i in t:
+#             if i in f: f[i]-=1
+#             else: f[i]=-1
+        
+#         i, j, ans= 0, 0, s+"0"
+#         while j<len(s):
+#             if s[j] in f: f[s[j]]+=1
+#             else: f[s[j]]=1
+            
+#             while valid() and i<=j:
+#                 if len(ans)>j-i+1: ans=s[i:j+1]
+#                 f[s[i]]-=1
+#                 i+=1
+#             j+=1
+        
+#         return ans if ans!=s+"0" else ""
