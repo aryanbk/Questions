ba68d1316a06a17d960148b962b1f4789b2c0cd5
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-05 01:22:40 +0530

    Time: 4621 ms (16.33%), Space: 29.9 MB (79.56%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,12 +1,31 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        dq, i, j, ans = deque(), 0, 0, []
-        while(j<len(nums)):
-            while dq and dq[-1]<nums[j]: dq.pop()
-            dq.append(nums[j])
-            if j-i+1==k:
+        dq, l, ans= deque(), 0, []
+        
+        for r, val in enumerate(nums):
+            while dq and dq[-1]<val:
+                dq.pop()
+            dq.append(val)
+            
+            if r-l+1==k:
                 ans.append(dq[0])
-                if nums[i]==dq[0]: dq.popleft()
-                i+=1
-            j+=1
+                if dq[0]==nums[l]:
+                    dq.popleft()
+                l+=1
+                
         return ans
+
+
+
+# class Solution:
+#     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+#         dq, i, j, ans = deque(), 0, 0, []
+#         while(j<len(nums)):
+#             while dq and dq[-1]<nums[j]: dq.pop()
+#             dq.append(nums[j])
+#             if j-i+1==k:
+#                 ans.append(dq[0])
+#                 if nums[i]==dq[0]: dq.popleft()
+#                 i+=1
+#             j+=1
+#         return ans
