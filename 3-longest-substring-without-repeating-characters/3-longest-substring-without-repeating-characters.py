b7d89ceed9f7ee2695f2ed893cbf1816ce487fb8
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-04 22:53:44 +0530

    Time: 63 ms (94.50%), Space: 14 MB (92.93%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,13 +1,28 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        i, j, ans, f= 0, 0, 0, {}
-        while j<len(s) and i<len(s):
-            if s[j] not in f or f[s[j]]==0:
-                f[s[j]]=1
-                j+=1
-                ans=max(ans, j-i)
-            else : #if s[j] in f and f[s[j]]!=0
-                f[s[i]]-=1
-                i+=1
+        l=0
+        ans=0
+        freq=set()
+        for r in range(len(s)):
+            while s[r] in freq:
+                freq.remove(s[l])
+                l+=1
+            freq.add(s[r])
+            ans=max(ans, r-l+1)
         return ans
         
+
+
+
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         i, j, ans, f= 0, 0, 0, {}
+#         while j<len(s) and i<len(s):
+#             if s[j] not in f or f[s[j]]==0:
+#                 f[s[j]]=1
+#                 j+=1
+#                 ans=max(ans, j-i)
+#             else : #if s[j] in f and f[s[j]]!=0
+#                 f[s[i]]-=1
+#                 i+=1
+#         return ans
