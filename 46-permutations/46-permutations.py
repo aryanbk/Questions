12b67df3094bbf483cf029d7dc4573cfc0c86c75
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-09 12:30:29 +0530

    Time: 48 ms (84.12%), Space: 14.2 MB (23.34%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,17 +1,44 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def dfs(ptr, sub, mp):
+        
+        def swap(i, j):
+            temp=nums[i]
+            nums[i]=nums[j]
+            nums[j]=temp
+        
+        def permute(ptr):
             if ptr>=len(nums):
-                ans.append(sub.copy())
-                return
-            for i in range(0, len(nums)):
-                if mp[i]:
-                    sub.append(nums[i])
-                    mp[i]=False
-                    dfs(ptr+1, sub, mp)
-                    mp[i]=True
-                    sub.pop()
+                ans.append(nums[:])
+            else:
+                for p2 in range(ptr, len(nums)):
+                    swap(ptr, p2)
+                    permute(ptr+1)
+                    swap(ptr, p2)
+                
         
         ans=[]
-        dfs(0, [], [True]*len(nums))
+        permute(0)
         return ans
+
+
+
+
+
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         def dfs(ptr, sub, mp):
+#             if ptr>=len(nums):
+#                 ans.append(sub.copy())
+#                 return
+#             for i in range(0, len(nums)):
+#                 if mp[i]:
+#                     sub.append(nums[i])
+#                     mp[i]=False
+#                     dfs(ptr+1, sub, mp)
+#                     mp[i]=True
+#                     sub.pop()
+        
+#         ans=[]
+#         dfs(0, [], [True]*len(nums))
+#         return ans
+        
