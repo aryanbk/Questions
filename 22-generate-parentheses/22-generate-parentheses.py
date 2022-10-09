94ce435ba92b0f9b7c81d6fa5b06a69e2312ffec
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-09 22:33:57 +0530

    Time: 77 ms (18.28%), Space: 14.2 MB (39.76%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -0,0 +1,28 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        def bk(o, c, strg, stk):
+            if o==c==0 and not stk:
+                ans.append(strg[:])
+                return
+            if c==o==0 and stk:
+                return
+            
+            if o:
+                strg+="("
+                stk.append('(')
+                bk(o-1, c, strg, stk)
+                strg=strg[:-1]
+                stk.pop()
+            if c and stk:
+                strg+=")"
+                temp=stk.pop()
+                bk(o, c-1, strg, stk)
+                strg=strg[:-1]
+                stk.append(temp)
+            
+            
+        ans=[]
+        bk(n, n , "", [])
+        return ans
+        
