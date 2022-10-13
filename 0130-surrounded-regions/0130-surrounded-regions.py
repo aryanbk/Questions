b8370210b9a57ccf19237653a9e22fa2d48e4928
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-13 23:00:06 +0530

    Time: 375 ms (23.47%), Space: 15.1 MB (98.02%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,24 +1,21 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        m,n = len(board), len(board[0])
-        q=[]
+        m,n,q = len(board), len(board[0]), []
 
         for i in range(m):
             for j in range(n):
                 if (i==0 or j==0 or i==m-1 or j==n-1) and board[i][j]=="O":
                     q.append([i, j])
                     
-        zero = [[0]*n for _ in range(m)]
         drctn=[[1,0], [0,1], [0, -1], [-1, 0]]
         while q:
             i, j = q.pop(0)
-            zero[i][j]+=1
+            board[i][j]="#"
             for d in drctn:
-                if not (i+d[0]<0 or j+d[1]<0 or i+d[0]>=m or j+d[1]>=n or zero[i+d[0]][j+d[1]]!=0 or board[i+d[0]][j+d[1]]=='X'):
+                if not (i+d[0]<0 or j+d[1]<0 or i+d[0]>=m or j+d[1]>=n or board[i+d[0]][j+d[1]]!="O"):
                     q.append([i+d[0], j+d[1]])
         
         for i in range(m):
             for j in range(n):
-                if zero[i][j]==0:
-                    board[i][j]="X"
+                board[i][j]="O" if board[i][j]=="#" else "X"
         
