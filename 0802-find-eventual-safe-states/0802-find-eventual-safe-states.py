83514c26781e1674c1af92310d9effbcfa5de788
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-15 23:59:47 +0530

    Time: 1592 ms (38.53%), Space: 21.5 MB (84.19%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,62 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        vis1 = vis2 = [False]*n
+        safe = [None]*n
+        
+        
+        def isSafe(idx):
+            if safe[idx]!=None:
+                return safe[idx]
+            if vis2[idx]:
+                return False
+            if vis1[idx]:
+                return True
+            
+            vis1[idx]=True
+            vis2[idx]=True
+            res=True
+            for node in graph[idx]:
+                res = res and isSafe(node)
+            
+            vis2[idx]=False
+            safe[idx]=res
+            return safe[idx]
+        
+        ans=[]
+        for i in range(n):
+            if isSafe(i):
+                ans.append(i)
+        return ans
+
+
+# class Solution:
+#     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+#         n = len(graph)
+#         vis1 = vis2 = [False]*n
+#         safe = [None]*n
+        
+        
+#         def isSafe(idx):
+#             if safe[idx]!=None:
+#                 return safe[idx]
+#             if vis2[idx]:
+#                 return False
+#             if vis1[idx]:
+#                 return True
+            
+#             vis1[idx]=True
+#             vis2[idx]=True
+#             res=True
+#             for node in graph[idx]:
+#                 res = res and isSafe(node)
+            
+#             vis2[idx]=False
+#             safe[idx]=res
+#             return safe[idx]
+        
+#         ans=[]
+#         for i in range(n):
+#             if isSafe(i):
+#                 ans.append(i)
+#         return ans
