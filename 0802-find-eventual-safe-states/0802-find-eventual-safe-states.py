73da0dc800edcdaa34573a0ac4cc3ddfe9eaeda7
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-16 16:56:50 +0530

    Time: 1751 ms (23.88%), Space: 21.8 MB (57.22%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,31 +1,60 @@
+# Topological sort BFS
+# taking edges reversed
+
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        n = len(graph)
-        vis1 = vis2 = [False]*n
-        safe = [None]*n
-        
-        def isSafe(idx):
-            if safe[idx]!=None:
-                return safe[idx]
+        q, indg, outd = [], [0]*len(graph), [[None] for _ in range(len(graph))]
+        for i,v in enumerate(graph):
+            for j in v:
+                if outd[j]==[None]:
+                    outd[j]=[i]
+                else:
+                    outd[j].append(i)
+            indg[i]=len(v)
+            if len(v)==0:
+                q.append(i)
+        ans = []
+        while q:
+            node =q.pop(0)
+            ans.append(node)
             
-            vis1[idx]=True
-            vis2[idx]=True
-            for node in graph[idx]:
-                if vis2[node]:
-                    return False
-                if not vis1[node]:
-                    if not isSafe(node):
-                        return False
-            
-            vis2[idx]=False
-            safe[idx]=True
-            return safe[idx]
+            if outd[node]==[None]:
+                continue
+            for i in outd[node]:
+                indg[i]-=1
+                if indg[i]==0:
+                    q.append(i)
+        return sorted(ans)
         
-        ans=[]
-        for i in range(n):
-            if isSafe(i):
-                ans.append(i)
-        return ans
+# DFS
+# class Solution:
+#     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+#         n = len(graph)
+#         vis1 = vis2 = [False]*n
+#         safe = [None]*n
+        
+#         def isSafe(idx):
+#             if safe[idx]!=None:
+#                 return safe[idx]
+            
+#             vis1[idx]=True
+#             vis2[idx]=True
+#             for node in graph[idx]:
+#                 if vis2[node]:
+#                     return False
+#                 if not vis1[node]:
+#                     if not isSafe(node):
+#                         return False
+            
+#             vis2[idx]=False
+#             safe[idx]=True
+#             return safe[idx]
+        
+#         ans=[]
+#         for i in range(n):
+#             if isSafe(i):
+#                 ans.append(i)
+#         return ans
 
 
 # class Solution:
@@ -58,3 +87,4 @@ class Solution:
 #             if isSafe(i):
 #                 ans.append(i)
 #         return ans
+        
