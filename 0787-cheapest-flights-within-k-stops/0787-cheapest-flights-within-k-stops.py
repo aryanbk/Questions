0747a731243b7b741972921a78b0653f86f09045
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-19 19:47:03 +0530

    Time: 302 ms (49.07%), Space: 15.7 MB (14.11%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -4,29 +4,52 @@ class Solution:
         for u,v,c in flights:
             adj[u].append((v, c))
         
-        vis = [[math.inf]*(k+2) for _ in range(n)]
-        minhp = [(0, 0, src)]
+        vis, q = [float('inf')]*n, [(0, 0, src)]
         
-        while minhp:
-            curr, k0, node = heappop(minhp)
-            
-            if node==dst and k0>k+1:
+        while q:
+            k0, curr, node = q.pop(0)
+            if node==dst:
+                vis[dst] = min(vis[dst], curr)
                 continue
-            if node!=dst and k0>k:
+            if k0>k or vis[node]<curr:
                 continue
-
-            i=0
-            while i<=k0:
-                if vis[node][i] <= curr:
-                    break
-                i+=1
-            if i!=k0+1:
-                continue
-            vis[node][k0] = curr
-            
-            
+            vis[node]=min(vis[node], curr)
             for v, c in adj[node]:
-                heappush(minhp, (curr+c, k0+1, v))
+                q.append((k0+1, curr+c, v))
                 
-        ans = min(vis[dst])
-        return ans if ans!=math.inf else -1
+        return vis[dst] if vis[dst]!=float('inf') else -1
+        
+        
+# class Solution:
+#     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+#         adj = collections.defaultdict(list)
+#         for u,v,c in flights:
+#             adj[u].append((v, c))
+        
+#         vis = [[math.inf]*(k+2) for _ in range(n)]
+#         minhp = [(0, 0, src)]
+        
+#         while minhp:
+#             curr, k0, node = heappop(minhp)
+            
+#             if node==dst and k0>k+1:
+#                 continue
+#             if node!=dst and k0>k:
+#                 continue
+
+#             i=0
+#             while i<=k0:
+#                 if vis[node][i] <= curr:
+#                     break
+#                 i+=1
+#             if i!=k0+1:
+#                 continue
+#             vis[node][k0] = curr
+            
+            
+#             for v, c in adj[node]:
+#                 heappush(minhp, (curr+c, k0+1, v))
+                
+#         ans = min(vis[dst])
+#         return ans if ans!=math.inf else -1
+        
