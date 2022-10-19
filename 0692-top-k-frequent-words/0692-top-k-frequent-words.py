f3af5ce6894ef0813274ec73515c34e159bea647
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-19 13:12:44 +0530

    Time: 144 ms (9.00%), Space: 14.1 MB (6.92%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,13 +1,37 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        c = Counter(words)
+        hp = []
         
-        mp = collections.defaultdict(int)
-        for w in words:
-            mp[w]-=1
+        for w,cnt in c.items():
+            heappush(hp, Pair(cnt, w))
+            if len(hp)>k:
+                heappop(hp)
         
-        arr = sorted([j,i] for i,j in mp.items())
-        while len(arr)-k:
-            arr.pop()
+        return [pair.word for pair in sorted(hp)][::-1]
+
+    
+class Pair:
+    def __init__(self, cnt, word):
+        self.cnt = cnt
+        self.word = word
+    
+    def __lt__(self, other):
+        if self.cnt==other.cnt:
+            return self.word > other.word
+        return self.cnt < other.cnt
+    
+    
+# class Solution:
+#     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        
+#         mp = collections.defaultdict(int)
+#         for w in words:
+#             mp[w]-=1
+        
+#         arr = sorted([j,i] for i,j in mp.items())
+#         while len(arr)-k:
+#             arr.pop()
             
-        return [j for i,j in arr]
+#         return [j for i,j in arr]
     
