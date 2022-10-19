8a84b94284885abab0131f82d1113150b01ab50b
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-19 14:31:39 +0530

    Time: 2046 ms (44.03%), Space: 15.4 MB (75.85%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        m, n = len(heights), len(heights[0])
+        minhp, effort = [(0, 0, 0)], [[math.inf]*n for _ in range(m)]
+        dirct = [[1,0], [0,1], [-1,0], [0,-1]]
+        
+        while minhp:
+            eff, i, j = heappop(minhp)
+            if eff > effort[i][j]:
+                continue
+            effort[i][j] = eff
+            
+            for dx,dy in dirct:
+                x, y = i+dx, j+dy
+                if 0<=x<m and 0<=y<n:
+                    eff2 = max(eff, abs(heights[i][j]-heights[x][y]))
+                    if eff2<effort[x][y]:
+                        effort[x][y] = eff2
+                        heappush(minhp, (eff2, x, y))
+        
+        return effort[-1][-1]
