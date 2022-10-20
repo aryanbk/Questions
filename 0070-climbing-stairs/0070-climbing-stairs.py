50ae67712d1130038ee5b4bc6594af29d582e502
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-20 17:59:46 +0530

    Time: 26 ms (98.27%), Space: 13.7 MB (96.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,26 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        t = [0]*(n+1)
+        t[0] = 1
+        t[1] = 1
         
-        def climb(step):
-            if step<0:
-                return 0
-            if memo[step]==-1:
-                memo[step] = climb(step-1) + climb(step-2)
-            return memo[step]
+        for i in range(2, n+1):
+            t[i] = t[i-1] + t[i-2]
+        return t[n]
         
-        memo = [-1]*(n+1)
-        memo[0] = 1
         
-        return climb(n)
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+        
+#         def climb(step):
+#             if step<0:
+#                 return 0
+#             if memo[step]==-1:
+#                 memo[step] = climb(step-1) + climb(step-2)
+#             return memo[step]
+        
+#         memo = [-1]*(n+1)
+#         memo[0] = 1
+        
+#         return climb(n)
         
