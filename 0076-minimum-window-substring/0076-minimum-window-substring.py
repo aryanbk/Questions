e6e7e5ac26f57f0d800da05a2e2c9de5c03fa833
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-22 12:03:35 +0530

    Time: 734 ms (12.37%), Space: 14.8 MB (36.77%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,87 @@
+
+        
+        
+        
+
+# failed
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+#         fq=Counter(t)
+#         missing=len(t)
+#         l=0
+#         anslo, anshi = 0, len(s)+1
+        
+#         for r in range(len(s)):
+#             if s[r] in fq:
+#                 fq[s[r]]-=1
+#                 missing-=fq.get(s[r], 0)>=0
+            
+#             while not missing and l<=r:
+#                 if anshi-anslo>r-l+1:
+#                     anshi=r+1
+#                     anslo=l
+#                 if s[l] in fq:
+#                     fq[s[l]]+=1
+#                     missing += fq.get(s[r], 0)<0
+#                 l+=1
+            
+#         return "" if (anslo==0 and anshi==len(s)+1) else s[anslo: anshi]
+
+
+
+#####################################################################################
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        fq=Counter(t)
+        l=0
+        anslo, anshi = 0, len(s)+1
+        
+        def valid():
+            for i in fq.values():
+                if i>0:
+                    return False
+            return True
+        
+        for r in range(len(s)):
+            fq[s[r]] = fq.get(s[r], 0) -1
+            
+            while valid():
+                if anshi-anslo > r-l+1:
+                    anslo=l
+                    anshi=r+1
+                fq[s[l]]+=1
+                l+=1
+        
+        return "" if (anslo==0 and anshi==len(s)+1) else s[anslo: anshi]
+        
+
+
+#####################################################################################
+
+
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+#         def valid():
+#             for i in f:
+#                 if f[i]<0: return False
+#             return True
+        
+#         f={}
+#         for i in t:
+#             if i in f: f[i]-=1
+#             else: f[i]=-1
+        
+#         i, j, ans= 0, 0, s+"0"
+#         while j<len(s):
+#             if s[j] in f: f[s[j]]+=1
+#             else: f[s[j]]=1
+            
+#             while valid() and i<=j:
+#                 if len(ans)>j-i+1: ans=s[i:j+1]
+#                 f[s[i]]-=1
+#                 i+=1
+#             j+=1
+        
+#         return ans if ans!=s+"0" else ""
