b4163d15b3cc1d9a5b1867f8604263abf3714a24
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-22 16:18:53 +0530

    Time: 117 ms (85.54%), Space: 14.7 MB (96.96%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,20 +1,35 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m,n = len(grid), len(grid[0])
-        dp = [[0]*n for _ in range(m)]
+        prev = [10**7]*n
+        prev[0] = 0
+        
         for i in range(m):
+            curr = [0]*n
             for j in range(n):
-                if i>0 or j>0:
-                    up = left = 10**7
-                    if i>0:
-                        up = dp[i-1][j]
-                    if j>0:
-                        left = dp[i][j-1]
-                    dp[i][j] = min(up, left) + grid[i][j]
-                elif i==0 and j==0:
-                    dp[i][j] = grid[i][j]
-        print(dp)
-        return dp[-1][-1]
+                up=prev[j]
+                left = curr[j-1] if j>0 else 10**7
+                curr[j]  = min(up, left) + grid[i][j]
+            prev = curr
+        return prev[-1]
+
+# class Solution:
+#     def minPathSum(self, grid: List[List[int]]) -> int:
+#         m,n = len(grid), len(grid[0])
+#         dp = [[0]*n for _ in range(m)]
+#         for i in range(m):
+#             for j in range(n):
+#                 if i>0 or j>0:
+#                     up = left = 10**7
+#                     if i>0:
+#                         up = dp[i-1][j]
+#                     if j>0:
+#                         left = dp[i][j-1]
+#                     dp[i][j] = min(up, left) + grid[i][j]
+#                 elif i==0 and j==0:
+#                     dp[i][j] = grid[i][j]
+#         print(dp)
+#         return dp[-1][-1]
         
         
         
@@ -33,4 +48,5 @@ class Solution:
 #         m,n = len(grid), len(grid[0])
 #         memo = [[-1]*n for _ in range(m)]
 #         return findpath(m-1, n-1)
+        
         
