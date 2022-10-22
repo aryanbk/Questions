79fcedf83e22502f8ea191b4f524c5691722d727
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-22 14:58:30 +0530

    Time: 76 ms (6.14%), Space: 13.9 MB (74.25%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,18 +1,36 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[-1]*(n+1) for _ in range(m+1)]
+        dp=[[-1]*n for _ in range(m)]
         
-        for i in range(m+1):
-            dp[i][n]=0
-        for i in range(n+1):
-            dp[m][i] = 0
-        dp[m-1][n] = 1
+        for i in range(m):
+            for j in range(n):
+                if not (i==0 and j==0):
+                    up = left = 0
+                    if j>0:
+                        left = dp[i][j-1]
+                    if i>0:
+                        up = dp[i-1][j]
+                    dp[i][j] = up+left
+                else:
+                    dp[i][j] = 1
+        return dp[m-1][n-1]
         
-        for i in range(m-1, -1, -1):
-            for j in range(n-1, -1, -1):
-                dp[i][j] = dp[i+1][j] + dp[i][j+1]
         
-        return dp[0][0]
+# class Solution:
+#     def uniquePaths(self, m: int, n: int) -> int:
+#         dp = [[-1]*(n+1) for _ in range(m+1)]
+        
+#         for i in range(m+1):
+#             dp[i][n]=0
+#         for i in range(n+1):
+#             dp[m][i] = 0
+#         dp[m-1][n] = 1
+        
+#         for i in range(m-1, -1, -1):
+#             for j in range(n-1, -1, -1):
+#                 dp[i][j] = dp[i+1][j] + dp[i][j+1]
+        
+#         return dp[0][0]
         
         
 
