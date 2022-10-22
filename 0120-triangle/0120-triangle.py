6f8ccd4405fbfc40a7ca054bac684fafba6b81b7
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-22 23:31:28 +0530

    Time: 189 ms (7.20%), Space: 15.3 MB (30.15%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        def findmin(i, j):
+            if j > i:
+                return 10**10
+            if i == len(triangle)-1:
+                return triangle[i][j]
+            if memo[i][j]==-1:
+                memo[i][j] = triangle[i][j] + min(findmin(i+1, j), findmin(i+1, j+1))
+            return memo[i][j]
+        
+        memo = [[-1 for j in range(i+1)] for i in range(len(triangle))]
+        return findmin(0, 0)
