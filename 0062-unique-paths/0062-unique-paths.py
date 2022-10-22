3073c9c4ff3ed6184df5a9da9fbd1f0137c10f54
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-10-22 12:40:58 +0530

    Time: 52 ms (62.50%), Space: 13.8 MB (97.90%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,13 +1,33 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def move(i, j):
-            nonlocal m,n
-            if 0<=i<m and 0<=j<n:
-                if memo[i][j] == -1:
-                    memo[i][j] = move(i+1, j) + move(i, j+1)
-                return memo[i][j]
-            return 0
+        dp = [[-1]*(n+1) for _ in range(m+1)]
         
-        memo = [[-1]*n for _ in range(m)]
-        memo[-1][-1] = 1
-        return move(0, 0)
+        for i in range(m+1):
+            dp[i][n]=0
+        for i in range(n+1):
+            dp[m][i] = 0
+        dp[m-1][n] = 1
+        
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                dp[i][j] = dp[i+1][j] + dp[i][j+1]
+        
+        return dp[0][0]
+        
+        
+
+# class Solution:
+#     def uniquePaths(self, m: int, n: int) -> int:
+#         def move(i, j):
+#             nonlocal m,n
+#             if 0<=i<m and 0<=j<n:
+#                 if memo[i][j] == -1:
+#                     memo[i][j] = move(i+1, j) + move(i, j+1)
+#                 return memo[i][j]
+#             return 0
+        
+#         memo = [[-1]*n for _ in range(m)]
+#         memo[-1][-1] = 1
+#         return move(0, 0)
+
+        
