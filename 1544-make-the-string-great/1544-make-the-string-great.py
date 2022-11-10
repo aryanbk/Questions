3612fd7fa8991321a415ec89864582293de0cd10
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-11-10 23:25:12 +0530

    Time: 41 ms (90.12%), Space: 13.8 MB (63.25%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,6 +1,19 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        for i in range(len(s)-1):
-            if abs(ord(s[i]) - ord(s[i+1])) == 32:
-                return self.makeGood(s[:i]+s[i+2:])
-        return s
+        stk = []
+        for c in s:
+            if stk and abs(ord(stk[-1]) - ord(c)) == 32:
+                stk.pop()
+            else:
+                stk.append(c)
+        return "".join(stk)
+        
+        
+        
+        
+# class Solution:
+#     def makeGood(self, s: str) -> str:
+#         for i in range(len(s)-1):
+#             if abs(ord(s[i]) - ord(s[i+1])) == 32:
+#                 return self.makeGood(s[:i]+s[i+2:])
+#         return s
