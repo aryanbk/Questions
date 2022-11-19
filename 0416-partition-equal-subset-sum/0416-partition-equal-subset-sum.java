c3478657f6281638c7067023b13fa0f492733625
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-11-19 12:09:35 +0530

    Time: 69 ms (61.58%), Space: 44.2 MB (72.14%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    int[][] memo;
+    boolean[][] memo;
     public boolean canPartition(int[] nums) {
         int target=0;
         for(int n:nums){
@@ -9,24 +9,54 @@ class Solution {
             return false;
         }
         target /= 2;
-        memo = new int[target+1][nums.length+1];
-        return knap(0, nums, target);
-    }
-    boolean knap(int ptr, int[] nums, int target){
-        if(target==0){
-            return true;
-        }else if(ptr>=nums.length || target<0){
-            return false;
-        }else if(memo[target][ptr] != 0){
-            return memo[target][ptr]==1 ? true : false; 
-        }else{
-            boolean res = knap(ptr+1, nums, target) || knap(ptr+1, nums, target-nums[ptr]);
-            memo[target][ptr] = (res) ? 1 : -1;
-            return res;
+        memo = new boolean[target+1][nums.length];
+        for(int t=0; t<=target; ++t){
+            for(int ptr=0; ptr<nums.length; ++ptr){
+                if(t==0){
+                    memo[t][ptr]=true;
+                }else if(ptr==0){
+                    memo[t][ptr]=false;
+                }else{
+                    memo[t][ptr]=memo[t][ptr-1];
+                    if(t>=nums[ptr] && !memo[t][ptr]){
+                        memo[t][ptr] = memo[t-nums[ptr]][ptr-1];
+                    }
+                }
+            }
         }
+        return memo[target][nums.length-1];
     }
 }
 
+
+// class Solution {
+//     int[][] memo;
+//     public boolean canPartition(int[] nums) {
+//         int target=0;
+//         for(int n:nums){
+//             target += n;
+//         }
+//         if(target%2==1){
+//             return false;
+//         }
+//         target /= 2;
+//         memo = new int[target+1][nums.length+1];
+//         return knap(0, nums, target);
+//     }
+//     boolean knap(int ptr, int[] nums, int target){
+//         if(target==0){
+//             return true;
+//         }else if(ptr>=nums.length || target<0){
+//             return false;
+//         }else if(memo[target][ptr] != 0){
+//             return memo[target][ptr]==1 ? true : false; 
+//         }else{
+//             boolean res = knap(ptr+1, nums, target) || knap(ptr+1, nums, target-nums[ptr]);
+//             memo[target][ptr] = (res) ? 1 : -1;
+//             return res;
+//         }
+//     }
+// }
 
 // class Solution {
 //     public boolean canPartition(int[] nums) {
