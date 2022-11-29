ee68d70626ae2d9f936969db4ce45813be1ace5e
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-11-29 22:23:45 +0530

    Time: 2132 ms (73.69%), Space: 16.2 MB (26.73%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,10 +1,24 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [[float('inf')]*(amount+1) for _ in range(len(coins)+1)]
-        for l in range(len(dp)):
-            for amt in range(amount+1):
-                if amt==0: dp[l][amt] = 0
-                elif l!=0:
-                    if amt>=coins[l-1]: dp[l][amt] = min(dp[l-1][amt], 1 + dp[l][amt-coins[l-1]])
-                    else: dp[l][amt] = dp[l-1][amt]
+        for l in range(len(dp)): dp[l][0] = 0
+                
+        for l in range(1, len(dp)):
+            for amt in range(1, amount+1):
+                if amt>=coins[l-1]: dp[l][amt] = min(dp[l-1][amt], 1 + dp[l][amt-coins[l-1]])
+                else: dp[l][amt] = dp[l-1][amt]
+        
         return -1 if dp[-1][-1] == float('inf') else dp[-1][-1]
+
+
+
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         dp = [[float('inf')]*(amount+1) for _ in range(len(coins)+1)]
+#         for l in range(len(dp)):
+#             for amt in range(amount+1):
+#                 if amt==0: dp[l][amt] = 0
+#                 elif l!=0:
+#                     if amt>=coins[l-1]: dp[l][amt] = min(dp[l-1][amt], 1 + dp[l][amt-coins[l-1]])
+#                     else: dp[l][amt] = dp[l-1][amt]
+#         return -1 if dp[-1][-1] == float('inf') else dp[-1][-1]
