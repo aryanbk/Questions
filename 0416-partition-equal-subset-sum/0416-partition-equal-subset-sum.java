4bfcf79b08bb1177b38f44a4c89b3149a2198ec3
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-11-29 10:27:31 +0530

    Time: 65 ms (63.80%), Space: 43 MB (78.48%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,32 +1,53 @@
 class Solution {
-    boolean[][] memo;
     public boolean canPartition(int[] nums) {
         int target=0;
-        for(int n:nums){
-            target += n;
-        }
-        if(target%2==1){
-            return false;
-        }
-        target /= 2;
-        memo = new boolean[target+1][nums.length];
-        for(int t=0; t<=target; ++t){
-            for(int ptr=0; ptr<nums.length; ++ptr){
-                if(t==0){
-                    memo[t][ptr]=true;
-                }else if(ptr==0){
-                    memo[t][ptr]=false;
-                }else{
-                    memo[t][ptr]=memo[t][ptr-1];
-                    if(t>=nums[ptr] && !memo[t][ptr]){
-                        memo[t][ptr] = memo[t-nums[ptr]][ptr-1];
-                    }
-                }
+        for(int n:nums) target += n;
+        return target%2==0 ? subsetsum(nums, target/2) : false;
+    }
+    boolean subsetsum(int[] nums, int trgt){
+        System.out.println(trgt);
+        int lngt = nums.length;
+        boolean dp[][] = new boolean[lngt+1][trgt+1];
+        for(int l=0; l<=lngt; ++l){
+            for(int t=0; t<=trgt; ++t){
+                if(t==0) dp[l][t] = true;
+                else if(l!=0)
+                    dp[l][t] = t>=nums[l-1] ? dp[l-1][t] || dp[l-1][t-nums[l-1]] : dp[l-1][t];
             }
         }
-        return memo[target][nums.length-1];
+        return dp[lngt][trgt];
     }
 }
+
+
+// class Solution {
+//     public boolean canPartition(int[] nums) {
+//         int target=0;
+//         for(int n:nums){
+//             target += n;
+//         }
+//         if(target%2==1){
+//             return false;
+//         }
+//         target /= 2;
+//         boolean[][] memo = new boolean[target+1][nums.length];
+//         for(int t=0; t<=target; ++t){
+//             for(int ptr=0; ptr<nums.length; ++ptr){
+//                 if(t==0){
+//                     memo[t][ptr]=true;
+//                 }else if(ptr==0){
+//                     memo[t][ptr]=false;
+//                 }else{
+//                     memo[t][ptr]=memo[t][ptr-1];
+//                     if(t>=nums[ptr] && !memo[t][ptr]){
+//                         memo[t][ptr] = memo[t-nums[ptr]][ptr-1];
+//                     }
+//                 }
+//             }
+//         }
+//         return memo[target][nums.length-1];
+//     }
+// }
 
 
 // class Solution {
