74ebe09459ae0b3b25e3e64abbb6afe0fae15908
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-11-29 11:46:37 +0530

    Time: 58 ms (55.73%), Space: 51.7 MB (24.54%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,23 +1,46 @@
 class Solution {
-    int[][] dp;
-    int t;
     public int findTargetSumWays(int[] nums, int target) {
-        dp= new int[nums.length+1][2001];
-        for(int[] row : dp) Arrays.fill(row, -1);
-        t=target;
-        return subsetsum(nums, nums.length, 0);
-    }
-    
-    int subsetsum(int[] nums, int lnt, int trgt){
-        if(lnt==0){
-            if(trgt==t) return 1;
-            else return 0;
+        int sumt=0;
+        for(int x: nums) sumt+=x;
+        if(Math.abs(target)>sumt) return 0;
+        int[][] dp = new int[nums.length+1][(2*sumt) + 1];
+        
+        for(int l=0; l<nums.length+1; ++l){
+            for(int t=0; t<dp[0].length; ++t){
+                if(l==0)
+                    dp[l][t] = t==sumt ? 1 : 0;
+                else{
+                    if(t-nums[l-1]>=0) dp[l][t] += dp[l-1][t-nums[l-1]];
+                    if(t+nums[l-1]<dp[0].length) dp[l][t] += dp[l-1][t+nums[l-1]];
+                }
+            }
         }
-        if(dp[lnt][trgt+1000]!=-1) return dp[lnt][trgt+1000];
-        dp[lnt][trgt+1000] = subsetsum(nums, lnt-1, trgt+nums[lnt-1]) + subsetsum(nums, lnt-1, trgt-nums[lnt-1]);
-        return dp[lnt][trgt+1000];
+        
+        return dp[nums.length][target+sumt];
     }
 }
+
+
+// class Solution {
+//     int[][] dp;
+//     int t;
+//     public int findTargetSumWays(int[] nums, int target) {
+//         dp= new int[nums.length+1][2001];
+//         for(int[] row : dp) Arrays.fill(row, -1);
+//         t=target;
+//         return subsetsum(nums, nums.length, 0);
+//     }
+    
+//     int subsetsum(int[] nums, int lnt, int trgt){
+//         if(lnt==0){
+//             if(trgt==t) return 1;
+//             else return 0;
+//         }
+//         if(dp[lnt][trgt+1000]!=-1) return dp[lnt][trgt+1000];
+//         dp[lnt][trgt+1000] = subsetsum(nums, lnt-1, trgt+nums[lnt-1]) + subsetsum(nums, lnt-1, trgt-nums[lnt-1]);
+//         return dp[lnt][trgt+1000];
+//     }
+// }
 
 // class Solution {
 //     public int findTargetSumWays(int[] nums, int target) {
