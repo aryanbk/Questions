ca1b6ab11c9e373c32d16d77a0e696c7c5ceb1dc
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-11-29 21:32:58 +0530

    Time: 2435 ms (10.72%), Space: 29.8 MB (32.51%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -6,6 +6,20 @@ class Solution:
                 if t==0: dp[t][l] = 1
                 elif l==0: dp[t][l] = 0
                 else:
-                    if t>=coins[l-1]: dp[t][l] = dp[t][l-1] + dp[t-coins[l-1]][l]
-                    else: dp[t][l] = dp[t][l-1]
+                    dp[t][l] = dp[t][l-1]
+                    if t>=coins[l-1]: dp[t][l] += dp[t-coins[l-1]][l]
         return dp[-1][-1]
+
+
+
+# class Solution:
+#     def change(self, amount: int, coins: List[int]) -> int:
+#         dp = [[0]*(len(coins)+1) for _ in range(amount+1)]
+#         for t in range(amount+1):
+#             for l in range(len(coins)+1):
+#                 if t==0: dp[t][l] = 1
+#                 elif l==0: dp[t][l] = 0
+#                 else:
+#                     if t>=coins[l-1]: dp[t][l] = dp[t][l-1] + dp[t-coins[l-1]][l]
+#                     else: dp[t][l] = dp[t][l-1]
+#         return dp[-1][-1]
