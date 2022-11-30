828b86fb460cf1ec6c37f29ea1c212ce8a7de9c3
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-11-30 23:46:47 +0530

    Time: 6914 ms (28.73%), Space: 13.9 MB (93.84%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -1,12 +1,27 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        lth1, lth2 = len(nums1), len(nums2)
-        dp = [[0]*(lth2+1) for _ in range(lth1+1)]
-        ans = 0
-        for l1 in range(1, len(dp)):
-            for l2 in range(1, len(dp[0])):
-                if nums1[l1-1]==nums2[l2-1]:
-                    dp[l1][l2] = dp[l1-1][l2-1] + 1
-                    ans = max(ans, dp[l1][l2])
+        lth1, lth2, ans = len(nums1), len(nums2), 0
+        prev = [0]*(lth2+1)
         
+        for l1 in range(1, lth1+1):
+            dp = [0]*(lth2+1)
+            for l2 in range(1, lth2+1):
+                if nums1[l1-1]==nums2[l2-1]:
+                    dp[l2] = prev[l2-1] + 1
+                    ans = max(ans, dp[l2])
+            prev = dp
         return ans
+
+
+# class Solution:
+#     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+#         lth1, lth2 = len(nums1), len(nums2)
+#         dp = [[0]*(lth2+1) for _ in range(lth1+1)]
+#         ans = 0
+#         for l1 in range(1, len(dp)):
+#             for l2 in range(1, len(dp[0])):
+#                 if nums1[l1-1]==nums2[l2-1]:
+#                     dp[l1][l2] = dp[l1-1][l2-1] + 1
+#                     ans = max(ans, dp[l1][l2])
+        
+#         return ans
