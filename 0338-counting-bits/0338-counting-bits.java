e3ca4b54d689544e7de56bdb5e364d0af18201fd
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-12-05 12:26:40 +0530

    Time: 10 ms (37.74%), Space: 48.2 MB (38.85%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,14 +1,23 @@
 class Solution {
     public int[] countBits(int n) {
         int[] ans = new int[n+1];
-        int last2power=1;
-        for(int i=1; i<=n; ++i){
-            if((i&(i-1))==0) last2power=i;
-            ans[i] = 1 + ans[i-last2power];
-        }
+        for(int i=1; i<=n; i++) ans[i] = i%2==0 ? ans[i/2] : ans[i/2]+1;
         return ans;
     }
 }
+
+
+// class Solution {
+//     public int[] countBits(int n) {
+//         int[] ans = new int[n+1];
+//         int last2power=1;
+//         for(int i=1; i<=n; ++i){
+//             if((i&(i-1))==0) last2power=i;
+//             ans[i] = 1 + ans[i-last2power];
+//         }
+//         return ans;
+//     }
+// }
 
 
 
