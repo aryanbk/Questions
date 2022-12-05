0f246dc00410cbea1356d38ff0ffb2568d8810fd
Aryan K <101513901+aryanbk@users.noreply.github.com>
2022-12-05 12:17:11 +0530

    Time: 1 ms (99.97%), Space: 46.8 MB (82.22%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,17 +1,31 @@
 class Solution {
-    public int noOf1(int n) {
-        int ans=0;
-        while(n != 0){
-            ans += 1;
-            n &= (n-1);
+    public int[] countBits(int n) {
+        int[] ans = new int[n+1];
+        int last2power=1;
+        for(int i=1; i<=n; ++i){
+            if((i&(i-1))==0) last2power=i;
+            ans[i] = 1 + ans[i-last2power];
         }
         return ans;
     }
-    //
-    //
-    public int[] countBits(int n) {
-        int[] ans = new int[n+1];
-        for(int i=1; i<=n; ++i) ans[i] = noOf1(i);
-        return ans;
-    }
 }
+
+
+
+// class Solution {
+//     public int noOf1(int n) {
+//         int ans=0;
+//         while(n != 0){
+//             ans += 1;
+//             n &= (n-1);
+//         }
+//         return ans;
+//     }
+//     //
+//     //
+//     public int[] countBits(int n) {
+//         int[] ans = new int[n+1];
+//         for(int i=1; i<=n; ++i) ans[i] = noOf1(i);
+//         return ans;
+//     }
+// }
