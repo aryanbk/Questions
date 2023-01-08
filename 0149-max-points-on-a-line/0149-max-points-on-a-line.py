46ee200d1ca3a0ec168b4411abeed94f59adce9a
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-01-08 13:05:49 +0530

    Time: 3703 ms (5.26%), Space: 13.8 MB (94.26%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxPoints(self, pts: List[List[int]]) -> int:
+        
+        def on_line(i, j, k):
+            if pts[j][0]-pts[i][0]==0:
+                return pts[k][0]==pts[i][0]
+            if pts[j][1]-pts[i][1]==0:
+                return pts[k][1]==pts[i][1]
+            return (pts[k][0]-pts[i][0])/(pts[j][0]-pts[i][0])==(pts[k][1]-pts[i][1])/(pts[j][1]-pts[i][1])
+        
+        ans=1
+        for i,(x,y) in enumerate(pts):
+            done=set()
+            for j in range(i+1, len(pts)):
+                if (pts[j][0], pts[j][1]) in done:
+                    continue
+                satisf=2
+                for k in range(j+1, len(pts)):
+                    if (pts[k][0], pts[k][1]) not in done and on_line(i, j, k):
+                        satisf+=1
+                        done.add((pts[k][0], pts[k][1]))
+                ans=max(ans, satisf)
+        return ans
+            
