b7d641b3c8707f23b98491dbfa9b21313895d733
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-01-08 14:25:14 +0530

    Time: 17 ms (93.65%), Space: 42.1 MB (82.37%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int maxPoints(int[][] pts) {
+        int ans=1;
+        for(int i=0; i<pts.length; ++i){
+            HashMap<Double, Integer> map = new HashMap<>();
+            for(int j=i+1; j<pts.length; ++j){
+                Double slope = Double.NaN;
+                if(pts[j][0] != pts[i][0]){
+                    double up = (pts[j][1]-pts[i][1]);
+                    double down = (pts[j][0]-pts[i][0]);
+                    // slope = ((pts[j][1]-pts[i][1])/(pts[j][0]-pts[i][0]));
+                    slope =  (up/down)==-0.0 ? 0.0 :(up/down);
+                }
+                if(map.containsKey(slope)) map.put(slope, map.get(slope)+1);
+                else map.put(slope, 1);
+                ans=Math.max(ans, map.get(slope)+1);
+            }
+        }
+        return ans;
+    }
+}
+
+// class Solution {
+//     public int maxPoints(int[][] pts) {
+//         int ans =1, n=pts.length;
+//         for(int i=0; i<n; ++i){
+//             HashSet<int[]> done = new HashSet<>();
+//             for(int j=i+1; j<n; ++j){
+//                 if(done.contains(new int[]{pts[j][0], pts[j][1]})) continue;
+//                 int ptsOnLine=2;
+//                 for(int k=j+1; k<n; k++){
+//                     if(!done.contains(new int[]{pts[k][0], pts[k][1]}) && onLine(pts[i][0], pts[i][1], pts[j][0], pts[j][1], pts[k][0], pts[k][1])){
+//                         ptsOnLine++;
+//                         done.add(new int[]{pts[k][0], pts[k][1]});
+//                     }
+//                 }
+//                 System.out.println(i+" "+j+" "+ptsOnLine);
+//                 ans=Math.max(ans, ptsOnLine);
+//             }
+//         }
+//         return ans;
+//     }
+//     private boolean onLine(int x1, int y1, int x2, int y2, int x, int y){
+//         if(x2==x1) return x==x1;
+//         if(y2==y1) return y==y1;
+//         return (x-x1)/(x2-x1)==(y-y1)/(y2-y1);
+//     }
+// }
