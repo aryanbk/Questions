71ca05ed99c34f3f46cbc9301c2d51b2ff9de461
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-01-08 13:46:01 +0530

    Time: 78 ms (91.81%), Space: 14 MB (75.79%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -1,24 +1,37 @@
 class Solution:
     def maxPoints(self, pts: List[List[int]]) -> int:
-        
-        def on_line(i, j, k):
-            if pts[j][0]-pts[i][0]==0:
-                return pts[k][0]==pts[i][0]
-            if pts[j][1]-pts[i][1]==0:
-                return pts[k][1]==pts[i][1]
-            return (pts[k][0]-pts[i][0])/(pts[j][0]-pts[i][0])==(pts[k][1]-pts[i][1])/(pts[j][1]-pts[i][1])
-        
         ans=1
-        for i,(x,y) in enumerate(pts):
-            done=set()
+        for i,(x, y) in enumerate(pts):
+            d = defaultdict(int)
             for j in range(i+1, len(pts)):
-                if (pts[j][0], pts[j][1]) in done:
-                    continue
-                satisf=2
-                for k in range(j+1, len(pts)):
-                    if (pts[k][0], pts[k][1]) not in done and on_line(i, j, k):
-                        satisf+=1
-                        done.add((pts[k][0], pts[k][1]))
-                ans=max(ans, satisf)
+                slope=(pts[j][1]-y)/(pts[j][0]-x) if pts[j][0]!=x else None
+                d[slope]+=1
+                ans=max(ans, d[slope]+1)
         return ans
+        
+        
+# class Solution:
+#     def maxPoints(self, pts: List[List[int]]) -> int:
+        
+#         def on_line(i, j, k):
+#             if pts[j][0]-pts[i][0]==0:
+#                 return pts[k][0]==pts[i][0]
+#             if pts[j][1]-pts[i][1]==0:
+#                 return pts[k][1]==pts[i][1]
+#             return (pts[k][0]-pts[i][0])/(pts[j][0]-pts[i][0])==(pts[k][1]-pts[i][1])/(pts[j][1]-pts[i][1])
+        
+#         ans=1
+#         for i,(x,y) in enumerate(pts):
+#             done=set()
+#             for j in range(i+1, len(pts)):
+#                 if (pts[j][0], pts[j][1]) in done:
+#                     continue
+#                 satisf=2
+#                 for k in range(j+1, len(pts)):
+#                     if (pts[k][0], pts[k][1]) not in done and on_line(i, j, k):
+#                         satisf+=1
+#                         done.add((pts[k][0], pts[k][1]))
+#                 ans=max(ans, satisf)
+#         return ans
             
+        
