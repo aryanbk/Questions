e2c71584e4475b0117cc687e6096ab66a880d423
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-01-27 12:30:52 +0530

    Time: 1505 ms (15.51%), Space: 32.6 MB (13.82%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -0,0 +1,53 @@
+class TrieNode:
+    def __init__(self):
+        self.child = [None]*26
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def add(self, word):
+        ptr = self.root
+        # print(self.root.child)
+        for i in range(len(word)):
+            idx = ord(word[i]) - 97
+            # print(i)
+            if ptr.child[idx] == None:
+                ptr.child[idx] = TrieNode()
+            ptr = ptr.child[idx]
+        ptr.end = True
+    
+    def search(self, word):
+        if not word:
+            return True
+        ptr = self.root
+        ans = False
+        for i in range(len(word)):
+            idx = ord(word[i]) - ord('a')
+            if ptr.end:
+                ans = ans or self.search(word[i:])
+                if ans: return ans
+            if not ptr.child[idx]:
+                return False
+            ptr = ptr.child[idx]
+        return ptr.end or ans
+
+            
+            
+            
+            
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        words.sort(key=lambda x: len(x))
+        # print(words)
+        ans = []
+        root = Trie()
+        for w in words:
+            if root.search(w):
+                ans.append(w)
+            else:
+                root.add(w)
+        # print(len(ans))
+        return ans
+        
