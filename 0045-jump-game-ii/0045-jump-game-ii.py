bec5bf2159805140ff0619d4a1fb630259972791
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-02-08 18:47:56 +0530

    Time: 5262 ms (26.84%), Space: 15 MB (89.52%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,14 +1,43 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        def find(ptr):
-            if ptr==n-1: return 0
-            if memo[ptr]==None:
-                memo[ptr]=float('inf')
-                for i in range(min(nums[ptr], n-ptr-1)):
-                    memo[ptr] = min(memo[ptr], find(1+i+ptr)+1)
-            return memo[ptr]
-        
         n = len(nums)
-        memo = [None]*n
-        return find(0)
+        dp = [0]*n
+        for i in range(n-2, -1, -1):
+            ans = float('inf')
+            for j in range(i+1, min(n, i+nums[i]+1)):
+                ans = min(ans, dp[j]+1)
+            dp[i] = ans
+        return dp[0]
+
+
+# class Solution:
+#     def jump(self, nums: List[int]) -> int:
+#         def find(ptr):
+#             if ptr==n-1: return 0
+#             if memo[ptr]==None:
+#                 memo[ptr]=float('inf')
+#                 for i in range(min(nums[ptr], n-ptr-1)):
+#                     memo[ptr] = min(memo[ptr], find(1+i+ptr)+1)
+#             return memo[ptr]
         
+#         n = len(nums)
+#         memo = [None]*n
+#         return find(0)
+
+
+# class Solution:
+#     def jump(self, nums: List[int]) -> int:
+#         def find(ptr):
+#             if ptr>=len(nums):
+#                 return float('inf')
+#             if ptr==len(nums)-1:
+#                 return 0
+#             if memo[ptr]!=None:
+#                 return memo[ptr]
+#             memo[ptr]=float('inf')
+#             for i in range(nums[ptr]):
+#                 memo[ptr] = min(memo[ptr], find(1+i+ptr)+1)
+#             return memo[ptr]
+        
+#         memo = [None]*len(nums)
+#         return find(0)
