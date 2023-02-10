3e7b37810d230313605b7a1e8bae645d47fcaca0
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-02-10 22:27:08 +0530

    Time: 64 ms (37.89%), Space: 14 MB (24.36%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -3,15 +3,14 @@ class Solution:
         n = len(nums)
         left = right = pos = 0
         while right<n:
-            while right<n and nums[left]==nums[right]:
-                right+=1
+            while right<n and nums[left]==nums[right]: right+=1
             if right-left<3:
                 while left<right:
                     nums[pos] = nums[left]
                     pos+=1
                     left+=1
             else:
-                for i in range(2): nums[pos+i] = nums[left+i]
+                nums[pos], nums[pos+1] = nums[left], nums[left+1]
                 pos += 2
                 left = right
         return pos
