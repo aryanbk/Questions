87e4e0d9f60abc3415a87bca335fbc6fe07c5408
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-02-10 21:29:10 +0530

    Time: 725 ms (73.12%), Space: 28.6 MB (58.06%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.py
+++ 2306-naming-a-company/2306-naming-a-company.py
@@ -5,6 +5,18 @@ class Solution:
         ans = 0
         for i in range(26):
             for j in range(i+1, 26):
-                m, n = len(nums[i]-nums[j]), len(nums[j]-nums[i])
-                ans += m*n*2
+                ans += len(nums[i]-nums[j])*len(nums[j]-nums[i])*2
         return ans
+
+
+
+# class Solution:
+#     def distinctNames(self, ideas: List[str]) -> int:
+#         nums = [set() for i in range(26)]
+#         for w in ideas: nums[ord(w[0])-97].add(w[1:])
+#         ans = 0
+#         for i in range(26):
+#             for j in range(i+1, 26):
+#                 m, n = len(nums[i]-nums[j]), len(nums[j]-nums[i])
+#                 ans += m*n*2
+#         return ans
