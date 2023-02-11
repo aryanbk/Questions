e770ab8423c4616938b26a423456cf7efa66e23a
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-02-11 13:17:03 +0530

    Time: 198 ms (14.96%), Space: 14.2 MB (73.17%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -2,23 +2,17 @@ class Solution:
     def shortestAlternatingPaths(self, n: int, red: List[List[int]], blue: List[List[int]]) -> List[int]:
         
         def find(dest):
-            q = [(0, None)]
-            step=1
-            vis=set()
-            while True:
-                if not q:
-                    return -1
+            q, step, vis = [(0, None)], 1, set()
+            while q:
                 for _ in range(len(q)):
                     curr, col = q.pop(0)
                     for node, node_col in edge[curr]:
-                        # print("hi", node, curr, node_col)
-                        if (col != node_col) and (curr, node,node_col) not in vis:
-                            if node == dest:
-                                # print('hi')
-                                return step
+                        if col != node_col and (curr, node,node_col) not in vis:
+                            if node == dest: return step
                             q.append((node, node_col))
                             vis.add((curr, node, node_col))
                 step+=1
+            return -1
         
         
         edge=defaultdict(list)
@@ -27,4 +21,35 @@ class Solution:
         ans = [0]*n
         for dest in range(1, n): ans[dest] = find(dest)
         return ans
+
+# class Solution:
+#     def shortestAlternatingPaths(self, n: int, red: List[List[int]], blue: List[List[int]]) -> List[int]:
+        
+#         def find(dest):
+#             # if dest==0:
+#                 # return 0
+#             q, step, vis = [(0, None)], 0, set()
+            
+#             while q:
+#                 for _ in range(len(q)):
+#                     curr, col = q.pop(0)
+#                     if curr==dest: return step
+#                     for node, node_col in edge[curr]:
+#                         if col != node_col and (curr, node,node_col) not in vis:
+#                             if node == dest: return step+1
+#                             q.append((node, node_col))
+#                             vis.add((curr, node, node_col))
+#                 step+=1
+#             return -1
+        
+        
+#         edge=defaultdict(list)
+#         for a,b in red: edge[a].append((b, True))
+#         for a,b in blue: edge[a].append((b, False))
+#         ans = [0]*n
+#         # for dest in range(1, n): ans[dest] = find(dest)
+#         # print(find(0))
+#         # return ans
+#         return [find(i) for i in range(n)]
+            
             
