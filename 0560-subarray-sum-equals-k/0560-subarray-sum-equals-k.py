ce05515f4a7b1690f44ed04647a336fa13c28a4f
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-02-12 15:50:34 +0530

    Time: 316 ms (55.28%), Space: 18.2 MB (15.38%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -8,3 +8,13 @@ class Solution:
             ans+=hm[sumt-k]
             hm[sumt]+=1
         return ans
+
+# class Solution:
+#     def subarraySum(self, nums: List[int], k: int) -> int:
+#         ans=sumt=0
+#         hm={0:1}
+#         for i in nums:
+#             sumt+=i
+#             ans+=hm.get(sumt-k, 0)
+#             hm[sumt] = hm.get(sumt, 0)+1
+#         return ans
