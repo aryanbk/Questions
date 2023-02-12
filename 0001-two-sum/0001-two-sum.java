cff5f57b3cc94e694aeb4c3dbc10c0f2b59af0be
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-02-12 12:53:55 +0530

    Time: 1 ms (99.42%), Space: 42.7 MB (43.17%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,29 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int[] ans = new int[2];
+//         for (int i = 0; i < nums.length; i++) {
+//             for (int j = i+1; j < nums.length; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     ans[0]=i;
+//                     ans[1]=j;
+//                     return ans;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
