5a645fd4dcb1ad2b732c41401de38f47606aa749
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-02-13 20:44:00 +0530

    Time: 576 ms (37.26%), Space: 42.3 MB (92.77%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -0,0 +1,62 @@
+class Trie{
+    Trie[] child;
+    String end;
+    public Trie(){
+        child = new Trie[26];
+    }
+}
+
+class Solution {
+    Trie head = new Trie();
+    // List<String> ans = new ArrayList<>();
+    HashSet<String> ans = new HashSet<>();
+    int m , n;
+    int[] dirx = new int[]{1, 0, -1, 0};
+    int[] diry = new int[]{0, 1, 0, -1};
+    public List<String> findWords(char[][] board, String[] words) {
+        for(String w: words){
+            Trie curr=head;
+            for(int i=0; i<w.length(); i++){
+                if(curr.child[w.charAt(i)-97]==null)
+                    curr.child[w.charAt(i)-97] = new Trie();
+                curr = curr.child[w.charAt(i)-97];
+            }
+            curr.end = w;
+        }
+        
+        m=board.length;
+        n = board[0].length;
+        for(int i=0; i<m; ++i){
+            for(int j=0; j<n; ++j){
+                dfs(i, j, board, head);
+            }
+        }
+        // List<String> ans = new ArrayList<>();
+        // for
+        // return res;
+        return new ArrayList<>(ans);
+    }
+    private void dfs(int row, int col, char[][] board, Trie curr){
+        if(curr.end!=null)
+            ans.add(curr.end);
+        if(row<0 || row>=m || col<0 || col>=n || board[row][col]=='#' || curr.child[board[row][col]-97]==null)
+            return;
+        char temp = board[row][col];
+        board[row][col] = '#';
+        
+        for(int i=0; i<4; ++i){
+            int rowi=row+dirx[i], coli=col+diry[i];
+            dfs(rowi, coli, board, curr.child[temp-97]);
+        }
+//         {
+//             if(curr.end!=null)
+//                 ans.add(curr.end);
+//             int rowi=row+dirx[i], coli=col+diry[i];
+//             if(!(rowi<0 || rowi>=m || coli<0 || coli>=n || board[rowi][coli]=='#' || curr.child[temp-97]==null)){
+                
+//             }
+//         }
+        
+        board[row][col] = temp;
+    }
+}
