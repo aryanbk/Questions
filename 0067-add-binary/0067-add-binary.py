a84cf4c45b6afd0885dfe9afb8a8d7f588ab3f4f
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-02-14 11:37:47 +0530

    Time: 43 ms (29.96%), Space: 13.8 MB (63.05%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -4,13 +4,9 @@
 
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        la, lb = len(a), len(b)
-        if la<lb:
-            return self.addBinary(b, a)
-        la-=1
-        lb-=1
-        carry=0
-        ans=""
+        la, lb = len(a)-1, len(b)-1
+        if la<lb: return self.addBinary(b, a)
+        carry, ans = 0, ""
         while lb>=0:
             pos = int(a[la])+int(b[lb])+carry
             ans = ans + str(pos%2)
