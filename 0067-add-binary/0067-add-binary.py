3930778810ae94ac20ef0c09a4069681d6adb307
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-02-14 11:36:54 +0530

    Time: 35 ms (69.29%), Space: 13.8 MB (63.05%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,3 +1,26 @@
+# class Solution:
+#     def addBinary(self, a: str, b: str) -> str:
+#         return bin(int(a, 2)+int(b, 2))[2:]
+
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a, 2)+int(b, 2))[2:]
+        la, lb = len(a), len(b)
+        if la<lb:
+            return self.addBinary(b, a)
+        la-=1
+        lb-=1
+        carry=0
+        ans=""
+        while lb>=0:
+            pos = int(a[la])+int(b[lb])+carry
+            ans = ans + str(pos%2)
+            carry = pos//2
+            la-=1
+            lb-=1
+        while la>=0:
+            pos = int(a[la])+carry
+            ans = ans + str(pos%2)
+            carry = pos//2
+            la-=1
+        if carry: ans = ans + str(carry)
+        return ans[::-1]
