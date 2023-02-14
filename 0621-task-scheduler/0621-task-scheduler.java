86655aad4147582154d2515c601341790b581610
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-02-14 20:09:22 +0530

    Time: 58 ms (15.69%), Space: 43.8 MB (28.49%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x, y) -> Integer.compare(y, x));
+        
+        for(char c : tasks) map.put(c, map.getOrDefault(c, 0)+1);
+        for(Integer a: map.values()) pq.offer(a);
+        
+        int time = 0;
+        while(!pq.isEmpty()){
+            Stack<Integer> stk = new Stack<>();
+            int i=0;
+            
+            while(!pq.isEmpty() && i<n+1){
+                int popped = pq.poll();
+                if(popped!=1) stk.add(popped-1);
+                i++;
+                if(pq.isEmpty() && stk.isEmpty()) return time+i;
+            }
+            
+            while(!stk.isEmpty()) pq.offer(stk.pop());
+            time+=n+1;
+        }
+        return time;
+    }
+}
