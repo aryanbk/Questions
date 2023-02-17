bc97f3c2326a14a4b2da30ad4a6138b17835f806
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-02-17 12:41:39 +0530

    Time: 31 ms (77.31%), Space: 14 MB (58.47%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -4,6 +4,14 @@ class Twitter:
         self.users = {}
         self.time = 0
 
+    def __get(self, userid):
+        if userid in self.users:
+            return self.users[userid]
+        else:
+            self.users[userid] = [set([userid]), []]
+            return self.users[userid]
+        # self.users.get(userid, [set([userid]), []])
+
     def postTweet(self, userId: int, tweetId: int) -> None:
         self.__get(userId)[1].append((-self.time, tweetId))
         self.time+=1
@@ -34,68 +42,3 @@ class Twitter:
         all_followee = self.__get(followerId)[0]
         if followeeId in all_followee:
             all_followee.remove(followeeId)
-
-        
-        
-    def __get(self, userid):
-        if userid in self.users:
-            return self.users[userid]
-        else:
-            self.users[userid] = [set(), []]
-            self.users[userid][0].add(userid)
-            return self.users[userid]
-
-# Your Twitter object will be instantiated and called as such:
-# obj = Twitter()
-# obj.postTweet(userId,tweetId)
-# param_2 = obj.getNewsFeed(userId)
-# obj.follow(followerId,followeeId)
-# obj.unfollow(followerId,followeeId)
-
-# class Twitter:
-
-#     def __init__(self):
-#         self.users = {}
-
-#     def postTweet(self, userId: int, tweetId: int) -> None:
-#         # if userId in self.users:
-#         #     self.users[userId][1].append(-tweetId)
-#         # else:
-#         #     self.users[userId] = [set([userId]), [-tweetId]]
-#         self.users.get(userId, [set([userId]), []])[1].append(-tweetId)
-        
-
-#     def getNewsFeed(self, userId: int) -> List[int]:
-#         followees = self.users.get(userId, [set(), []])[0]
-#         all_posts = [self.users.get(i, [set(), []])[1] for i in followees]
-#         minhp = []
-        
-#         for i,posts in enumerate(all_posts):
-#             if posts: heappush(minhp, (posts[-1],i, len(posts)-1))
-        
-#         res = []
-        
-#         while len(res)<10 and minhp:
-#             tweet, posts, pos = heappop(minhp)
-#             res.append(-tweet)
-#             if pos!= 0:
-#                 heappush(minhp, (all[posts][pos-1], posts, pos-1))
-        
-#         return res
-            
-
-#     def follow(self, followerId: int, followeeId: int) -> None:
-#         self.users.get(followerId, [set([followerId]), []])[0].add(followeeId)
-        
-        
-#     def unfollow(self, followerId: int, followeeId: int) -> None:
-#         if followeeId in self.users[followerId][0]:
-#             self.users.get(followerId)[0].remove(followeeId)
-
-
-# # Your Twitter object will be instantiated and called as such:
-# # obj = Twitter()
-# # obj.postTweet(userId,tweetId)
-# # param_2 = obj.getNewsFeed(userId)
-# # obj.follow(followerId,followeeId)
-# # obj.unfollow(followerId,followeeId)
