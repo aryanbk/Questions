c8b613e0c6510a6521130046fca2bcd12c971087
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-03-01 13:08:58 +0530

    Time: 1844 ms (59.09%), Space: 23 MB (35.33%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,3 +1,39 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        return sorted(nums)
+        
+        def merge(a, b):
+            n, m = len(a), len(b)
+            ans = []
+            
+            i = j = k = 0
+            
+            while i<n and j<m:
+                if a[i]<=b[j]:
+                    # ans[k] = a[i]
+                    ans.append(a[i])
+                    i+=1
+                else:
+                    # ans[k] = b[j]
+                    ans.append(b[j])
+                    j+=1
+                k+=1
+            while i<n:
+                # ans[k] = a[i]
+                ans.append(a[i])
+                i+=1
+                k+=1
+            while j<m:
+                # ans[k] = b[j]
+                ans.append(b[j])
+                j+=1
+                k+=1
+            return ans
+        
+        def divide(l):
+            if len(l)<=1:
+                return l
+            first, last = divide(l[len(l)//2:]), divide(l[:len(l)//2])
+            
+            return merge(first, last)
+            
+        return divide(nums)
