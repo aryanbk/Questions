2b42c9e2932b60b0a2fbe89f076fb4ef106fa191
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-03-01 13:17:26 +0530

    Time: 812 ms (86.68%), Space: 21.9 MB (68.37%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,32 +1,41 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
+        heapify(nums)
+        ans = [0]*len(nums)
+        for i in range(len(nums)):
+            ans[i] = heappop(nums)
+        return ans
+
+
+# class Solution:
+#     def sortArray(self, nums: List[int]) -> List[int]:
         
-        def merge(a, b):
-            n, m = len(a), len(b)
-            ans = []
+#         def merge(a, b):
+#             n, m = len(a), len(b)
+#             ans = []
             
-            i = j = 0
+#             i = j = 0
             
-            while i<n and j<m:
-                if a[i]<=b[j]:
-                    ans.append(a[i])
-                    i+=1
-                else:
-                    ans.append(b[j])
-                    j+=1
-            while i<n:
-                ans.append(a[i])
-                i+=1
-            while j<m:
-                ans.append(b[j])
-                j+=1
-            return ans
+#             while i<n and j<m:
+#                 if a[i]<=b[j]:
+#                     ans.append(a[i])
+#                     i+=1
+#                 else:
+#                     ans.append(b[j])
+#                     j+=1
+#             while i<n:
+#                 ans.append(a[i])
+#                 i+=1
+#             while j<m:
+#                 ans.append(b[j])
+#                 j+=1
+#             return ans
         
-        def divide(l):
-            if len(l)<=1:
-                return l
-            first, last = divide(l[len(l)//2:]), divide(l[:len(l)//2])
+#         def divide(l):
+#             if len(l)<=1:
+#                 return l
+#             first, last = divide(l[len(l)//2:]), divide(l[:len(l)//2])
             
-            return merge(first, last)
+#             return merge(first, last)
             
-        return divide(nums)
+#         return divide(nums)
