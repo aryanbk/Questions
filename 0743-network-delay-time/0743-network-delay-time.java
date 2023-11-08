92934fb7fdc408a2883bcc72e64662e7a0cc7d8b
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-08 12:31:09 +0530

    Time: 10 ms (90.93%), Space: 48.1 MB (10.78%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,16 +1,28 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
+        // array of list of arrays to store adj list
         List<int[]>[] adj = new ArrayList[n+1];
+        
+        // initializing every element with new ArrayList<>();
         for(int i=0; i<n+1; ++i)
             adj[i] = new ArrayList<>();
-        for(int[] t: times){
+        // add adj nodes with delay time
+        for(int[] t: times)
             adj[t[0]].add(new int[]{t[1], t[2]});
-        }
         
+        // minTime will store minTime to reach a node
+        // it'll also work as 
         int[] minTime = new int[n+1];
         Arrays.fill(minTime, Integer.MAX_VALUE);
+        
+        // we will keep of maxTime at every node
+        // so we don't have to reiterate over minTime at the end
         int maxTime = 0;
+        // vis will keep track of no. of visited node
+        // if vis != n then all nodes are not reachable
         int vis = 0;
+        
+        // keep in mind pq syntax for arrays
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
         pq.add(new int[]{0, k});
         
@@ -18,22 +30,26 @@ class Solution {
             int currTime = pq.peek()[0];
             int node = pq.peek()[1];
             pq.poll();
+            
             if(minTime[node] < currTime)
                 continue;
-            maxTime = currTime;
+            
             minTime[node] = currTime;
+            maxTime = currTime;
             vis++;
             
-            for(int i=0; i<adj[node].size(); ++i){
-                int nbr = adj[node].get(i)[0];
-                int nbrTime = currTime + adj[node].get(i)[1];
-                if(minTime[nbr]>nbrTime){
-                    minTime[nbr] = nbrTime;
-                    pq.offer(new int[]{nbrTime, nbr});
+            // Iterate over neighboring nodes and update their times.
+            for (int[] nbr : adj[node]) {
+                int nbrNode = nbr[0];
+                int nbrTime = currTime + nbr[1];
+
+                if (nbrTime < minTime[nbrNode]) {
+                    minTime[nbrNode] = nbrTime;
+                    pq.offer(new int[]{nbrTime, nbrNode});
                 }
             }
         }
-        System.out.println(Arrays.toString(minTime));
+        
         return vis>=n ? maxTime : -1;
     }
 }
