c3c290d82bad217682640b9dbb7ab0cc138d5e4a
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-09 14:22:31 +0530

    Time: 33 ms (78.50%), Space: 55.3 MB (92.15%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -1,50 +1,113 @@
 class Solution {
-    public double maxProbability(int n, int[][] edge, double[] prob, int start_node, int end_node) {
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        // Create an adjacency list to represent the graph
         List<Pair>[] adj = new ArrayList[n];
-        for(int i=0; i<n; ++i)
+        for (int i = 0; i < n; ++i)
             adj[i] = new ArrayList<>();
-        for(int i=0; i<edge.length; ++i){
-            adj[edge[i][0]].add(new Pair(edge[i][1], prob[i]));
-            adj[edge[i][1]].add(new Pair(edge[i][0], prob[i]));
+        
+        // Populate the adjacency list based on edge probabilities
+        for (int i = 0; i < edges.length; ++i) {
+            int from = edges[i][0];
+            int to = edges[i][1];
+            double probability = succProb[i];
+            adj[from].add(new Pair(to, probability));
+            adj[to].add(new Pair(from, probability)); // Undirected graph
         }
+
+        // Use a priority queue to explore paths with higher probabilities first
+        PriorityQueue<Pair> pq = new PriorityQueue<>(new ProbabilityComparator());
+        pq.offer(new Pair(start, 1.0)); // Starting node with probability 1.0
+        double[] maxProbability = new double[n];
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>(new pCompare());
-        pq.offer(new Pair(start_node, 1.0));
-        double[] maxProb = new double[n];
-        
-        while(!pq.isEmpty()){
-            int node = pq.peek().node;
-            double p = pq.peek().p;
-            pq.poll();
-            if(maxProb[node]>p)
+        while (!pq.isEmpty()) {
+            Pair current = pq.poll();
+            int currentNode = current.node;
+            double currentProbability = current.p;
+            
+            // If we have already explored a path with higher probability, skip
+            if (maxProbability[currentNode] > currentProbability)
                 continue;
-            for(Pair nbr: adj[node]){
-                double newProb = nbr.p*p;
-                if(maxProb[nbr.node] < newProb){
-                    maxProb[nbr.node] = newProb;
-                    pq.offer(new Pair(nbr.node, newProb));
+
+            // Explore neighbors and update probabilities
+            for (Pair neighbor : adj[currentNode]) {
+                double newProbability = neighbor.p * currentProbability;
+                if (maxProbability[neighbor.node] < newProbability) {
+                    maxProbability[neighbor.node] = newProbability;
+                    pq.offer(new Pair(neighbor.node, newProbability));
                 }
             }
         }
-        return maxProb[end_node];
+        
+        return maxProbability[end];
     }
-    
 }
 
-class Pair{
+class Pair {
     int node;
-    double p;
-    public Pair(int _node, double _p){
+    double p;   
+    public Pair(int _node, double _p) {
         node = _node;
         p = _p;
     }
 }
-
-class pCompare implements Comparator<Pair>{
+class ProbabilityComparator implements Comparator<Pair> {
     @Override
-    public int compare(Pair pair1, Pair pair2){
-        if(pair1.p==pair2.p)
-            return 0;
-        return pair1.p - pair2.p >0 ? -1 : 1;
+    public int compare(Pair pair1, Pair pair2) {
+        // Compare probabilities in descending order
+        return Double.compare(pair2.p, pair1.p);
     }
 }
+
+
+
+
+// class Solution {
+//     public double maxProbability(int n, int[][] edge, double[] prob, int start_node, int end_node) {
+//         List<Pair>[] adj = new ArrayList[n];
+//         for(int i=0; i<n; ++i)
+//             adj[i] = new ArrayList<>();
+//         for(int i=0; i<edge.length; ++i){
+//             adj[edge[i][0]].add(new Pair(edge[i][1], prob[i]));
+//             adj[edge[i][1]].add(new Pair(edge[i][0], prob[i]));
+//         }
+        
+//         PriorityQueue<Pair> pq = new PriorityQueue<>(new pCompare());
+//         pq.offer(new Pair(start_node, 1.0));
+//         double[] maxProb = new double[n];
+        
+//         while(!pq.isEmpty()){
+//             int node = pq.peek().node;
+//             double p = pq.peek().p;
+//             pq.poll();
+//             if(maxProb[node]>p)
+//                 continue;
+//             for(Pair nbr: adj[node]){
+//                 double newProb = nbr.p*p;
+//                 if(maxProb[nbr.node] < newProb){
+//                     maxProb[nbr.node] = newProb;
+//                     pq.offer(new Pair(nbr.node, newProb));
+//                 }
+//             }
+//         }
+//         return maxProb[end_node];
+//     }
+    
+// }
+
+// class Pair{
+//     int node;
+//     double p;
+//     public Pair(int _node, double _p){
+//         node = _node;
+//         p = _p;
+//     }
+// }
+
+// class pCompare implements Comparator<Pair>{
+//     @Override
+//     public int compare(Pair pair1, Pair pair2){
+//         if(pair1.p==pair2.p)
+//             return 0;
+//         return pair1.p - pair2.p >0 ? -1 : 1;
+//     }
+// }
