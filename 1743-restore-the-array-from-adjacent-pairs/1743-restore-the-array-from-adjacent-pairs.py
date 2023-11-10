1b950e423e4fd89e57698f9020e67ff736a8bdc1
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-10 10:59:30 +0530

    Time: 1058 ms (22.78%), Space: 172.2 MB (10.28%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -2,17 +2,22 @@ class Solution:
     def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
         
         def bfs(node):
-            ans = []
+            ptr=0
             q=[(node, None)]
             
             while q:
                 curr, prev = q.pop(0)
-                ans.append(curr)
+                ans[ptr] = curr
+                ptr += 1
                 for nbr in adj[curr]:
                     if nbr != prev:
                         q.append((nbr, curr))
-            
-            return ans
+        
+        def dfs(node, prev=None, ptr=0):
+            ans[ptr] = node
+            for nbr in adj[node]:
+                if nbr != prev:
+                    dfs(nbr, node, ptr+1)
                 
                 
         
@@ -21,7 +26,9 @@ class Solution:
         for a,b in adjacentPairs:
             adj[a].append(b)
             adj[b].append(a)
-        
+        ans = [0]*(len(adjacentPairs)+1)
         for k,v in adj.items():
             if len(v)==1:
-                return bfs(k)
+                # bfs(k)
+                dfs(k)
+                return ans
