20fba16eb7daa7180b63aa1950b91527556228d4
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-14 12:18:28 +0530

    Time: 40 ms (85.05%), Space: 44.4 MB (49.02%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -3,20 +3,53 @@ class Solution {
         int[] first = new int[26];
         int[] last = new int[26];
         Arrays.fill(first, -1);
-        for(int i=0; i<s.length(); ++i){
-            int ind = s.charAt(i)-97;
+        
+        // Finding the first and last occurrence of each character in the string
+        for(int i = 0; i < s.length(); ++i) {
+            int ind = s.charAt(i) - 'a';
             if(first[ind] == -1)
                 first[ind] = i;
             last[ind] = i;
         }
+        
         int ans = 0;
-        for(int i=0; i<26; ++i){
-            Set<Character> set =  new HashSet<>();
-            for(int j=first[i]+1; j<last[i]; ++j){
-                set.add(s.charAt(j));
+        for(int i = 0; i < 26; ++i) {
+            // Counting distinct characters between the first and last occurrence of each character
+            boolean[] seen = new boolean[26];
+            for(int j = first[i] + 1; j < last[i]; ++j) {
+                int index = s.charAt(j) - 'a';
+                if (!seen[index]) {
+                    seen[index] = true;
+                    ans++;
+                }
             }
-            ans += set.size();
         }
         return ans;
     }
 }
+
+
+
+// not-clean
+
+// class Solution {
+//     public int countPalindromicSubsequence(String s) {        
+//         int[] first = new int[26];
+//         int[] last = new int[26];
+//         for(int i=0; i<s.length(); ++i){
+//             int ind = s.charAt(i)-97;
+//             if(first[ind] == 0)
+//                 first[ind] = i+1;
+//             last[ind] = i+1;
+//         }
+//         int ans = 0;
+//         for(int i=0; i<26; ++i){
+//             Set<Character> set =  new HashSet<>();
+//             for(int j=first[i]; j<last[i]-1; ++j){
+//                 set.add(s.charAt(j));
+//             }
+//             ans += set.size();
+//         }
+//         return ans;
+//     }
+// }
