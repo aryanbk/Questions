b49ec2f48108a1f9fb8933de0f237479838346b0
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-15 20:21:57 +0530

    Time: 2 ms (83.32%), Space: 41.7 MB (22.00%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,9 +1,9 @@
 class Solution {
-    int m;
-    int n;
     public int orangesRotting(int[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
+        
+        int m = grid.length;
+        int n = grid[0].length;
+        int fresh = 0;
         int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         Queue<int[]> q = new LinkedList<>();
         
@@ -11,12 +11,15 @@ class Solution {
             for(int j=0; j<n; ++j){
                 if(grid[i][j] == 2)
                     q.offer(new int[]{i, j});
+                else if(grid[i][j] == 1)
+                    ++fresh;
             }
         }
         
-        int time = 0;
         // BFS
-        while(!q.isEmpty()){
+        // fresh > 0 so that we dont have to do return time-1;
+        int time = 0;
+        while(!q.isEmpty() && fresh>0){
             int l = q.size();
             for(int a=0; a<l; ++a){                
                 int i = q.peek()[0];
@@ -29,17 +32,13 @@ class Solution {
                     if(x<0 || x>=m || y<0 || y>=n || grid[x][y] != 1)
                         continue;
                     grid[x][y] = 2;
+                    --fresh;
                     q.offer(new int[]{x, y});
                 }
             }
             ++time;
         }
-        for(int i=0; i<m; ++i){
-            for(int j=0; j<n; ++j){
-                if(grid[i][j]==1)
-                    return -1;
-            }
-        }
-        return Math.max(time-1, 0);
+        
+        return fresh==0 ? time : -1;
     }
 }
