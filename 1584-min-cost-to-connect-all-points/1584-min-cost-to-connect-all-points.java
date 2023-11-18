ced4ebec95245fb656ef9908750d22812f81c1be
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-18 16:48:10 +0530

    Time: 616 ms (13.77%), Space: 59 MB (61.46%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -2,26 +2,27 @@ class Solution {
     public int minCostConnectPoints(int[][] points) {
         int ans = 0, n = points.length;
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a->a[2]));
-        pq.offer(new int[]{points[0][0], points[0][1], 0, 0});
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
+        pq.offer(new int[]{0, points[0][0], points[0][1], 0}); // cost, x, y, index
         boolean[] vis = new boolean[n];
         
         while(!pq.isEmpty()){
-            int i = pq.peek()[0];
-            int j = pq.peek()[1];
-            int dis = pq.peek()[2];
+            
+            int cost = pq.peek()[0];
+            int i = pq.peek()[1];
+            int j = pq.peek()[2];
             int idx = pq.peek()[3];
             pq.poll();
             
             if(vis[idx]) continue;
             vis[idx] = true;
             
-            ans += dis;
+            ans += cost;
             
             for(int p=0; p<n; ++p){
-                if(p==idx) continue;
-                int newDis = Math.abs(points[p][0]-i)+Math.abs(points[p][1]-j);
-                pq.offer(new int[]{points[p][0], points[p][1], newDis, p});
+                if(vis[p]) continue;
+                int newCost = Math.abs(points[p][0]-i)+Math.abs(points[p][1]-j);
+                pq.offer(new int[]{newCost, points[p][0], points[p][1], p});
             }
         }
         
