192123f74798bf6c2839674ba3ee74a7e41c113b
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-20 00:00:49 +0530

    Time: 85 ms (63.26%), Space: 65.8 MB (72.65%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -22,7 +22,7 @@ public class Segtree {
     public Segtree(int[] nums) {
         this.nums = nums;
         int n = nums.length;
-        int height = (int) (Math.ceil(Math.log(n) / Math.log(2)));
+        int height = (int)(Math.ceil(Math.log(n) / Math.log(2)));
         int maxSize = 2 * (int) Math.pow(2, height) - 1;
         seg = new int[maxSize];
         build(0, n - 1, 0);
@@ -52,10 +52,10 @@ public class Segtree {
         }
         int mid = start + (end - start) / 2;
         return queryRange(start, mid, left, right, 2 * index + 1) +
-                queryRange(mid + 1, end, left, right, 2 * index + 2);
+            queryRange(mid + 1, end, left, right, 2 * index + 2);
     }
 
-    public void update(int index, int val){
+    public void update(int index, int val) {
         updateValue(0, nums.length - 1, 0, val, index);
     }
 
@@ -74,67 +74,6 @@ public class Segtree {
         updateValue(mid + 1, end, 2 * index + 2, val, i);
     }
 }
-
-
-// public class Segtree {
-//     private int[] seg;
-//     private int[] nums;
-
-//     public Segtree(int[] nums) {
-//         this.nums = nums;
-//         int n = nums.length;
-//         int height = (int) (Math.ceil(Math.log(n) / Math.log(2)));
-//         int maxSize = 2 * (int) Math.pow(2, height) - 1;
-//         seg = new int[maxSize];
-//         build(0, n - 1, 0);
-//     }
-
-//     private void build(int start, int end, int index) {
-//         if (start == end) {
-//             seg[index] = nums[start];
-//             return;
-//         }
-//         int mid = start + (end - start) / 2;
-//         build(start, mid, 2 * index + 1);
-//         build(mid + 1, end, 2 * index + 2);
-//         seg[index] = seg[2 * index + 1] + seg[2 * index + 2];
-//     }
-
-//     public int query(int left, int right) {
-//         return queryHelper(0, nums.length - 1, left, right, 0);
-//     }
-
-//     public int queryHelper(int start, int end, int left, int right, int index) {
-//         if (left <= start && right >= end) {
-//             return seg[index];
-//         }
-//         if (end < left || start > right) {
-//             return 0;
-//         }
-//         int mid = start + (end - start) / 2;
-//         return queryHelper(start, mid, left, right, 2 * index + 1) +
-//                 queryHelper(mid + 1, end, left, right, 2 * index + 2);
-//     }
-
-//     public void update(int index, int val){
-//         updateHelper(0, nums.length - 1, 0, val, index);
-//     }
-
-//     public void updateHelper(int start, int end, int index, int val, int i) {
-//         if (i < start || i > end) {
-//             return;
-//         }
-//         if (start == end && start == i) {
-//             nums[i] = val;
-//             seg[index] = val;
-//             return;
-//         }
-//         seg[index] += val - nums[i];
-//         int mid = start + (end - start) / 2;
-//         updateHelper(start, mid, 2 * index + 1, val, i);
-//         updateHelper(mid + 1, end, 2 * index + 2, val, i);
-//     }
-// }
 
 /**
  * Your NumArray object will be instantiated and called as such:
