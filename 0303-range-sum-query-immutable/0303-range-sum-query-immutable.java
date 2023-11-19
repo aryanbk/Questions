c4e8e5fa78440d89ba729290bf2db6f7d9068ae8
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-20 00:03:01 +0530

    Time: 10 ms (37.34%), Space: 49.2 MB (5.76%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,13 +1,11 @@
 class NumArray {
-    // int[] nums;
     Segtree st;
     public NumArray(int[] nums) {
-        // this.nums = nums;
         st = new Segtree(nums);
     }
     
     public int sumRange(int left, int right) {
-        return st.sumRange(left, right);
+        return st.query(left, right);
     }
 }
 
@@ -18,7 +16,7 @@ public class Segtree {
     public Segtree(int[] nums) {
         this.nums = nums;
         int n = nums.length;
-        int height = (int) (Math.ceil(Math.log(n) / Math.log(2)));
+        int height = (int)(Math.ceil(Math.log(n) / Math.log(2)));
         int maxSize = 2 * (int) Math.pow(2, height) - 1;
         seg = new int[maxSize];
         build(0, n - 1, 0);
@@ -35,7 +33,11 @@ public class Segtree {
         seg[index] = seg[2 * index + 1] + seg[2 * index + 2];
     }
 
-    public int query(int start, int end, int left, int right, int index) {
+    public int query(int left, int right) {
+        return queryRange(0, nums.length - 1, left, right, 0);
+    }
+
+    private int queryRange(int start, int end, int left, int right, int index) {
         if (left <= start && right >= end) {
             return seg[index];
         }
@@ -43,26 +45,28 @@ public class Segtree {
             return 0;
         }
         int mid = start + (end - start) / 2;
-        return query(start, mid, left, right, 2 * index + 1) +
-                query(mid + 1, end, left, right, 2 * index + 2);
+        return queryRange(start, mid, left, right, 2 * index + 1) +
+            queryRange(mid + 1, end, left, right, 2 * index + 2);
     }
 
-    public void update(int start, int end, int index, int val, int i) {
+    public void update(int index, int val) {
+        updateValue(0, nums.length - 1, 0, val, index);
+    }
+
+    private void updateValue(int start, int end, int index, int val, int i) {
         if (i < start || i > end) {
             return;
         }
-        seg[index] += val - nums[i];
-        if (start != end) {
-            int mid = start + (end - start) / 2;
-            update(start, mid, 2 * index + 1, val, i);
-            update(mid + 1, end, 2 * index + 2, val, i);
+        if (start == end && start == i) {
+            nums[i] = val;
+            seg[index] = val;
+            return;
         }
+        seg[index] += val - nums[i];
+        int mid = start + (end - start) / 2;
+        updateValue(start, mid, 2 * index + 1, val, i);
+        updateValue(mid + 1, end, 2 * index + 2, val, i);
     }
-
-    public int sumRange(int left, int right) {
-        return query(0, nums.length - 1, left, right, 0);
-    }
-
 }
 
 
