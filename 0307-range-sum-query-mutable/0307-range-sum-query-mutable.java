48e9e65c17613a24cd6b457da0b86baba0e526f1
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-19 23:57:52 +0530

    Time: 87 ms (57.67%), Space: 76.4 MB (5.23%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -7,11 +7,11 @@ class NumArray {
     }
     
     public void update(int index, int val) {
-        st.update(0, n - 1, 0, val, index);
+        st.update(index, val);
     }
     
     public int sumRange(int left, int right) {
-        return st.sumRange(left, right);
+        return st.query(left, right);
     }
 }
 
@@ -39,7 +39,11 @@ public class Segtree {
         seg[index] = seg[2 * index + 1] + seg[2 * index + 2];
     }
 
-    public int query(int start, int end, int left, int right, int index) {
+    public int query(int left, int right) {
+        return queryRange(0, nums.length - 1, left, right, 0);
+    }
+
+    private int queryRange(int start, int end, int left, int right, int index) {
         if (left <= start && right >= end) {
             return seg[index];
         }
@@ -47,31 +51,90 @@ public class Segtree {
             return 0;
         }
         int mid = start + (end - start) / 2;
-        return query(start, mid, left, right, 2 * index + 1) +
-                query(mid + 1, end, left, right, 2 * index + 2);
+        return queryRange(start, mid, left, right, 2 * index + 1) +
+                queryRange(mid + 1, end, left, right, 2 * index + 2);
     }
 
-    public void update(int start, int end, int index, int val, int i) {
+    public void update(int index, int val){
+        updateValue(0, nums.length - 1, 0, val, index);
+    }
+
+    private void updateValue(int start, int end, int index, int val, int i) {
         if (i < start || i > end) {
             return;
         }
-        // If the current node represents the update index
         if (start == end && start == i) {
-            nums[i] = val;  // Update the value in the nums array
-            seg[index] = val; // Update the value in the segment tree
+            nums[i] = val;
+            seg[index] = val;
             return;
         }
-        seg[index] += val - nums[i]; // Update the segment tree node
+        seg[index] += val - nums[i];
         int mid = start + (end - start) / 2;
-        update(start, mid, 2 * index + 1, val, i);
-        update(mid + 1, end, 2 * index + 2, val, i);
+        updateValue(start, mid, 2 * index + 1, val, i);
+        updateValue(mid + 1, end, 2 * index + 2, val, i);
     }
-
-    public int sumRange(int left, int right) {
-        return query(0, nums.length - 1, left, right, 0);
-    }
-
 }
+
+
+// public class Segtree {
+//     private int[] seg;
+//     private int[] nums;
+
+//     public Segtree(int[] nums) {
+//         this.nums = nums;
+//         int n = nums.length;
+//         int height = (int) (Math.ceil(Math.log(n) / Math.log(2)));
+//         int maxSize = 2 * (int) Math.pow(2, height) - 1;
+//         seg = new int[maxSize];
+//         build(0, n - 1, 0);
+//     }
+
+//     private void build(int start, int end, int index) {
+//         if (start == end) {
+//             seg[index] = nums[start];
+//             return;
+//         }
+//         int mid = start + (end - start) / 2;
+//         build(start, mid, 2 * index + 1);
+//         build(mid + 1, end, 2 * index + 2);
+//         seg[index] = seg[2 * index + 1] + seg[2 * index + 2];
+//     }
+
+//     public int query(int left, int right) {
+//         return queryHelper(0, nums.length - 1, left, right, 0);
+//     }
+
+//     public int queryHelper(int start, int end, int left, int right, int index) {
+//         if (left <= start && right >= end) {
+//             return seg[index];
+//         }
+//         if (end < left || start > right) {
+//             return 0;
+//         }
+//         int mid = start + (end - start) / 2;
+//         return queryHelper(start, mid, left, right, 2 * index + 1) +
+//                 queryHelper(mid + 1, end, left, right, 2 * index + 2);
+//     }
+
+//     public void update(int index, int val){
+//         updateHelper(0, nums.length - 1, 0, val, index);
+//     }
+
+//     public void updateHelper(int start, int end, int index, int val, int i) {
+//         if (i < start || i > end) {
+//             return;
+//         }
+//         if (start == end && start == i) {
+//             nums[i] = val;
+//             seg[index] = val;
+//             return;
+//         }
+//         seg[index] += val - nums[i];
+//         int mid = start + (end - start) / 2;
+//         updateHelper(start, mid, 2 * index + 1, val, i);
+//         updateHelper(mid + 1, end, 2 * index + 2, val, i);
+//     }
+// }
 
 /**
  * Your NumArray object will be instantiated and called as such:
