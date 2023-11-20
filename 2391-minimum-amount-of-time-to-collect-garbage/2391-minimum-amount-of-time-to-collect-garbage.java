2b6a74dbb2160dc948f37c451e3b6a21174fc181
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-20 14:02:49 +0530

    Time: 210 ms (5.30%), Space: 67.7 MB (16.86%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
@@ -1,33 +1,18 @@
 class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
-        int ans = 0;
-        for(int i=1; i<travel.length; ++i)
-            travel[i] += travel[i-1];
-        ans += collect(garbage, travel, 'M');
-        System.out.println(ans);
-        ans += collect(garbage, travel, 'G');
-        System.out.println(ans);
-        ans += collect(garbage, travel, 'P');
-        System.out.println(ans);
-        
-        return ans;
+        for(int i=1; i<travel.length; ++i) travel[i] += travel[i-1];
+        return collect(garbage, travel, 'M') + collect(garbage, travel, 'G') + collect(garbage, travel, 'P');
         
     }
     private int collect(String[] gbg, int[] tvl, char g){
-        int ans = 0;
-        int last = 0;
+        int ans = 0, last = 0;
         for(int i=0; i<gbg.length; ++i){
-            int occur = (int)(gbg[i].chars().filter(ch -> ch == g).count());
-            if (occur != 0)
-                last = i;
-            ans += occur;
-            // ans += i<tvl.length ? tvl[i] : 0;
+            
+            // count no of occurances of g in gbg[i]
+            int count = (int)(gbg[i].chars().filter(ch -> ch == g).count());
+            ans += count;
+            if (count != 0) last = i;
         }
-        // if (last>0)
-            // ans += tvl[last];
         return last>0 ? ans+tvl[last-1] : ans;
     }
-    // public static int count(String str, char character) {
-        // return str.chars().filter(ch -> ch == character).count();
-    // }
 }
