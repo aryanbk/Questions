0908c8c01b295e800b5188157c5344d92b40c8f5
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-21 16:15:41 +0530

    Time: 1 ms (98.01%), Space: 41 MB (34.19%) - LeetHub

---
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.java
@@ -0,0 +1,61 @@
+class Solution {
+    public boolean equationsPossible(String[] equations) {
+        DJS djs = new DJS(26);
+        for(String s: equations){
+            if(s.charAt(1)=='=')
+                djs.union(s.charAt(0)-'a', s.charAt(3)-'a');
+        }
+        for(String s: equations){
+            if(s.charAt(1) == '!')
+                if(djs.isSame(s.charAt(0)-'a', s.charAt(3)-'a'))
+                    return false;
+        }
+        return true;
+    }
+}
+
+class DJS {
+    private int[] parent;
+    private int[] rank;
+
+    public DJS(int size) {
+        parent = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < size; i++) {
+            parent[i] = i; // Each element is its own parent initially
+            rank[i] = 0; // Initialize rank as 0
+        }
+    }
+
+    // Find the root/representative of the set
+    public int find(int x) {
+        int i = x;
+        while(x != parent[x])
+            x = parent[x];
+        parent[i] = x;
+        return parent[x];
+    }
+    
+    public boolean isSame(int x, int y){
+        return find(x) == find(y);
+    }
+
+    // Merge two sets by rank (Union by Rank)
+    public boolean union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+            return true;
+        }
+        return false;
+    }
+}
