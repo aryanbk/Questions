8c22fa613f89d90668038200b52c03da00181cc4
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-21 11:29:59 +0530

    Time: 40 ms (55.89%), Space: 54.8 MB (63.30%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.java
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.java
@@ -1,28 +1,57 @@
 class Solution {
     public int countNicePairs(int[] nums) {
         Map<Long, Integer> map = new HashMap<>();
+        final int MOD = 1_000_000_007;
         long ans = 0;
-        int MOD = 1000000007;
-        for(int i=0; i<nums.length; ++i){
-            long num = nums[i];
-            long revnum = rev(nums[i]);
-            long rhs = nums[i] - revnum;
-            ans = (ans + map.getOrDefault(rhs, 0))%MOD;
+
+        for (int num : nums) {
+            long rhs = num - reverse(num);
+            ans = (ans + map.getOrDefault(rhs, 0)) % MOD;
             map.put(rhs, map.getOrDefault(rhs, 0) + 1);
-            // System.out.println(num+" "+revnum+" "+rhs);
         }
-        // for(long k: map.keySet()){
-            // System.out.println(k+" "+map.get(k));
-        // }
-        return (int)ans;
+
+        return (int) ans;
     }
-    private long rev(int i){
-        long ans = 0;
-        while(i != 0){
-            ans *= 10;
-            ans += i%10;
-            i /= 10;
+
+    private long reverse(int num) {
+        long reversed = 0;
+        while (num != 0) {
+            reversed *= 10;
+            reversed += num % 10;
+            num /= 10;
         }
-        return ans;
+        return reversed;
     }
 }
+
+
+
+
+// class Solution {
+//     public int countNicePairs(int[] nums) {
+//         Map<Long, Integer> map = new HashMap<>();
+//         long ans = 0;
+//         int MOD = 1000000007;
+//         for(int i=0; i<nums.length; ++i){
+//             long num = nums[i];
+//             long revnum = rev(nums[i]);
+//             long rhs = nums[i] - revnum;
+//             ans = (ans + map.getOrDefault(rhs, 0))%MOD;
+//             map.put(rhs, map.getOrDefault(rhs, 0) + 1);
+//             // System.out.println(num+" "+revnum+" "+rhs);
+//         }
+//         // for(long k: map.keySet()){
+//             // System.out.println(k+" "+map.get(k));
+//         // }
+//         return (int)ans;
+//     }
+//     private long rev(int i){
+//         long ans = 0;
+//         while(i != 0){
+//             ans *= 10;
+//             ans += i%10;
+//             i /= 10;
+//         }
+//         return ans;
+//     }
+// }
