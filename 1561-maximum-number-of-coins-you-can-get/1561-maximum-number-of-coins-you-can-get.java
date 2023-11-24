913cdb14f045b7b907ddf64998d196a44450ed1d
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-24 20:30:38 +0530

    Time: 28 ms (40.74%), Space: 54.4 MB (72.31%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -1,14 +1,24 @@
 class Solution {
     public int maxCoins(int[] piles) {
         Arrays.sort(piles);
-        int i=0;
-        int j=piles.length-2;
         int ans = 0;
-        while(i<=j){
-            ans += piles[j];
-            ++i;
-            j-=2;
-        }
+        for(int i=0; i<piles.length*2/3; i+=2) ans += piles[piles.length - i - 2];
         return ans;
     }
 }
+
+
+// class Solution {
+//     public int maxCoins(int[] piles) {
+//         Arrays.sort(piles);
+//         int i=0;
+//         int j=piles.length-2;
+//         int ans = 0;
+//         while(i<=j){
+//             ans += piles[j];
+//             ++i;
+//             j-=2;
+//         }
+//         return ans;
+//     }
+// }
