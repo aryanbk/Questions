01a550a2cf8415e09d85519781ef9be24ac2b128
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-27 15:06:50 +0530

    Time: 5 ms (36.35%), Space: 40.2 MB (57.46%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -1,31 +1,71 @@
+// dp tab space optimized
+// TC O(n * sumt/2)
+// SC O(sumt/2)
+
 class Solution {
     public int lastStoneWeightII(int[] stones) {
         int n = stones.length;
         int sumt = 0;
-        for(int s: stones) sumt += s;
-        boolean[][] dp = new boolean[n+1][(sumt/2)+1];
         int ans = Integer.MAX_VALUE;
-        
-        for(int sz=0; sz<=n; ++sz){
-            for(int sm=0; sm<=sumt/2; ++sm){
-                if(sm==0) dp[sz][sm] = true;
-                else if(sz == 0) dp[sz][sm] = false;
+        for(int s: stones) sumt += s;
+
+        boolean[] dp = new boolean[(sumt/2)+1];
+        dp[0] = true;
+
+        for(int sz = 1; sz <= n; ++sz){
+            boolean[] temp = new boolean[(sumt/2)+1];
+            for(int sm = 0; sm <= sumt/2; ++sm){
+                if(sm == 0) temp[sm] = true;
+                else if(sz == 0) temp[sm] = false;
                 else{
-                    dp[sz][sm] = dp[sz-1][sm];
-                    if(sm>=stones[sz-1])
-                        dp[sz][sm] |= dp[sz-1][sm-stones[sz-1]];
+                    temp[sm] = dp[sm];
+                    if(sm >= stones[sz - 1])
+                        temp[sm] |= dp[sm - stones[sz - 1]];
                 }
-                
-                if(sz == n && dp[n][sm]){
-                    int currDiff = sumt - 2*sm;
+                if(sz == n && temp[sm]){
+                    int currDiff = sumt - 2 * sm;
                     ans = Math.min(ans, currDiff);
                 }
-                
             }
+            dp = temp;
         }
         return ans;
     }
 }
+
+
+
+
+// dp - tab - without space opti
+//
+// class Solution {
+//     public int lastStoneWeightII(int[] stones) {
+//         int n = stones.length;
+//         int sumt = 0;
+//         for(int s: stones) sumt += s;
+//         boolean[][] dp = new boolean[n+1][(sumt/2)+1];
+//         int ans = Integer.MAX_VALUE;
+        
+//         for(int sz=0; sz<=n; ++sz){
+//             for(int sm=0; sm<=sumt/2; ++sm){
+//                 if(sm==0) dp[sz][sm] = true;
+//                 else if(sz == 0) dp[sz][sm] = false;
+//                 else{
+//                     dp[sz][sm] = dp[sz-1][sm];
+//                     if(sm>=stones[sz-1])
+//                         dp[sz][sm] |= dp[sz-1][sm-stones[sz-1]];
+//                 }
+                
+//                 if(sz == n && dp[n][sm]){
+//                     int currDiff = sumt - 2*sm;
+//                     ans = Math.min(ans, currDiff);
+//                 }
+                
+//             }
+//         }
+//         return ans;
+//     }
+// }
 
 // first attempt - accepted
 // made some mistakes
