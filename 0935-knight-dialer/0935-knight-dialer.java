1253763f5d36c596b9f9999c5d1091b7ebd44b27
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-27 11:21:43 +0530

    Time: 112 ms (25.27%), Space: 43.7 MB (45.92%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.java
+++ 0935-knight-dialer/0935-knight-dialer.java
@@ -1,34 +1,35 @@
 class Solution {
     int[][] dir = {{-2, -1}, {-1, -2}, {1, -2}, {2, -1}, {-2, 1}, {-1, 2}, {1, 2}, {2, 1}};
-    int MOD = 1000000007;
+    int MOD = 1_000_000_007;
     int[][][] memo;
+    
     public int knightDialer(int n) {
         int ans = 0;
         memo = new int[4][3][n+1];
-        for(var i: memo){
-            for(var j: i){
-                Arrays.fill(j, -1);
+        
+        for(var m1: memo){
+            for(var m2: m1){
+                Arrays.fill(m2, -1);
             }
         }
+        
         for(int i=0; i<4; ++i){
             for(int j=0; j<3; ++j){
-                ans = (ans + help(i, j, n))%MOD;
+                ans = (ans + recurssive(i, j, n))%MOD;
             }
         }
+        
         return ans;
     }
-    private int help(int i, int j, int n){
-        if(i<0 || i>=4 || j<0 || j>=3 || (i==3 && j==0) || (i==3 && j==2)) return 0;
+    private int recurssive(int i, int j, int n){
+        if(i<0 || i>=4 || j<0 || j>=3 || (i == 3 && (j == 0 || j == 2))) return 0;
         if(n==1) return 1;
         
         if(memo[i][j][n] != -1) return memo[i][j][n];
         
         memo[i][j][n] = 0;
-        for(int[] d: dir){
-            int x = i + d[0];
-            int y = j + d[1];
-            memo[i][j][n] = (memo[i][j][n] + help(x, y, n-1))%MOD;
-        }
+        for(int[] d: dir)
+            memo[i][j][n] = (memo[i][j][n] + recurssive(i+d[0], j+d[1], n-1))%MOD;
         
         return memo[i][j][n];
     }
