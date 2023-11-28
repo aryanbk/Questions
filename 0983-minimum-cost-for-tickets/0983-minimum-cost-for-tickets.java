7baee739dbc197234f2bf91ea2876a137b787358
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-28 17:55:13 +0530

    Time: 3 ms (19.01%), Space: 44.4 MB (5.01%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,74 +1,7 @@
-// class Solution {
-//     int[][] memo;
-    
-//     public int mincostTickets(int[] days, int[] costs) {
-//         memo = new int[days.length][366];
-//         for (int[] m1 : memo) Arrays.fill(m1, -1); // Initialize with -1 instead of Integer.MAX_VALUE
-        
-//         return help(0, 0, days, costs);
-//     }
-    
-//     private int help(int i, int freeTill, int[] days, int[] costs) {
-//         if (i == days.length) return 0;
-        
-//         if (memo[i][freeTill] != -1) return memo[i][freeTill];
-        
-//         int result;
-//         if (freeTill >= days[i]) {
-//             result = help(i + 1, freeTill, days, costs);
-//         } else {
-//             result = Math.min(
-//                 costs[0] + help(i + 1, days[i], days, costs),
-//                 Math.min(
-//                     costs[1] + help(i + 1, days[i] + 6, days, costs),
-//                     costs[2] + help(i + 1, days[i] + 29, days, costs)
-//                 )
-//             );
-//         }
-        
-//         memo[i][freeTill] = result;
-//         return result;
-//     }
-// }
-
-
-
-// class Solution {
-//     int[][] memo;
-//     public int mincostTickets(int[] days, int[] costs) {
-//         memo = new int[days.length][366];
-//         for(int[] m1: memo) Arrays.fill(m1, -1);
-        
-//         return help(0, 0, days, costs);
-//     }
-//     private int help(int i, int freeTill, int[] days, int[] costs){
-//         if(i==days.length) return 0;
-        
-//         if(memo[i][freeTill] != -1) return memo[i][freeTill];
-        
-//         int ans = Integer.MAX_VALUE;
-//         if(freeTill>=days[i]) ans = Math.min(ans, help(i+1, freeTill, days, costs));
-//         else{
-//             ans = Math.min(ans, costs[0]+help(i+1, Math.min(i, 365), days, costs));
-//             if(i==0) System.out.println(ans);
-//             ans = Math.min(ans, costs[1]+help(i+1, Math.min(i+6, 365), days, costs));
-//             if(i==0) System.out.println(ans);
-//             ans = Math.min(ans, costs[2]+help(i+1, Math.min(i+29, 365), days, costs));
-//             if(i==0) System.out.println(ans);
-
-//         }
-//         memo[i][freeTill] = ans;
-//         return memo[i][freeTill];
-//     }
-// }
-
-
-
 class Solution {
     int[][] memo;
     public int mincostTickets(int[] days, int[] costs) {
-        Arrays.sort(days);
-        memo = new int[days.length][410];
+        memo = new int[days.length][366];
         for(int[] m1: memo) Arrays.fill(m1, -1);
         
         return help(0, 0, days, costs);
@@ -78,13 +11,19 @@ class Solution {
         
         if(memo[i][freeTill] != -1) return memo[i][freeTill];
         
-        memo[i][freeTill] = Integer.MAX_VALUE;
-        if(freeTill>=days[i]) memo[i][freeTill] = Math.min(memo[i][freeTill], help(i+1, freeTill, days, costs));
+        int ans = Integer.MAX_VALUE;
+        
+        if(freeTill>=days[i]) ans = Math.min(ans, help(i+1, freeTill, days, costs));
         else{
-            memo[i][freeTill] = Math.min(memo[i][freeTill], costs[0]+help(i+1, 0,days, costs));
-            memo[i][freeTill] = Math.min(memo[i][freeTill], costs[1]+help(i+1, days[i]+6, days, costs));
-            memo[i][freeTill] = Math.min(memo[i][freeTill], costs[2]+help(i+1, days[i]+29, days, costs));
+            ans = Math.min(ans, costs[0]+help(i+1, 0,days, costs));
+            
+            int newFreeTill = Math.min(days[i]+6, 365);
+            ans = Math.min(ans, costs[1]+help(i+1, newFreeTill, days, costs));
+            
+            newFreeTill = Math.min(days[i]+29, 365);
+            ans = Math.min(ans, costs[2]+help(i+1, newFreeTill, days, costs));
         }
+        memo[i][freeTill] = ans;
         return memo[i][freeTill];
     }
 }
