1e2c1253d7de1b4328b8be0049e4b61cc7e04c1b
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-11-30 12:05:08 +0530

    Time: 25 ms (64.72%), Space: 44 MB (90.80%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minInsertions(String s) {
+        StringBuilder reversed = new StringBuilder(s).reverse();
+        String s2 = reversed.toString();
+        return s.length() - longestCommonSubsequence(s, s2);
+    }
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[] t = new int[n+1];
+        
+        for(int l1=1; l1<=m; ++l1){
+            int[] temp = new int[n+1];
+            for(int l2=1; l2<=n; ++l2){
+                temp[l2] = text1.charAt(l1-1)==text2.charAt(l2-1) ? 1+t[l2-1] :
+                Math.max(temp[l2-1], t[l2]);
+            }
+            t = temp;
+        }
+        
+        return t[n];
+    }
+}
