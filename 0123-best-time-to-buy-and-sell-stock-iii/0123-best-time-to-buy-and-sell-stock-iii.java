b51ee180d0507d9a9bb397caded1716cd668948d
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-04 14:28:41 +0530

    Time: 182 ms (5.02%), Space: 109 MB (5.02%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,29 @@
+class Solution {
+    Integer[][][] memo;
+    
+    public int maxProfit(int[] prices) {
+        int k = 2; // number of transactions
+        memo = new Integer[prices.length+1][2][k+1];
+        
+        return find(0, 1, k, prices); //buy==1 means to buy and 0 means to sell
+    }
+    
+    private int find(int i, int buy, int k, int[] prices){
+        if(i==prices.length || k==0)
+            return 0;
+        if(memo[i][buy][k] != null) return memo[i][buy][k];
+        
+        int buyOrSell = 0;
+        if(buy==1)
+            buyOrSell = find(i+1, 0, k, prices) - prices[i];
+        else
+            buyOrSell = find(i+1, 1, k-1, prices) + prices[i];
+
+        
+        int hodl = find(i+1, buy, k, prices);
+        
+        memo[i][buy][k] = Math.max(buyOrSell, hodl);
+        
+        return memo[i][buy][k];
+    }
+}
