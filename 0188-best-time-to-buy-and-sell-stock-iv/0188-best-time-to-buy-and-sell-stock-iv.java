525460dab16bd660549a9f869dd7718c79bce739
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-04 17:23:33 +0530

    Time: 7 ms (43.19%), Space: 43.7 MB (27.94%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,18 +1,24 @@
+///////////////////////////////////// base conditions in side for loops
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
 
         int[][][] dp = new int[n + 1][2][k + 1];
 
-        for (int kk = 0; kk <= k; ++kk) {
-            dp[0][0][kk] = 0;
-            dp[0][1][kk] = Integer.MIN_VALUE; // Set an initial value for not holding any stock
-        }
-
-        for (int i = 1; i <= n; ++i) {
-            for (int kk = 1; kk <= k; ++kk) {
-                dp[i][0][kk] = Math.max(dp[i - 1][0][kk], dp[i - 1][1][kk] + prices[i - 1]);
-                dp[i][1][kk] = Math.max(dp[i - 1][1][kk], dp[i - 1][0][kk - 1] - prices[i - 1]);
+        for (int i = 0; i <= n; ++i) {
+            for (int kk = 0; kk <= k; ++kk) {
+                if(i==0){
+                    dp[0][0][kk] = 0;
+                    dp[0][1][kk] = Integer.MIN_VALUE;
+                }
+                else if(kk==0){
+                    dp[0][0][kk] = 0;
+                    dp[0][1][kk] = 0;
+                }
+                else{
+                    dp[i][0][kk] = Math.max(dp[i - 1][0][kk], dp[i - 1][1][kk] + prices[i - 1]);
+                    dp[i][1][kk] = Math.max(dp[i - 1][1][kk], dp[i - 1][0][kk - 1] - prices[i - 1]);
+                }
             }
         }
 
@@ -20,6 +26,30 @@ class Solution {
     }
 }
 
+
+///////////////////////////////////// base conditions out side for loops
+//
+// class Solution {
+//     public int maxProfit(int k, int[] prices) {
+//         int n = prices.length;
+
+//         int[][][] dp = new int[n + 1][2][k + 1];
+
+//         for (int kk = 0; kk <= k; ++kk) {
+//             dp[0][0][kk] = 0;
+//             dp[0][1][kk] = Integer.MIN_VALUE; // Set an initial value for not holding any stock
+//         }
+
+//         for (int i = 1; i <= n; ++i) {
+//             for (int kk = 1; kk <= k; ++kk) {
+//                 dp[i][0][kk] = Math.max(dp[i - 1][0][kk], dp[i - 1][1][kk] + prices[i - 1]);
+//                 dp[i][1][kk] = Math.max(dp[i - 1][1][kk], dp[i - 1][0][kk - 1] - prices[i - 1]);
+//             }
+//         }
+
+//         return dp[n][0][k]; // Maximum profit when not holding any stock after all transactions
+//     }
+// }
 
 // class Solution {
 //     Integer[][][] memo;
