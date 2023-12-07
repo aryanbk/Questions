51f89f6ba89e638808948ddfa5151982f5db65d1
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-07 16:35:22 +0530

    Time: 6 ms (71.07%), Space: 54.3 MB (64.40%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,37 +1,57 @@
-// modified first attempt
-//
 class Solution {
     public int countSquares(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-//         int[][][] map = new int[n][n][2];
-        
-//         for(int i=0; i<n; ++i){
-//             for(int j=0; j<n; ++j){
-//                 if(mat[i][j] != 0){
-//                     map[i][j][0] = j>0 && map[i][j-1][0] > 0 ? map[i][j-1][0] + 1 : mat[i][j];
-//                     map[i][j][1] = i>0 && map[i-1][j][1] > 0 ? map[i-1][j][1] + 1 : mat[i][j];
-//                 }
-//             }
-//         }
-
         int ans = 0;
+        
         for(int i=0; i<n; ++i){ 
             for(int j=0; j<m; ++j){
                 if(mat[i][j] == 1 && i>0 && j>0){
-                    // mat[i][j] = Math.min(mat[i-1][j-1] + 1, Math.min(map[i][j][0], map[i][j][1]));
                     mat[i][j] = Math.min(Math.min(mat[i-1][j], mat[i][j-1]), mat[i-1][j-1]) + 1;
                 }
-                // ans = Math.max(ans, mat[i][j]*mat[i][j]);
                 ans += mat[i][j];
             }
-            // System.out.println(Arrays.toString(mat[i]));
         }
         
         return ans;
-        
     }
 }
+
+
+// // modified first attempt
+// //
+// class Solution {
+//     public int countSquares(int[][] mat) {
+//         int n = mat.length;
+//         int m = mat[0].length;
+// //         int[][][] map = new int[n][n][2];
+        
+// //         for(int i=0; i<n; ++i){
+// //             for(int j=0; j<n; ++j){
+// //                 if(mat[i][j] != 0){
+// //                     map[i][j][0] = j>0 && map[i][j-1][0] > 0 ? map[i][j-1][0] + 1 : mat[i][j];
+// //                     map[i][j][1] = i>0 && map[i-1][j][1] > 0 ? map[i-1][j][1] + 1 : mat[i][j];
+// //                 }
+// //             }
+// //         }
+
+//         int ans = 0;
+//         for(int i=0; i<n; ++i){ 
+//             for(int j=0; j<m; ++j){
+//                 if(mat[i][j] == 1 && i>0 && j>0){
+//                     // mat[i][j] = Math.min(mat[i-1][j-1] + 1, Math.min(map[i][j][0], map[i][j][1]));
+//                     mat[i][j] = Math.min(Math.min(mat[i-1][j], mat[i][j-1]), mat[i-1][j-1]) + 1;
+//                 }
+//                 // ans = Math.max(ans, mat[i][j]*mat[i][j]);
+//                 ans += mat[i][j];
+//             }
+//             // System.out.println(Arrays.toString(mat[i]));
+//         }
+        
+//         return ans;
+        
+//     }
+// }
 
 
 // // first attempt
