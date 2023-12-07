6526cd552333526c684f078f3b7bcd8586b23c75
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-07 19:18:47 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (35.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,27 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        dp[0] = dp[1] = 1;
-        for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        int prev = 1;
+        int cur = 1;
+        for(int i=2; i<=n; ++i){
+            int temp = cur;
+            cur = prev + cur;
+            prev = temp;
         }
-        return dp[n];
+        return cur;
     }
 }
+
+
+// class Solution {
+//     public int climbStairs(int n) {
+//         int[] dp = new int[n+1];
+//         dp[0] = dp[1] = 1;
+//         for(int i=2; i<=n; i++){
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// }
 
 
 // class Solution {
