de5da0c5dc1b888daebe2d2eb63c490e6bb3a1be
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-09 13:59:46 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (10.09%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,13 +13,35 @@
  *     }
  * }
  */
+
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root == null) return new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
-        ans.addAll(inorderTraversal(root.left));
-        ans.add(root.val);
-        ans.addAll(inorderTraversal(root.right));
-        return ans;
+        var ans = new ArrayList<Integer>();
+        var stk = new Stack<TreeNode>();
+        while(true){
+            if(root != null){
+                stk.push(root);
+                root = root.left;
+            }else{
+                if(stk.isEmpty())
+                    return ans;
+                root = stk.pop();
+                ans.add(root.val);
+                root = root.right;
+            }
+        }
     }
 }
+
+
+
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         if(root == null) return new ArrayList<>();
+//         List<Integer> ans = new ArrayList<>();
+//         ans.addAll(inorderTraversal(root.left));
+//         ans.add(root.val);
+//         ans.addAll(inorderTraversal(root.right));
+//         return ans;
+//     }
+// }
