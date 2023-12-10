ece435d0830da20df09e413aa74e97b9a42f1c8d
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-10 18:48:14 +0530

    Time: 14 ms (93.88%), Space: 40 MB (52.04%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -1,28 +1,48 @@
 class Solution {
-    int[] memo;
     public boolean winnerSquareGame(int n) {
-        memo = new int[n+1];
-        Arrays.fill(memo, -1);
+        int[] dp = new int[n+1];
         
-        return solve(n) == 1;
-    }
-    private int solve(int n){
-        if(n<=0) return 0; // method returns 0 if curr player looses
-        
-        if(memo[n] != -1) return memo[n];
-        
-        for(int i=1; i*i<=n; ++i){
-            if(solve(n- i*i) == 0){
-                memo[n] = 1;
-                return 1; // method returns 1 if the other plater looses
+        for(int i=1; i<=n; ++i){
+            for(int j=1; j*j<=i; ++j){
+                if(dp[i - j*j] == 0){
+                    dp[i] = 1;
+                    break;
+                }
             }
         }
-        memo[n] = 0;
-        return 0;
+        return dp[n]==1;
     }
 }
 
 
+// // dp - memo
+//
+// class Solution {
+//     int[] memo;
+//     public boolean winnerSquareGame(int n) {
+//         memo = new int[n+1];
+//         Arrays.fill(memo, -1);
+        
+//         return solve(n) == 1;
+//     }
+//     private int solve(int n){
+//         if(n<=0) return 0; // method returns 0 if curr player looses
+        
+//         if(memo[n] != -1) return memo[n];
+        
+//         for(int i=1; i*i<=n; ++i){
+//             if(solve(n- i*i) == 0){
+//                 memo[n] = 1;
+//                 return 1; // method returns 1 if the other plater looses
+//             }
+//         }
+//         memo[n] = 0;
+//         return 0;
+//     }
+// }
+
+// // recurssion
+//
 // class Solution {
 //     public boolean winnerSquareGame(int n) {
 //         return solve(n) == 1;
