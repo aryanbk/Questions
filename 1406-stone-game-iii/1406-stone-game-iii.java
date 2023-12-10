48295bd6d2253169d851aa81017daa66006264d4
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-10 17:31:58 +0530

    Time: 10 ms (77.88%), Space: 55.1 MB (77.88%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,28 +1,74 @@
+// DP - tabulation 
+// O(n)
+// O(n)
+
 class Solution {
-    
-    int[] memo;
+    int[] dp;
     public String stoneGameIII(int[] val) {
-        memo = new int[val.length];
-        Arrays.fill(memo, -1);
-        int diff = solve(0, val);
-        return diff>0 ? "Alice" : (diff<0 ? "Bob" : "Tie");
-    }
-    
-    private int solve(int i, int[] piles){
-        if(i>=piles.length)
-            return 0;
+        int n = val.length;
+        dp = new int[n+1];
         
-        if(memo[i] != -1) return memo[i];
-        
-        int sumt = 0;
-        int ans = -1000000;
-        
-        for(int j=i; j<i+3 && j<piles.length; ++j){
-            sumt += piles[j];
-            ans = Math.max(ans, sumt - solve(j+1, piles));
+        for(int i=n-1; i>=0; --i){
+            int sumt = 0;
+            dp[i] = -1000000;
+            
+            for(int j=i; j<i+3 && j<n; ++j){
+                sumt += val[j];
+                dp[i] = Math.max(dp[i], sumt - dp[j+1]);
+            }
         }
         
-        memo[i] = ans;
-        return ans;
+        return dp[0]>0 ? "Alice" : (dp[0]<0 ? "Bob" : "Tie");
     }
+    
+//     private int solve(int i, int[] piles){
+//         if(i>=piles.length)
+//             return 0;
+        
+//         if(memo[i] != -1) return memo[i];
+        
+//         int sumt = 0;
+//         int ans = -1000000;
+        
+//         for(int j=i; j<i+3 && j<piles.length; ++j){
+//             sumt += piles[j];
+//             ans = Math.max(ans, sumt - solve(j+1, piles));
+//         }
+        
+//         memo[i] = ans;
+//         return ans;
+//     }
 }
+
+
+// // DP - memo 
+// // O(n)
+// // O(n)
+// class Solution {
+    
+//     int[] memo;
+//     public String stoneGameIII(int[] val) {
+//         memo = new int[val.length];
+//         Arrays.fill(memo, -1);
+//         int diff = solve(0, val);
+//         return diff>0 ? "Alice" : (diff<0 ? "Bob" : "Tie");
+//     }
+    
+//     private int solve(int i, int[] piles){
+//         if(i>=piles.length)
+//             return 0;
+        
+//         if(memo[i] != -1) return memo[i];
+        
+//         int sumt = 0;
+//         int ans = -1000000;
+        
+//         for(int j=i; j<i+3 && j<piles.length; ++j){
+//             sumt += piles[j];
+//             ans = Math.max(ans, sumt - solve(j+1, piles));
+//         }
+        
+//         memo[i] = ans;
+//         return ans;
+//     }
+// }
