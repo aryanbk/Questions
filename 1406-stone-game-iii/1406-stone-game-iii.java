0f675315f8be3c70e248ec9453450beabc3a254a
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-10 17:32:21 +0530

    Time: 10 ms (77.88%), Space: 55.2 MB (77.88%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -20,24 +20,6 @@ class Solution {
         
         return dp[0]>0 ? "Alice" : (dp[0]<0 ? "Bob" : "Tie");
     }
-    
-//     private int solve(int i, int[] piles){
-//         if(i>=piles.length)
-//             return 0;
-        
-//         if(memo[i] != -1) return memo[i];
-        
-//         int sumt = 0;
-//         int ans = -1000000;
-        
-//         for(int j=i; j<i+3 && j<piles.length; ++j){
-//             sumt += piles[j];
-//             ans = Math.max(ans, sumt - solve(j+1, piles));
-//         }
-        
-//         memo[i] = ans;
-//         return ans;
-//     }
 }
 
 
