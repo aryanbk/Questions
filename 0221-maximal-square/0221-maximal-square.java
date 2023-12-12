ea9f6222102ff08b8f7fb3270e8e5aecf60a71b9
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-12 17:15:02 +0530

    Time: 5 ms (97.90%), Space: 54.2 MB (43.20%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.java
@@ -0,0 +1,70 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int[][] mat = new int[m][n];
+        for(int i=0; i<m; ++i){
+            for(int j=0; j<n; ++j){
+                mat[i][j] = matrix[i][j] == '1' ? 1 : 0;
+            }
+        }
+        
+        int ans = 0;
+        
+        for(int i=0; i<m; ++i){
+            for(int j=0; j<n; ++j){
+                if(i>0 && j>0 && mat[i][j]==1){
+                    int up = mat[i-1][j];
+                    int left = mat[i][j-1];
+                    int diag = mat[i-1][j-1];
+                    mat[i][j] = Math.min(up, Math.min(left, diag)) + 1;
+                }
+                ans = Math.max(ans, mat[i][j]);
+            }
+        }
+        
+        return ans*ans;
+    }
+}
+
+
+// // wont run if when the squre is more than 9
+// // char wont store '10'
+
+// class Solution {
+//     public int maximalSquare(char[][] mat) {
+        
+//         int m = mat.length;
+//         int n = mat[0].length;
+//         // int m = matrix.length;
+//         // int n = matrix[0].length;
+        
+        
+        
+// //         int[][] mat = new int[m][n];
+// //         for(int i=0; i<m; ++i){
+// //             for(int j=0; j<n; ++j){
+// //                 mat[i][j] = matrix[i][j] == '1' ? 1 : 0;
+// //             }
+// //         }
+        
+//         int ans = 0;
+
+//         for(int i=0; i<m; ++i){
+//             for(int j=0; j<n; ++j){
+//                 if(i>0 && j>0 && mat[i][j]=='1'){
+//                     int up = mat[i-1][j] - '0';
+//                     int left = mat[i][j-1] - '0';
+//                     int diag = mat[i-1][j-1] - '0';
+//                     mat[i][j] = (char)(Math.min(up, Math.min(left, diag)) + '1');
+//                 }
+//                 ans = Math.max(ans, mat[i][j]-'0');
+//             }
+//         }
+//         for(char[] c: mat){
+//             System.out.println(Arrays.toString(c));
+//         }
+//         return ans*ans;
+//     }
+// }
