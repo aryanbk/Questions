94c0cdbe2f34ac579b64a7e2f08a5ad01e28743a
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-13 14:07:07 +0530

    Time: 11 ms (59.01%), Space: 45.1 MB (44.14%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -1,21 +1,42 @@
 class Solution {
-    int[][] memo;
     public int maxSumDivThree(int[] nums) {
-        memo = new int[nums.length+1][3];
-        for(int[] row: memo) Arrays.fill(row, -1);
-        return help(nums.length, 0,nums);
-    }
-    private int help(int i, int remainder, int[] nums){
-        if(i==0)
-            return remainder==0 ? 0 : -100000000;
+        int n = nums.length;
+        int[][] dp = new int[n+1][3];
+        dp[0] = new int[]{0, -1000000000, -1000000000};
         
-        if(memo[i][remainder] != -1) return memo[i][remainder];
+        for(int i=1; i<n+1; ++i){
+            for(int j=0; j<3; ++j){
+                dp[i][j] = Math.max(dp[i-1][j], nums[i-1] + dp[i-1][(j+nums[i-1])%3]);
+            }
+        }
         
-        memo[i][remainder] = Math.max(help(i-1, remainder, nums), 
-                                      nums[i-1]+help(i-1, (remainder+nums[i-1])%3, nums));
-        return memo[i][remainder];
+        return dp[n][0];
     }
 }
+
+
+// // Memo
+// // O(n*3)
+// // O(n*3)
+//
+// class Solution {
+//     int[][] memo;
+//     public int maxSumDivThree(int[] nums) {
+//         memo = new int[nums.length+1][3];
+//         for(int[] row: memo) Arrays.fill(row, -1);
+//         return help(nums.length, 0,nums);
+//     }
+//     private int help(int i, int remainder, int[] nums){
+//         if(i==0)
+//             return remainder==0 ? 0 : -100000000;
+        
+//         if(memo[i][remainder] != -1) return memo[i][remainder];
+        
+//         memo[i][remainder] = Math.max(help(i-1, remainder, nums), 
+//                                       nums[i-1]+help(i-1, (remainder+nums[i-1])%3, nums));
+//         return memo[i][remainder];
+//     }
+// }
 
 
 // // memo 
