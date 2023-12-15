059d8c40fe22902677cbb922cfcf9d129b7dd4a0
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-15 11:59:49 +0530

    Time: 1 ms (100.00%), Space: 42.6 MB (85.46%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        // Map<String, String> adj = new HashMap<>();
+        // Map<String, Boolean> outDeg = new HashMap<>();
+        var set = new HashSet<String>();
+        for(var p : paths){
+            // adj.put(p.get(0), p.get(1));
+            // outDeg(p.get(0), false);
+            set.add(p.get(0));
+        }
+        // for(Str)
+        for(var p: paths){
+            if(!set.contains(p.get(1))){
+                return p.get(1);
+            }
+        }
+        return "";
+        
+    }
+}
