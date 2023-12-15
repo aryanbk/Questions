a489309deee0fa16bd18ee1ce12ed2b459cab7ad
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-15 12:01:32 +0530

    Time: 2 ms (79.52%), Space: 43 MB (49.78%) - LeetHub

--- 1436-destination-city/1436-destination-city.java
+++ 1436-destination-city/1436-destination-city.java
@@ -1,20 +1,37 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
-        // Map<String, String> adj = new HashMap<>();
-        // Map<String, Boolean> outDeg = new HashMap<>();
         var set = new HashSet<String>();
-        for(var p : paths){
-            // adj.put(p.get(0), p.get(1));
-            // outDeg(p.get(0), false);
-            set.add(p.get(0));
-        }
-        // for(Str)
-        for(var p: paths){
-            if(!set.contains(p.get(1))){
-                return p.get(1);
-            }
-        }
-        return "";
+        for(var p : paths) set.add(p.get(0));
         
+        for(var p: paths){
+            if(!set.contains(p.get(1))) return p.get(1);
+        }
+        
+        return "";
     }
 }
+
+
+
+// // first attempt
+//
+// class Solution {
+//     public String destCity(List<List<String>> paths) {
+//         // Map<String, String> adj = new HashMap<>();
+//         // Map<String, Boolean> outDeg = new HashMap<>();
+//         var set = new HashSet<String>();
+//         for(var p : paths){
+//             // adj.put(p.get(0), p.get(1));
+//             // outDeg(p.get(0), false);
+//             set.add(p.get(0));
+//         }
+//         // for(Str)
+//         for(var p: paths){
+//             if(!set.contains(p.get(1))){
+//                 return p.get(1);
+//             }
+//         }
+//         return "";
+        
+//     }
+// }
