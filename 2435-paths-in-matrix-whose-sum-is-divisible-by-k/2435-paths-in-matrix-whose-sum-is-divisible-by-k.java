12c833cb03903394abe301bcda006072e4e7d2f5
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-16 17:15:05 +0530

    Time: 64 ms (80.58%), Space: 76 MB (35.92%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -5,25 +5,57 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         
-        int[][][] dp = new int[m][n][kk];
+        int[][] dp = new int[n][kk];
 
         for(int i=m-1; i>=0; --i){
+            int[][] newDp = new int[n][kk];
+            
             for(int j=n-1; j>=0; --j){
                 for(int k=kk-1; k>=0; --k){
                     int newK = (k+grid[i][j])%kk;
                     if(i==m-1 && j==n-1)
-                        dp[i][j][k] = newK == 0 ? 1 : 0;
+                        newDp[j][k] = newK == 0 ? 1 : 0;
                     else{
-                        if(i<m-1) dp[i][j][k] = (dp[i][j][k] + dp[i+1][j][newK])%MOD;
-                        if(j<n-1) dp[i][j][k] = (dp[i][j][k] + dp[i][j+1][newK])%MOD;
+                        if(i<m-1) newDp[j][k] = (newDp[j][k] + dp[j][newK])%MOD;
+                        if(j<n-1) newDp[j][k] = (newDp[j][k] + newDp[j+1][newK])%MOD;
                     }
                 }
             }
+            dp = newDp;
         }
         
-        return dp[0][0][0];
+        return dp[0][0];
     }
 }
+
+
+
+// class Solution {
+//     int MOD = 1_000_000_007;
+    
+//     public int numberOfPaths(int[][] grid, int kk) {
+//         int m = grid.length;
+//         int n = grid[0].length;
+        
+//         int[][][] dp = new int[m][n][kk];
+
+//         for(int i=m-1; i>=0; --i){
+//             for(int j=n-1; j>=0; --j){
+//                 for(int k=kk-1; k>=0; --k){
+//                     int newK = (k+grid[i][j])%kk;
+//                     if(i==m-1 && j==n-1)
+//                         dp[i][j][k] = newK == 0 ? 1 : 0;
+//                     else{
+//                         if(i<m-1) dp[i][j][k] = (dp[i][j][k] + dp[i+1][j][newK])%MOD;
+//                         if(j<n-1) dp[i][j][k] = (dp[i][j][k] + dp[i][j+1][newK])%MOD;
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return dp[0][0][0];
+//     }
+// }
 
 
 // class Solution {
