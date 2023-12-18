2cbef62a2f81fe4ceb8e589846498bc781eddfa9
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-18 13:41:45 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (46.00%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,18 +1,38 @@
 class Solution {
-    Integer[][] memo;
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-        memo = new Integer[n][n];
-        return n%2==0 ? true : help(0, n-1, nums)>=0;
-    }
-    private int help(int i, int j, int[] nums){
-        if(i>j)
-            return 0;
-        if(memo[i][j] == null) 
-            memo[i][j] = Math.max(nums[i] - help(i+1, j, nums), nums[j] - help(i, j-1, nums));
-        return memo[i][j];
+        if(n%2==0 || n==1) return true; // WA #3
+        
+        int[][] dp = new int[n][n];
+        
+        for(int i=n-1; i>=0; --i){
+            for(int j=i; j<n; ++j){
+                dp[i][j] = -1000000000; // WA #2
+                if(i<n-1) dp[i][j] = Math.max(dp[i][j], nums[i]-dp[i+1][j]);
+                if(j>0) dp[i][j] = Math.max(dp[i][j], nums[j]-dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1] >= 0;
     }
 }
+
+// // memo
+//
+// class Solution {
+//     Integer[][] memo;
+//     public boolean predictTheWinner(int[] nums) {
+//         int n = nums.length;
+//         memo = new Integer[n][n];
+//         return n%2==0 ? true : help(0, n-1, nums)>=0;
+//     }
+//     private int help(int i, int j, int[] nums){
+//         if(i>j)
+//             return 0;
+//         if(memo[i][j] == null) 
+//             memo[i][j] = Math.max(nums[i] - help(i+1, j, nums), nums[j] - help(i, j-1, nums));
+//         return memo[i][j];
+//     }
+// }
 
 
 // // recurence
