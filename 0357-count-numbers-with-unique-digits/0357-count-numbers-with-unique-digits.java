d47230e23279f65a224d78479c919f4f01f958d0
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-18 13:19:07 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (64.21%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
@@ -1,12 +1,27 @@
 class Solution {
     public int countNumbersWithUniqueDigits(int n) {
-        if(n==0)
-            return 1;
-        
-        int ans = 9;
-        for(int i=0; i<n-1; ++i)
-            ans *= (9-i);
-        
-        return ans + countNumbersWithUniqueDigits(n-1);
+        int prev = 1;
+        for(int i=1; i<=n; ++i){
+            int ans = 9;
+            for(int x=0; x<i-1; ++x) ans *= (9-x);
+            prev += ans;
+        }
+        return prev;
     }
 }
+
+
+// // DP - memo
+//
+// class Solution {
+//     public int countNumbersWithUniqueDigits(int n) {
+//         if(n==0)
+//             return 1;
+        
+//         int ans = 9;
+//         for(int i=0; i<n-1; ++i)
+//             ans *= (9-i);
+        
+//         return ans + countNumbersWithUniqueDigits(n-1);
+//     }
+// }
