86740b922afe57a77aae8edbb59260515e2cce93
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-19 13:34:01 +0530

    Time: 8 ms (23.81%), Space: 44.8 MB (22.69%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.java
+++ 0661-image-smoother/0661-image-smoother.java
@@ -5,21 +5,33 @@ class Solution {
         m = img.length;
         n = img[0].length;
         
-        int[][] ans = new int[m][n];
         for(int i=0; i<m; ++i){
             for(int j=0; j<n; ++j){
-                ans[i][j] = getAvg(i, j, img);
+                int ans = getAvg(i, j, img);
+                ans = ans<<8;
+                img[i][j] = img[i][j] | ans;
             }
         }
-        return ans;
+
+
+        for(int i=0; i<m; ++i){
+            for(int j=0; j<n; ++j){
+                int ans = getAvg(i, j, img);
+                img[i][j] = img[i][j] >> 8;
+            }
+        }
+        
+        return img;
     }
     
     int getAvg(int i, int j, int[][] img){
         int sumt = 0;
         int cnt = 0;
+        int mask = (1<<8)-1;
+        
         for(int x=Math.max(i-1, 0); x<=Math.min(i+1, m-1); ++x){
             for(int y = Math.max(j-1, 0); y<=Math.min(j+1, n-1); ++y){
-                sumt += img[x][y];
+                sumt += img[x][y] & mask;
                 ++cnt;
             }
         }
