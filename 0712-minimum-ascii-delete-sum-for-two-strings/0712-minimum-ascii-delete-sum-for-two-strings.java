c49638e71716177ad0027320717a4fcb66384f7c
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-19 19:18:28 +0530

    Time: 24 ms (39.42%), Space: 44.3 MB (41.35%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -2,25 +2,27 @@ class Solution {
     public int minimumDeleteSum(String s1, String s2) {
         int n1 = s1.length();
         int n2 = s2.length();
-        int[][] dp = new int[n1+1][n2+1];
+        int[] dp = new int[n2+1];
         
         for(int i=0; i<=n1; ++i){
+            int[] t = new int[n2+1];
             for(int j=0; j<=n2; ++j){
                 if(i==0 && j==0) continue;
                 if(i==0){
-                    for(int x=0; x<j; ++x) dp[i][j] += s2.charAt(x);
+                    for(int x=0; x<j; ++x) t[j] += s2.charAt(x);
                     continue;
                 }
                 if(j==0){
-                    for(int x=0; x<i; ++x) dp[i][j] += s1.charAt(x);
+                    for(int x=0; x<i; ++x) t[j] += s1.charAt(x);
                     continue;
                 }
-                dp[i][j] = Math.min(s1.charAt(i-1)+dp[i-1][j], s2.charAt(j-1)+dp[i][j-1]);
+                t[j] = Math.min(s1.charAt(i-1)+dp[j], s2.charAt(j-1)+t[j-1]);
                 if(s1.charAt(i-1)==s2.charAt(j-1))
-                    dp[i][j] = Math.min(dp[i][j], dp[i-1][j-1]);
+                    t[j] = Math.min(t[j], dp[j-1]);
             }
+            dp = t;
         }
         
-        return dp[n1][n2];
+        return dp[n2];
     }
 }
