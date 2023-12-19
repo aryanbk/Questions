d2deaf76adec7fd870cb9c92595c5873a62c752a
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-19 20:40:28 +0530

    Time: 5 ms (91.17%), Space: 47.3 MB (35.32%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
@@ -2,27 +2,55 @@ class Solution {
     public int maxTurbulenceSize(int[] arr) {
         int n = arr.length;
         int ans = 1;
-        int[][] dp = new int[n][2];
-        dp[0][0] = dp[0][1] = 1;
+        int pos = 1;
+        int neg = 1;
         
         for(int i=1; i<n; ++i){
+            int pos2 = 1;
+            int neg2 = 1;
             if(arr[i]>arr[i-1]){
-                dp[i][1] = dp[i-1][0] + 1;
-                dp[i][0] = 1;
-                ans = Math.max(ans, dp[i][1]);
+                pos2 += neg;
+                ans = Math.max(ans, pos2);
             }
             else if(arr[i]<arr[i-1]){
-                dp[i][0] = dp[i-1][1] + 1;
-                dp[i][1] = 1;
-                ans = Math.max(ans, dp[i][0]);
+                neg2 += pos;
+                ans = Math.max(ans, neg2);
             }
-            else{
-                dp[i] = new int[]{1, 1};
-            }
+            pos = pos2;
+            neg = neg2;
         }
         return ans;
     }
 }
+
+
+// // DP - first attempt
+//
+// class Solution {
+//     public int maxTurbulenceSize(int[] arr) {
+//         int n = arr.length;
+//         int ans = 1;
+//         int[][] dp = new int[n][2];
+//         dp[0][0] = dp[0][1] = 1;
+        
+//         for(int i=1; i<n; ++i){
+//             if(arr[i]>arr[i-1]){
+//                 dp[i][1] = dp[i-1][0] + 1;
+//                 dp[i][0] = 1;
+//                 ans = Math.max(ans, dp[i][1]);
+//             }
+//             else if(arr[i]<arr[i-1]){
+//                 dp[i][0] = dp[i-1][1] + 1;
+//                 dp[i][1] = 1;
+//                 ans = Math.max(ans, dp[i][0]);
+//             }
+//             else{
+//                 dp[i] = new int[]{1, 1};
+//             }
+//         }
+//         return ans;
+//     }
+// }
 
 
 // // No DP - sliding window
