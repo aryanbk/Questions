e7e61a3ffa907a8e1536b7f24af26b64d14c370c
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-19 20:25:41 +0530

    Time: 6 ms (63.48%), Space: 47.3 MB (35.32%) - LeetHub

---
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int maxTurbulenceSize(int[] arr) {
+        int n = arr.length;
+        if(n==1) return 1;
+        int ans = 1;
+        int l = 0;
+        
+        for(int r=1; r<n; ++r){
+            if(arr[r] == arr[r-1]){
+                l=r;
+            }
+            else if(r>1 && arr[r]>arr[r-1] == arr[r-1]>arr[r-2]){
+                l = r -1;
+            }
+            ans = Math.max(ans, r-l+1);
+        }
+        
+        return ans;
+    }
+}
+
+// // tried to contribute a testcase
+//
+// class Solution {
+//     public int maxTurbulenceSize(int[] arr) {
+//         int n = arr.length;
+//         if(n==1) return 1;
+//         if(n==2) return 1;
+//         int ans = 2;
+//         int l = 0;
+//         while(l+1<n && arr[l]==arr[l+1]) l++;
+//         boolean prev = arr[1]-arr[0] < 0;
+        
+//         for(int r=l+1; r<n; ++r){ // WA #2
+//             boolean curr = arr[r] > arr[r-1];
+//             if(prev == curr){
+//                 l = r-1;
+//             }
+//             if(arr[r]==arr[r-1]) l=r; // WA #1
+//             ans = Math.max(ans, r-l+1);
+//             prev = curr;
+//         }
+        
+//         return ans;
+//     }
+// }
