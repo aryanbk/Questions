3c6dac9dc5f7ab91d7d8f19a797c2d7a4d86f6ce
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-20 10:50:40 +0530

    Time: 548 ms (25.88%), Space: 70 MB (48.01%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.java
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.java
@@ -2,21 +2,46 @@ class Solution {
     public int longestArithSeqLength(int[] nums) {
         int n = nums.length;
         int ans = 0;
-        int[][] dp = new int[n][1001]; // diff can be negative, so will add 500 to diff
+        
+        Map<Integer, Integer>[] dp = new HashMap[n];
+        for (int i = 0; i < n; ++i) dp[i] = new HashMap<>();
         
         for(int i=0; i<n; ++i){
             for(int j=0; j<i; ++j){
+                int diff = nums[i] - nums[j];
                 
-                int diff = nums[i] - nums[j] + 500;
-                dp[i][diff] = Math.max(dp[i][diff], dp[j][diff]+1);
-                ans = Math.max(ans, dp[i][diff]);
-                
+                dp[i].put(diff, Math.max(dp[i].getOrDefault(diff, 0), 
+                                        dp[j].getOrDefault(diff, 0)+1));
+                ans = Math.max(ans, dp[i].get(diff));
             }
         }
         
         return ans+1;
     }
 }
+
+
+// // using 2d array
+//
+// class Solution {
+//     public int longestArithSeqLength(int[] nums) {
+//         int n = nums.length;
+//         int ans = 0;
+//         int[][] dp = new int[n][1001]; // diff can be negative, so will add 500 to diff
+        
+//         for(int i=0; i<n; ++i){
+//             for(int j=0; j<i; ++j){
+                
+//                 int diff = nums[i] - nums[j] + 500;
+//                 dp[i][diff] = Math.max(dp[i][diff], dp[j][diff]+1);
+//                 ans = Math.max(ans, dp[i][diff]);
+                
+//             }
+//         }
+        
+//         return ans+1;
+//     }
+// }
 
 
 // // first attempt
