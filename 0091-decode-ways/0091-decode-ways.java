c30b1695d1245c5ba59d5899a73b18d37122aae5
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-22 16:46:09 +0530

    Time: 2 ms (23.04%), Space: 41.6 MB (9.43%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,21 +1,39 @@
 class Solution {
-    int[] memo;
     public int numDecodings(String s) {
-        memo = new int[s.length() + 1];
-        Arrays.fill(memo, -1);
-        return help(s.length(), s);
-    }
-    int help(int i, String s){
-        if(i==0)
-            return 1;
+        int n = s.length();
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
         
-        if(memo[i] != -1) return memo[i];
+        for(int i=1; i<=n; ++i){
+            dp[i] = s.charAt(i-1)=='0' ? 0 : dp[i-1];
+            dp[i] += i>1 && s.charAt(i-2) != '0' && Integer.parseInt(s.substring(i-2, i)) <27
+                ? dp[i-2] : 0;
+        }
         
-        int ans = s.charAt(i-1) != '0' ? help(i-1, s) : 0;
-        ans += i>1 && s.charAt(i-2) != '0' && Integer.parseInt(s.substring(i-2, i)) < 27 ?
-            help(i-2, s) : 0;
-        
-        memo[i] = ans;
-        return ans;
+        return dp[n];
     }
 }
+
+// // DP - memo
+//
+// class Solution {
+//     int[] memo;
+//     public int numDecodings(String s) {
+//         memo = new int[s.length() + 1];
+//         Arrays.fill(memo, -1);
+//         return help(s.length(), s);
+//     }
+//     int help(int i, String s){
+//         if(i==0)
+//             return 1;
+        
+//         if(memo[i] != -1) return memo[i];
+        
+//         int ans = s.charAt(i-1) != '0' ? help(i-1, s) : 0;
+//         ans += i>1 && s.charAt(i-2) != '0' && Integer.parseInt(s.substring(i-2, i)) < 27 ?
+//             help(i-2, s) : 0;
+        
+//         memo[i] = ans;
+//         return ans;
+//     }
+// }
