c8ccffe9b8092313c1acce92ee760d1b196ea3f3
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-25 11:13:57 +0530

    Time: 1 ms (84.19%), Space: 41.7 MB (11.16%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,18 +1,43 @@
+// // DP - Space optimized
+// // O(n) O(1)
+//
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
-        int[] dp = new int[s.length() + 1];
-        dp[0] = 1;
+        int cur = 1;
+        int prev = 0;
         
         for(int i=1; i<=n; ++i){
-            dp[i] = s.charAt(i-1)=='0' ? 0 : dp[i-1];
-            dp[i] += i>1 && s.charAt(i-2) != '0' && Integer.parseInt(s.substring(i-2, i)) <27
-                ? dp[i-2] : 0;
+            int newCur = s.charAt(i-1)=='0' ? 0 : cur;
+            newCur += i>1 && s.charAt(i-2) != '0' && Integer.parseInt(s.substring(i-2, i)) <27
+                ? prev : 0;
+            prev = cur;
+            cur = newCur;
         }
         
-        return dp[n];
+        return cur;
     }
 }
+
+
+// // DP - tabulation
+// // O(n) O(n)
+//
+// class Solution {
+//     public int numDecodings(String s) {
+//         int n = s.length();
+//         int[] dp = new int[s.length() + 1];
+//         dp[0] = 1;
+        
+//         for(int i=1; i<=n; ++i){
+//             dp[i] = s.charAt(i-1)=='0' ? 0 : dp[i-1];
+//             dp[i] += i>1 && s.charAt(i-2) != '0' && Integer.parseInt(s.substring(i-2, i)) <27
+//                 ? dp[i-2] : 0;
+//         }
+        
+//         return dp[n];
+//     }
+// }
 
 // // DP - memo
 //
