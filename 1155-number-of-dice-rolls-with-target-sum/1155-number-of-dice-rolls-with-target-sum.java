05f17745a26e99f5039c55666d449f49b558a2fd
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-26 12:34:48 +0530

    Time: 23 ms (26.51%), Space: 41.7 MB (26.87%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,19 +1,45 @@
 class Solution {
-    int MOD = 1_000_000_007;
-    Integer[][] memo = new Integer[31][1001];
     public int numRollsToTarget(int n, int k, int target) {
-        if(n==0) return target==0 ? 1 : 0;
+        int MOD = 1_000_000_007;
+        int[][] dp = new int[n+1][target+1];
+        dp[0][0] = 1;
         
-        if(memo[n][target] != null) return memo[n][target];
-        
-        memo[n][target] = 0;
-        
-        for(int x=1; x<=k && x<=target; ++x) 
-            memo[n][target] = (memo[n][target] + numRollsToTarget(n-1, k, target-x))%MOD;
-        
-        return memo[n][target];
+        for(int i=0; i<=n; ++i){
+            for(int j=0; j<=target; ++j){
+                if(i==0){
+                    dp[i][j] = j==0 ? 1 : 0;
+                    continue;
+                }
+                for(int x=1; x<=k && x<=j; ++x)
+                    dp[i][j] = (dp[i][j] + dp[i-1][j-x])%MOD;
+            }
+        }
+        return dp[n][target];
     }
 }
+
+
+
+// // DP - memo
+// // O(n*target*k)
+// // O(n*target)
+//
+// class Solution {
+//     int MOD = 1_000_000_007;
+//     Integer[][] memo = new Integer[31][1001];
+//     public int numRollsToTarget(int n, int k, int target) {
+//         if(n==0) return target==0 ? 1 : 0;
+        
+//         if(memo[n][target] != null) return memo[n][target];
+        
+//         memo[n][target] = 0;
+        
+//         for(int x=1; x<=k && x<=target; ++x) 
+//             memo[n][target] = (memo[n][target] + numRollsToTarget(n-1, k, target-x))%MOD;
+        
+//         return memo[n][target];
+//     }
+// }
 
 
 // // Recursion
