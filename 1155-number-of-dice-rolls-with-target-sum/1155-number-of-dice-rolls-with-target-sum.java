8072fc6e129f83d7971b9f84455d4acbcdbb195a
Aryan K <101513901+aryanbk@users.noreply.github.com>
2023-12-26 12:37:06 +0530

    Time: 23 ms (26.51%), Space: 41.6 MB (29.72%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -4,12 +4,8 @@ class Solution {
         int[][] dp = new int[n+1][target+1];
         dp[0][0] = 1;
         
-        for(int i=0; i<=n; ++i){
-            for(int j=0; j<=target; ++j){
-                if(i==0){
-                    dp[i][j] = j==0 ? 1 : 0;
-                    continue;
-                }
+        for(int i=1; i<=n; ++i){
+            for(int j=1; j<=target; ++j){
                 for(int x=1; x<=k && x<=j; ++x)
                     dp[i][j] = (dp[i][j] + dp[i-1][j-x])%MOD;
             }
