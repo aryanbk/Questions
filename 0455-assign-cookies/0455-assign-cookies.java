4caa59aca76b441d03edff0261d05abcd365d813
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-01-01 23:43:28 +0530

    Time: 9 ms (44.73%), Space: 45.4 MB (9.19%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -2,18 +2,36 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        
-        int i = g.length-1;
-        int j = s.length-1;
         int ans = 0;
         
-        while(i>=0 && j>=0){
-            if(g[i] <= s[j]){
+        for(int i = g.length-1, j = s.length -1; i>=0 && j>=0; --i){
+            if(s[j] >= g[i]){
                 ++ans;
                 --j;
             }
-            --i;
         }
+        
         return ans;
     }
 }
+
+
+// class Solution {
+//     public int findContentChildren(int[] g, int[] s) {
+//         Arrays.sort(g);
+//         Arrays.sort(s);
+        
+//         int i = g.length-1;
+//         int j = s.length-1;
+//         int ans = 0;
+        
+//         while(i>=0 && j>=0){
+//             if(g[i] <= s[j]){
+//                 ++ans;
+//                 --j;
+//             }
+//             --i;
+//         }
+//         return ans;
+//     }
+// }
