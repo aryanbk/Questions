921d8f9a26d5fffa4104d5de484d480493ae1905
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-01-02 11:01:56 +0530

    Time: 4 ms (55.08%), Space: 44.8 MB (26.06%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
@@ -2,27 +2,48 @@ class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
         int n = nums.length;
         int[] freq = new int[n+1];
-        
-        for(var num: nums)
-            freq[num]++;
-        
         var ans = new ArrayList<List<Integer>>();
         
-        for(int i=0; i<=n; ++i){
-            for(int j=0; j<freq[i]; ++j){
-                if(ans.size() > j)
-                    ans.get(j).add(i);
-                else{
-                    var sub = new ArrayList<Integer>();
-                    sub.add(i);
-                    ans.add(sub);  
-                }
-            }
+        for(var num: nums){
+            
+            if(freq[num] >= ans.size()) ans.add(new ArrayList<>());
+            
+            ans.get(freq[num]).add(num);
+            freq[num]++;
         }
         
         return ans;
     }
 }
+
+
+
+
+// class Solution {
+//     public List<List<Integer>> findMatrix(int[] nums) {
+//         int n = nums.length;
+//         int[] freq = new int[n+1];
+        
+//         for(var num: nums)
+//             freq[num]++;
+        
+//         var ans = new ArrayList<List<Integer>>();
+        
+//         for(int i=0; i<=n; ++i){
+//             for(int j=0; j<freq[i]; ++j){
+//                 if(ans.size() > j)
+//                     ans.get(j).add(i);
+//                 else{
+//                     var sub = new ArrayList<Integer>();
+//                     sub.add(i);
+//                     ans.add(sub);  
+//                 }
+//             }
+//         }
+        
+//         return ans;
+//     }
+// }
 
 
 // class Solution {
