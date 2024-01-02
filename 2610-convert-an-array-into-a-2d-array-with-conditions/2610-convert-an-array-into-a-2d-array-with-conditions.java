c417d67c398128f97be8fc836ccc1af65939a892
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-01-02 10:55:46 +0530

    Time: 6 ms (16.31%), Space: 44.4 MB (56.57%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,24 +1,50 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
-        var freq = new int[201];
+        int[] freq = new int[201];
         
-        for(var n: nums) freq[n]++;
+        for(var n: nums)
+            freq[n]++;
         
         var ans = new ArrayList<List<Integer>>();
         
-        while(true){
-            var sub = new ArrayList<Integer>();
-            for(int i=0; i<201; ++i){
-                if(freq[i]-- > 0) sub.add(i);
-            }    
-            
-            if(sub.size()==0) break;
-            ans.add(sub);
+        for(int i=0; i<201; ++i){
+            for(int j=0; j<freq[i]; ++j){
+                if(ans.size() > j)
+                    ans.get(j).add(i);
+                else{
+                    var sub = new ArrayList<Integer>();
+                    sub.add(i);
+                    ans.add(sub);  
+                }
+            }
         }
         
         return ans;
     }
 }
+
+
+// class Solution {
+//     public List<List<Integer>> findMatrix(int[] nums) {
+//         var freq = new int[201];
+        
+//         for(var n: nums) freq[n]++;
+        
+//         var ans = new ArrayList<List<Integer>>();
+        
+//         while(true){
+//             var sub = new ArrayList<Integer>();
+//             for(int i=0; i<201; ++i){
+//                 if(freq[i]-- > 0) sub.add(i);
+//             }    
+            
+//             if(sub.size()==0) break;
+//             ans.add(sub);
+//         }
+        
+//         return ans;
+//     }
+// }
 
 
 
