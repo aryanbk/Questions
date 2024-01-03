18caff1c409721811b0233a777e03a67b885af25
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-01-03 23:20:19 +0530

    Time: 26 ms (82.58%), Space: 68.2 MB (72.90%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.java
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.java
@@ -1,24 +1,50 @@
 class Solution {
     
-    // Map<List<Integer>, Integer> map;
     Integer[][] memo;
+    int end;
     int MOD = 1_000_000_007;
     
     public int numberOfWays(int startPos, int endPos, int k) {
-        memo = new Integer[4002][1001];
-        return help(startPos, endPos, k);
+        memo = new Integer[2*k + 2][k+1];
+        end = k + Math.abs(startPos - endPos);
+        return help(k, k);
     }
     
-    int help(int s, int e, int k){
-        if(k==0) return s==e ? 1 : 0;
-        if(Math.abs(s-e) > k) return 0; // TLE
+    int help(int s, int k){
+        if(k==0) return s==end ? 1 : 0;
+        if(Math.abs(s-end) > k) return 0; // TLE
         
-        if(memo[s+2000][k] == null)
-            memo[s+2000][k] = (help(s+1, e, k-1) + help(s-1, e, k-1))%MOD;
+        if(memo[s][k] == null)
+            memo[s][k] = (help(s+1, k-1) + help(s-1, k-1))%MOD;
         
-        return memo[s+2000][k];
+        return memo[s][k];
     }
 }
+
+
+
+// class Solution {
+//     Integer[][] memo;
+//     int MOD = 1_000_000_007;
+    
+//     public int numberOfWays(int startPos, int endPos, int k) {
+//         memo = new Integer[4002][1001];
+//         return help(startPos, endPos, k);
+//     }
+    
+//     int help(int s, int e, int k){
+//         if(k==0) return s==e ? 1 : 0;
+//         if(Math.abs(s-e) > k) return 0; // TLE
+        
+//         if(memo[s+2000][k] == null)
+//             memo[s+2000][k] = (help(s+1, e, k-1) + help(s-1, e, k-1))%MOD;
+        
+//         return memo[s+2000][k];
+//     }
+// }
+
+
+
 
 // // BFS - TLE
 //
