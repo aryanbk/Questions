383a8f12f5acdf08dafda8766630d5411dc30140
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-01-04 12:42:36 +0530

    Time: 4 ms (58.33%), Space: 40.9 MB (19.05%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.java
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.java
@@ -1,45 +1,48 @@
-class Solution {
-    public int peopleAwareOfSecret(int n, int delay, int forget) {
-        long[] dp = new long[n+1];
-        dp[1] = 1;
-        long active = 0;
-        int MOD = 1_000_000_007;
-        long ans = 0;
-        
-        for(int i=2; i<n+1; ++i){
-            dp[i] = active = (active + dp[Math.max(i-delay, 0)] - 
-                              dp[Math.max(i-forget, 0)] + MOD) % MOD;
-            
-            if(i>n-forget)
-                ans = (ans + dp[i]) % MOD;
-        
-        }
-        return n-forget > 0 ? (int)ans : (int)ans + 1;
-    }
-}
-
-
-
+// // bad code - less readable
+//
 // class Solution {
 //     public int peopleAwareOfSecret(int n, int delay, int forget) {
 //         long[] dp = new long[n+1];
 //         dp[1] = 1;
 //         long active = 0;
 //         int MOD = 1_000_000_007;
+//         long ans = 0;
         
 //         for(int i=2; i<n+1; ++i){
-//             active = (active + dp[Math.max(i-delay, 0)] + MOD) % MOD;
-//             active = (active - dp[Math.max(i-forget, 0)] + MOD) % MOD;
-//             dp[i] = active;
-//         }
+//             dp[i] = active = (active + dp[Math.max(i-delay, 0)] - 
+//                               dp[Math.max(i-forget, 0)] + MOD) % MOD;
+            
+//             if(i>n-forget)
+//                 ans = (ans + dp[i]) % MOD;
         
-//         long ans = 0;
-//         for(int i = n-forget+1; i<n+1; ++i){
-//             ans = (ans + dp[i])%MOD;
 //         }
-//         return (int)ans;
+//         return n-forget > 0 ? (int)ans : (int)ans + 1;
 //     }
 // }
+
+
+// // more readable
+//
+class Solution {
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+        long[] dp = new long[n+1];
+        dp[1] = 1;
+        long active = 0;
+        int MOD = 1_000_000_007;
+        
+        for(int i=2; i<n+1; ++i){
+            active = (active + dp[Math.max(i-delay, 0)] + MOD) % MOD;
+            active = (active - dp[Math.max(i-forget, 0)] + MOD) % MOD;
+            dp[i] = active;
+        }
+        
+        long ans = 0;
+        for(int i = n-forget+1; i<n+1; ++i){
+            ans = (ans + dp[i])%MOD;
+        }
+        return (int)ans;
+    }
+}
 
 
 // // works but gives TLE
