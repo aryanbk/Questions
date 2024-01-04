83b17277fdc416c49521b8863644b000524a9b83
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-01-04 15:50:04 +0530

    Time: 34 ms (88.78%), Space: 55.8 MB (65.42%) - LeetHub

--- 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.java
+++ 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.java
@@ -1,28 +1,68 @@
 class Solution {
     public int countDistinct(int[] nums, int k, int p) {
         int n = nums.length;
-        int div = 0;
+        Trie root = new Trie();
+        int ans = 0;
         
-        Set<List<Integer>> set = new HashSet<>();
-        List<Integer> list = new ArrayList<>();
-        for(int x: nums) list.add(x);
-        
-        for(int l=0, r=0; r<n; ++r){
-            div += list.get(r)%p == 0 ? 1 : 0;
+        for(int i=0; i<n; ++i){
             
-            while(l<n && div > k){
-                div -= list.get(l)%p == 0 ? 1 : 0;
-                l++;
+            int div = 0;
+            Trie tempRoot = root;
+            
+            for(int j=i; j<n; ++j){
+                if(nums[j] % p == 0) ++div;
+                if(div > k) break;
+                
+                if(!tempRoot.map.containsKey(nums[j])){
+                    tempRoot.map.put(nums[j], new Trie());
+                    ++ans;
+                }
+                tempRoot = tempRoot.map.get(nums[j]);
+                
             }
-            
-            for(int x=0; x<r-l+1; ++x)
-                set.add(list.subList(x+l, r+1));
-            
         }
         
-        return set.size();
+        return ans;
     }
 }
+
+
+class Trie {
+    Map<Integer, Trie> map;    
+    public Trie() {
+        map = new HashMap<>();
+    }
+}
+
+
+
+
+
+// class Solution {
+//     public int countDistinct(int[] nums, int k, int p) {
+//         int n = nums.length;
+//         int div = 0;
+        
+//         Set<List<Integer>> set = new HashSet<>();
+//         List<Integer> list = new ArrayList<>();
+//         for(int x: nums) list.add(x);
+        
+//         for(int l=0, r=0; r<n; ++r){
+//             div += list.get(r)%p == 0 ? 1 : 0;
+            
+//             while(l<n && div > k){
+//                 div -= list.get(l)%p == 0 ? 1 : 0;
+//                 l++;
+//             }
+            
+//             for(int x=0; x<r-l+1; ++x)
+//                 set.add(list.subList(x+l, r+1));
+            
+//         }
+        
+//         return set.size();
+//     }
+// }
 
 
 // // wont give correct answer
