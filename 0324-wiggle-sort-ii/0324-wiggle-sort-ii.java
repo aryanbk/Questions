5c33ad73197bd57c1b3b78a0312656a8a183ef02
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-01-06 16:26:47 +0530

    Time: 15 ms (16.81%), Space: 49 MB (5.32%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -1,26 +1,65 @@
+// // O(n)
+// // O(n)
+//
 class Solution {
     public void wiggleSort(int[] nums) {
         int n = nums.length;
-        Arrays.sort(nums);
+        int m = findKthLargest(nums, (n+1)/2); //median
+        int hi = 1;
+        int lo = n%2==0 ? n-2 : n-1;
         int[] ans = new int[n];
-        
-        int i = n-1;
-        int j = 1;
-        while(j<n){
-            ans[j] = nums[i--];
-            j+=2;
+        Arrays.fill(ans, -1);
+        for(var x: nums){
+            if(x < m){
+                ans[lo] = x;
+                lo-=2;
+            }else if (x > m) {
+                ans[hi] = x;
+                hi+=2;
+            }
         }
-        j = 0;
-        while(j<n){
-            ans[j] = nums[i--];
-            j+=2;
+        for(int i=0; i<n; ++i)
+            nums[i] = ans[i]==-1 ? m : ans[i];
+    }
+    
+    int findKthLargest(int[] nums, int k){
+        var pq = new PriorityQueue<Integer>();
+        for(var n: nums){
+            pq.offer(n);
+            if(pq.size() > k)
+                pq.poll();
         }
-        
-        for(int x=0; x<n; ++x)
-            nums[x] = ans[x];
-        
+        return pq.peek();
     }
 }
+
+
+// // O(n log n)
+// // O(n)
+//
+// class Solution {
+//     public void wiggleSort(int[] nums) {
+//         int n = nums.length;
+//         Arrays.sort(nums);
+//         int[] ans = new int[n];
+        
+//         int i = n-1;
+//         int j = 1;
+//         while(j<n){
+//             ans[j] = nums[i--];
+//             j+=2;
+//         }
+//         j = 0;
+//         while(j<n){
+//             ans[j] = nums[i--];
+//             j+=2;
+//         }
+        
+//         for(int x=0; x<n; ++x)
+//             nums[x] = ans[x];
+        
+//     }
+// }
 
 
 // // wrong approch - wont work
