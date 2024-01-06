e5ab8c78eff76b8f03e596c7577b28634b6439be
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-01-06 16:42:57 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (23.41%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -4,15 +4,10 @@ class Solution {
         int i=0;
         int j=n-1;
         for(int x=0; x<=j; ++x){
-            if(nums[x] < 1){
-                swap(nums, x, i);
-                i++;
-            }
-            else if(nums[x] > 1){
-                swap(nums, x, j);
-                j--;
-                x--;
-            }
+            if(nums[x] < 1)
+                swap(nums, x, i++);
+            else if(nums[x] > 1)
+                swap(nums, x--, j--);
         }
     }
     
@@ -24,6 +19,36 @@ class Solution {
 }
 
 
+
+// // O(n)
+//
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         int n = nums.length;
+//         int i=0;
+//         int j=n-1;
+//         for(int x=0; x<=j; ++x){
+//             if(nums[x] < 1){
+//                 swap(nums, x, i);
+//                 i++;
+//             }
+//             else if(nums[x] > 1){
+//                 swap(nums, x, j);
+//                 j--;
+//                 x--;
+//             }
+//         }
+//     }
+    
+//     void swap(int[] nums, int i, int j){
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+// }
+
+// // O(n log n)
+//
 // class Solution {
 //     public void sortColors(int[] nums) {
 //         Arrays.sort(nums);
