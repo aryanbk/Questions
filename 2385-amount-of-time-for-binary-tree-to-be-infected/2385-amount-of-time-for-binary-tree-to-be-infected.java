5502e4b085d9ac08500857f0799f3bdbc8799fa2
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-01-10 12:39:33 +0530

    Time: 94 ms (62.93%), Space: 97.9 MB (28.84%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
@@ -13,28 +13,26 @@
  *     }
  * }
  */
+
 class Solution {
-    Map<Integer, TreeNode> par;
-    int start;
-    TreeNode startNode;
+    Map<Integer, TreeNode[]> par;
     public int amountOfTime(TreeNode root, int start) {
         par = new HashMap<>();
-        this.start = start;
+        
         dfs(root, null);
-        return bfs(startNode);
+        return bfs(start);
     }
     
     void dfs(TreeNode root, TreeNode parent){
         if(root==null) return;
-        par.put(root.val, parent);
-        if(root.val == start) startNode = root;
+        par.put(root.val, new TreeNode[]{parent, root.left, root.right});
         dfs(root.left, root);
         dfs(root.right, root);
     }
     
-    int bfs(TreeNode start){
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(start);
+    int bfs(int start){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
         HashSet<Integer> vis = new HashSet<>();
         int lvl = 0;
         
@@ -42,15 +40,13 @@ class Solution {
             int l = q.size();
             
             for(int i=0; i<l; ++i){
-                TreeNode node = q.poll();
-                vis.add(node.val);
+                int node = q.poll();
+                vis.add(node);
                 
-                if(node.left != null && !vis.contains(node.left.val))
-                    q.offer(node.left);
-                if(node.right != null &&  !vis.contains(node.right.val))
-                    q.offer(node.right);
-                if(par.get(node.val) != null && !vis.contains(par.get(node.val).val))
-                    q.offer(par.get(node.val));
+                for(var nbr: par.get(node)){
+                    if(nbr != null && !vis.contains(nbr.val))
+                        q.offer(nbr.val);
+                }
             }
             ++lvl;
         }
@@ -58,3 +54,52 @@ class Solution {
         return lvl-1;
     }
 }
+
+
+
+
+// class Solution {
+//     Map<Integer, TreeNode> par;
+//     int start;
+//     TreeNode startNode;
+//     public int amountOfTime(TreeNode root, int start) {
+//         par = new HashMap<>();
+//         this.start = start;
+//         dfs(root, null);
+//         return bfs(startNode);
+//     }
+    
+//     void dfs(TreeNode root, TreeNode parent){
+//         if(root==null) return;
+//         par.put(root.val, parent);
+//         if(root.val == start) startNode = root;
+//         dfs(root.left, root);
+//         dfs(root.right, root);
+//     }
+    
+//     int bfs(TreeNode start){
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.add(start);
+//         HashSet<Integer> vis = new HashSet<>();
+//         int lvl = 0;
+        
+//         while(!q.isEmpty()){
+//             int l = q.size();
+            
+//             for(int i=0; i<l; ++i){
+//                 TreeNode node = q.poll();
+//                 vis.add(node.val);
+                
+//                 if(node.left != null && !vis.contains(node.left.val))
+//                     q.offer(node.left);
+//                 if(node.right != null &&  !vis.contains(node.right.val))
+//                     q.offer(node.right);
+//                 if(par.get(node.val) != null && !vis.contains(par.get(node.val).val))
+//                     q.offer(par.get(node.val));
+//             }
+//             ++lvl;
+//         }
+        
+//         return lvl-1;
+//     }
+// }
