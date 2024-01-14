a10cb6cea250b7eb3952f4d83d7400d2d854a234
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-01-15 00:16:45 +0530

    Time: 20 ms (56.80%), Space: 45.8 MB (14.80%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -2,22 +2,47 @@ class Solution {
     public boolean closeStrings(String word1, String word2) {
         if(word1.length() != word2.length()) return false;
         
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
+        int[] freq1 = new int[26];
+        int[] set1 = new int[26];
+        int[] freq2 = new int[26];
+        int[] set2 = new int[26];
         
         for(int i=0; i<word1.length(); ++i){
-            char ch1 = word1.charAt(i);
-            map1.put(ch1, map1.getOrDefault(ch1, 0)+1);
-            char ch2 = word2.charAt(i);
-            map2.put(ch2, map2.getOrDefault(ch2, 0)+1);
+            freq1[word1.charAt(i)-'a']++;
+            set1[word1.charAt(i)-'a'] = 1;
+
+            freq2[word2.charAt(i)-'a']++;
+            set2[word2.charAt(i)-'a'] = 1;
         }
-        var list1 = new ArrayList<>(map1.values());
-        var list2 = new ArrayList<>(map2.values());
         
-        Collections.sort(list1);
-        Collections.sort(list2);
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
         
-        return  list1.equals(list2) && map1.keySet().equals(map2.keySet());
-        
+        return Arrays.equals(freq1, freq2) && Arrays.equals(set1, set2);
     }
 }
+
+
+// class Solution {
+//     public boolean closeStrings(String word1, String word2) {
+//         if(word1.length() != word2.length()) return false;
+        
+//         Map<Character, Integer> map1 = new HashMap<>();
+//         Map<Character, Integer> map2 = new HashMap<>();
+        
+//         for(int i=0; i<word1.length(); ++i){
+//             char ch1 = word1.charAt(i);
+//             char ch2 = word2.charAt(i);
+//             map1.put(ch1, map1.getOrDefault(ch1, 0)+1);
+//             map2.put(ch2, map2.getOrDefault(ch2, 0)+1);
+//         }
+//         var list1 = new ArrayList<>(map1.values());
+//         var list2 = new ArrayList<>(map2.values());
+        
+//         Collections.sort(list1);
+//         Collections.sort(list2);
+        
+//         return  list1.equals(list2) && map1.keySet().equals(map2.keySet());
+        
+//     }
+// }
