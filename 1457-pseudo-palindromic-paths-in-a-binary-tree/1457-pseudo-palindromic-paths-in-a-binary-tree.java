cb14e8e95eb73c98366dff520a96cbb6f43f6aac
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-01-24 16:36:24 +0530

    Time: 13 ms (68.33%), Space: 70.1 MB (46.97%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -13,26 +13,53 @@
  *     }
  * }
  */
+
+
 class Solution {
     public int pseudoPalindromicPaths (TreeNode root) {
-        return dfs(root, new HashMap<>());
+        return dfs(root, new boolean[10]);
     }
-    int dfs(TreeNode root, Map<Integer, Integer> map){
+    int dfs(TreeNode root, boolean[] map){
         if(root.left==null && root.right==null){
-            map.put(root.val, map.getOrDefault(root.val, 0)+1);
-            // System.out.println(map);
+            map[root.val] ^= true;
             int odd = 0;
-            for(int key: map.keySet()){
-                odd += map.get(key)%2;
-            }
-            map.put(root.val, map.get(root.val)-1);
+            for(var m: map) odd += m ? 1 : 0;
+            map[root.val] ^= true;
             return odd < 2 ? 1 : 0;
         }
-        map.put(root.val, map.getOrDefault(root.val, 0)+1);
+        map[root.val] ^= true;
         int ans = 0;
         if(root.left != null) ans += dfs(root.left, map);
         if(root.right != null) ans += dfs(root.right, map);
-        map.put(root.val, map.get(root.val)-1);
+        map[root.val] ^= true;
         return ans;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public int pseudoPalindromicPaths (TreeNode root) {
+//         return dfs(root, new HashMap<>());
+//     }
+//     int dfs(TreeNode root, Map<Integer, Integer> map){
+//         if(root.left==null && root.right==null){
+//             map.put(root.val, map.getOrDefault(root.val, 0)+1);
+//             // System.out.println(map);
+//             int odd = 0;
+//             for(int key: map.keySet()){
+//                 odd += map.get(key)%2;
+//             }
+//             map.put(root.val, map.get(root.val)-1);
+//             return odd < 2 ? 1 : 0;
+//         }
+//         map.put(root.val, map.getOrDefault(root.val, 0)+1);
+//         int ans = 0;
+//         if(root.left != null) ans += dfs(root.left, map);
+//         if(root.right != null) ans += dfs(root.right, map);
+//         map.put(root.val, map.get(root.val)-1);
+//         return ans;
+//     }
+// }
