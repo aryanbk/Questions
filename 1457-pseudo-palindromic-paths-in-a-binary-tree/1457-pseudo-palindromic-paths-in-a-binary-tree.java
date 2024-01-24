7e341bb57b52a3b7e9ba7cc46a0576b0a7864bf6
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-01-24 16:31:18 +0530

    Time: 74 ms (12.46%), Space: 79 MB (13.52%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pseudoPalindromicPaths (TreeNode root) {
+        return dfs(root, new HashMap<>());
+    }
+    int dfs(TreeNode root, Map<Integer, Integer> map){
+        if(root.left==null && root.right==null){
+            map.put(root.val, map.getOrDefault(root.val, 0)+1);
+            // System.out.println(map);
+            int odd = 0;
+            for(int key: map.keySet()){
+                odd += map.get(key)%2;
+            }
+            map.put(root.val, map.get(root.val)-1);
+            return odd < 2 ? 1 : 0;
+        }
+        map.put(root.val, map.getOrDefault(root.val, 0)+1);
+        int ans = 0;
+        if(root.left != null) ans += dfs(root.left, map);
+        if(root.right != null) ans += dfs(root.right, map);
+        map.put(root.val, map.get(root.val)-1);
+        return ans;
+    }
+}
