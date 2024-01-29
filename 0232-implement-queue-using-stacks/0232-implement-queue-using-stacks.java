08404f9507cf6016b7041a9dc2f15d51e7993ed8
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-01-29 10:43:37 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (29.67%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,36 +1,71 @@
 class MyQueue {
     Stack<Integer> stk1;
-    Stack<Integer> stk2;
     
     public MyQueue() {
         stk1 = new Stack<>();
-        stk2 = new Stack<>();
     }
     
     public void push(int x) {
-        stk1.add(x);
+        if(stk1.isEmpty()){
+            stk1.push(x);
+            return;
+        }
+        int top = stk1.pop();
+        push(x);
+        stk1.push(top);
+        return;
     }
     
     public int pop() {
-        while(!stk1.isEmpty())
-            stk2.add(stk1.pop());
-        
-        int ans = stk2.pop();
-        
-        while(!stk2.isEmpty())
-            stk1.add(stk2.pop());
-        
-        return ans;
+        return stk1.pop();
     }
     
     public int peek() {
-        return stk1.get(0);
+        return stk1.peek();
     }
     
     public boolean empty() {
         return stk1.isEmpty();
     }
 }
+
+
+
+// // brute force
+// //
+// class MyQueue {
+//     Stack<Integer> stk1;
+//     Stack<Integer> stk2;
+    
+//     public MyQueue() {
+//         stk1 = new Stack<>();
+//         stk2 = new Stack<>();
+//     }
+    
+//     public void push(int x) {
+//         stk1.push(x);
+//     }
+    
+//     public int pop() {
+//         while(!stk1.isEmpty())
+//             stk2.push(stk1.pop());
+        
+//         int ans = stk2.pop();
+        
+//         while(!stk2.isEmpty())
+//             stk1.push(stk2.pop());
+        
+//         return ans;
+//     }
+    
+//     public int peek() {
+//         return stk1.get(0);
+//     }
+    
+//     public boolean empty() {
+//         return stk1.isEmpty();
+//     }
+// }
 
 /**
  * Your MyQueue object will be instantiated and called as such:
