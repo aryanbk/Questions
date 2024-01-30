7c849b5e23dae5d2258f29307223b5ecfaad84d9
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-01-30 10:35:26 +0530

    Time: 5 ms (97.05%), Space: 44.5 MB (54.70%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,31 +1,51 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<String> stk = new Stack<>();
-        for(var t: tokens){
-            if(t.equals("+")){
-                int sec = Integer.parseInt(stk.pop());
-                int first = Integer.parseInt(stk.pop());
-                stk.push(Integer.toString(first+sec));
+        Stack<Integer> stk = new Stack<>();
+        for(String c: tokens){
+            if(c.equals("+")) stk.push(stk.pop()+stk.pop());
+            else if(c.equals("-")) stk.push((stk.pop()*-1)+stk.pop());
+            else if(c.equals("*")) stk.push(stk.pop()*stk.pop());
+            else if(c.equals("/")){
+                int a = stk.pop(), b = stk.pop();
+                stk.push(b/a);
             }
-            else if(t.equals("-")){
-                int sec = Integer.parseInt(stk.pop());
-                int first = Integer.parseInt(stk.pop());
-                stk.push(Integer.toString(first-sec));
-            }
-            else if(t.equals("*")){
-                int sec = Integer.parseInt(stk.pop());
-                int first = Integer.parseInt(stk.pop());
-                stk.push(Integer.toString(first*sec)); 
-            }
-            else if(t.equals("/")){
-                int sec = Integer.parseInt(stk.pop());
-                int first = Integer.parseInt(stk.pop());
-                stk.push(Integer.toString(first/sec));                
-            }
-            else{
-                stk.push(t);
-            }
+            else stk.push(Integer.parseInt(c));
         }
-        return Integer.parseInt(stk.pop());
+        return stk.peek();
     }
 }
+
+
+
+
+// class Solution {
+//     public int evalRPN(String[] tokens) {
+//         Stack<String> stk = new Stack<>();
+//         for(var t: tokens){
+//             if(t.equals("+")){
+//                 int sec = Integer.parseInt(stk.pop());
+//                 int first = Integer.parseInt(stk.pop());
+//                 stk.push(Integer.toString(first+sec));
+//             }
+//             else if(t.equals("-")){
+//                 int sec = Integer.parseInt(stk.pop());
+//                 int first = Integer.parseInt(stk.pop());
+//                 stk.push(Integer.toString(first-sec));
+//             }
+//             else if(t.equals("*")){
+//                 int sec = Integer.parseInt(stk.pop());
+//                 int first = Integer.parseInt(stk.pop());
+//                 stk.push(Integer.toString(first*sec)); 
+//             }
+//             else if(t.equals("/")){
+//                 int sec = Integer.parseInt(stk.pop());
+//                 int first = Integer.parseInt(stk.pop());
+//                 stk.push(Integer.toString(first/sec));                
+//             }
+//             else{
+//                 stk.push(t);
+//             }
+//         }
+//         return Integer.parseInt(stk.pop());
+//     }
+// }
