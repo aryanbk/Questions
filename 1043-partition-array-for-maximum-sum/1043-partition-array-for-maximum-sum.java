a80f0f199068361847421fc2da88cb0bbaf62ee1
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-02-03 07:00:52 +0530

    Time: 415 ms (5.39%), Space: 45.6 MB (5.65%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -1,3 +1,26 @@
+// class Solution {
+//     public int maxSumAfterPartitioning(int[] arr, int k) {
+//         int n = arr.length;
+//         int[] dp = new int[n + 1]; // Updated to 1D dp array
+        
+//         for (int i = 1; i <= n; ++i) {
+//             int maxVal = 0;
+//             for (int j = 1; j <= k && i - j >= 0; ++j) {
+//                 maxVal = Math.max(maxVal, arr[i - j]); // Update the maximum value for the current partition
+//                 dp[i] = Math.max(dp[i], dp[i - j] + maxVal * j);
+//             }
+//         }
+        
+//         return dp[n];
+//     }
+// }
+// 
+// not tried above code
+
+
+
+
+
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int n = arr.length;
