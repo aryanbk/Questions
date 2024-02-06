7ad069e8b9bc8e2f00cce1f69ab3b670fd40514b
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-02-06 15:51:02 +0530

    Time: 3 ms (22.10%), Space: 42.1 MB (6.63%) - LeetHub

---
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.java
@@ -0,0 +1,49 @@
+class Solution {
+    Integer[][][] cache;
+    public int findIntegers(int n) {
+        String str = Integer.toBinaryString(n);
+        cache = new Integer[str.length()+1][2][2];
+        return dp(0, 1, 0, str);
+    }
+    int dp(int i, int one, int allow, String str){
+        if(i ==  str.length()) return 1;
+        if(cache[i][one][allow] != null) return cache[i][one][allow];
+        
+        int limit = allow==1 ? 1 : str.charAt(i)-'0';
+        int ans = 0;
+        
+        for(int x=0; x<=limit; ++x){
+            if(x==1 && !(one==1)) continue;
+            int newAllow = (allow==1 || x<limit) ? 1 : 0;
+            int newOne = (x!=1) ? 1 : 0;
+            ans += dp(i+1, newOne, newAllow, str);
+        }
+        cache[i][one][allow] = ans;
+        return ans;
+    }
+}
+
+
+
+
+// same problem without binary
+//
+// class Solution {
+//     public int findIntegers(int n) {
+//         return dp(0, 1, 0, Integer.toString(n));
+//     }
+//     int dp(int i, int one, int allow, String str){
+//         if(i ==  str.length()) return 1;
+//         int limit = allow==1 ? 9 : str.charAt(i)-'0';
+//         int ans = 0;
+        
+//         for(int x=0; x<=limit; ++x){
+//             if(x==1 && !(one==1)) continue;
+//             int newAllow = (allow==1 || x<limit) ? 1 : 0;
+//             int newOne = (x!=1) ? 1 : 0;
+//             ans += dp(i+1, newOne, newAllow, str);
+//         }
+        
+//         return ans;
+//     }
+// }
