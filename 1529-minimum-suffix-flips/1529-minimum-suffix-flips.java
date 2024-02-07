14fa3f735e88f6f2d30043b3ce4da8f4cf27ff91
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-02-07 14:31:48 +0530

    Time: 7 ms (54.62%), Space: 44 MB (89.33%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.java
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.java
@@ -2,12 +2,14 @@ class Solution {
     public int minFlips(String target) {
         int all = 0;
         int flip = 0;
+        
         for(int i=0; i<target.length(); ++i){
             if(target.charAt(i)-'0' != all){
-                all = all^1;
+                all ^= 1;
                 ++flip;
             }
         }
+        
         return flip;
     }
 }
