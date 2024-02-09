403a92ee7e8a71d13952b363cbad570bd8dd14e1
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-02-09 12:07:23 +0530

    Time: 15 ms (44.59%), Space: 43 MB (49.96%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -44,3 +44,53 @@ class Solution {
         return listLIS.size(); // Return the length of LIS
     }
 }
+
+
+
+// // without sorting - wont work
+//
+// class Solution {
+//     List<Integer> listLIS;
+//     public List<Integer> largestDivisibleSubset(int[] nums) {
+//         lengthOfLIS(nums);
+//         return listLIS;
+//     }
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         int[] dp = new int[n];
+//         Arrays.fill(dp, 1);
+//         int[] map = new int[n];
+        
+//         for(int i = 0; i < n; ++i) {
+//             for(int j = 0; j < i; ++j) {
+//                 if(nums[i]%nums[j] == 0 || nums[j]%nums[i] == 0) {
+//                     if(dp[j] + 1 > dp[i]) {
+//                         dp[i] = dp[j] + 1;
+//                         map[i] = j;
+//                     }
+//                 }
+//             }
+//         }
+        
+//         int maxLen = 0;
+//         int endIdx = 0;
+//         for(int i = 0; i < n; ++i) {
+//             if(dp[i] > maxLen) {
+//                 maxLen = dp[i];
+//                 endIdx = i;
+//             }
+//         }
+        
+//         listLIS = new ArrayList<>();
+//         while(maxLen > 0) {
+//             listLIS.add(nums[endIdx]);
+//             endIdx = map[endIdx];
+//             maxLen--;
+//         }
+        
+//         Collections.reverse(listLIS);
+//         System.out.println(listLIS);
+        
+//         return listLIS.size(); // Return the length of LIS
+//     }
+// }
