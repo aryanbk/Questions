f300eff829223d27a4c01d7fde6443c3cb159ba6
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-02-19 12:23:53 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (71.58%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,10 +1,18 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n<=0) return false;
-        while(n!=1 && n!=-1){
-            if(n%2!=0) return false;
-            n/=2;
-        }
-        return true;
+        return (n>0) && ((n & (n-1)) == 0);
     }
 }
+
+
+
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         if(n<=0) return false;
+//         while(n!=1){
+//             if(n%2!=0) return false;
+//             n/=2;
+//         }
+//         return true;
+//     }
+// }
