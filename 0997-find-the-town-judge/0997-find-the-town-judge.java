da565a1cb104850f1325b2f92a2f028b53b369f9
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-02-22 11:56:10 +0530

    Time: 3 ms (71.04%), Space: 54 MB (35.74%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,13 +1,29 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int[][] map = new int[n+1][2]; //map[out-degree][in-degree]
-        for(int[] t: trust){
-            map[t[0]][0]++;
-            map[t[1]][1]++;
+        int[] t = new int[n];
+        for(int[] e : trust){
+            t[e[0]-1]--;
+            t[e[1]-1]++;
         }
-        for(int i=1; i<n+1; ++i){
-            if(map[i][0] == 0 && map[i][1]==n-1) return i;
+        for(int i=0; i<n; ++i){
+            if(t[i]==n-1) return i+1;
         }
         return -1;
     }
 }
+
+
+
+// class Solution {
+//     public int findJudge(int n, int[][] trust) {
+//         int[][] map = new int[n+1][2]; //map[out-degree][in-degree]
+//         for(int[] t: trust){
+//             map[t[0]][0]++;
+//             map[t[1]][1]++;
+//         }
+//         for(int i=1; i<n+1; ++i){
+//             if(map[i][0] == 0 && map[i][1]==n-1) return i;
+//         }
+//         return -1;
+//     }
+// }
