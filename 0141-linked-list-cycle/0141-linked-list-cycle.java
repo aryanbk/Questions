6739cb643a96d459fe2dbc011bb6593b065aee86
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-03-06 14:25:00 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (93.76%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,17 +9,33 @@
  *     }
  * }
  */
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null) return false;
-        
-        ListNode slow = head;
-        ListNode fast = head;
-        
-        do{
-            slow = slow.next;
-            fast = fast.next.next;
-        }while(slow!=fast && slow.next!=null && fast.next!=null && fast.next.next!=null);
-        return slow==fast;
+        ListNode fast=head,slow=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
     }
 }
+
+
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         if(head == null || head.next == null) return false;
+        
+//         ListNode slow = head;
+//         ListNode fast = head;
+        
+//         do{
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }while(slow!=fast && slow.next!=null && fast.next!=null && fast.next.next!=null);
+//         return slow==fast;
+//     }
+// }
