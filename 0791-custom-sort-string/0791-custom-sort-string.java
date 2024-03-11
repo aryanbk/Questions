6751fa96de42b509cb4b50e46d566c944bf26a92
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-03-11 11:16:05 +0530

    Time: 7 ms (8.46%), Space: 41.5 MB (49.15%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -5,23 +5,52 @@ class Solution {
             map.put(order.charAt(i), i);
         }
 
-        Character[] ss = new Character[s.length()];
-
-        for (int i = 0; i < s.length(); ++i) {
-            ss[i] = s.charAt(i);
+        List<Character> charList = new ArrayList<>();
+        for (char c : s.toCharArray()) {
+            charList.add(c);
         }
 
-        Arrays.sort(ss, (a, b) -> {
+        Collections.sort(charList, (a, b) -> {
             int aa = map.containsKey(a) ? map.get(a) : 27;
             int bb = map.containsKey(b) ? map.get(b) : 27;
             return aa - bb;
         });
 
-        StringBuilder result = new StringBuilder();
-        for (char c : ss) {
-            result.append(c);
+        StringBuilder sb = new StringBuilder();
+        for (char c : charList) {
+            sb.append(c);
         }
 
-        return result.toString();
+        return sb.toString();
     }
 }
+
+
+
+// class Solution {
+//     public String customSortString(String order, String s) {
+//         HashMap<Character, Integer> map = new HashMap<>();
+//         for (int i = 0; i < order.length(); ++i) {
+//             map.put(order.charAt(i), i);
+//         }
+
+//         Character[] ss = new Character[s.length()];
+
+//         for (int i = 0; i < s.length(); ++i) {
+//             ss[i] = s.charAt(i);
+//         }
+
+//         Arrays.sort(ss, (a, b) -> {
+//             int aa = map.containsKey(a) ? map.get(a) : 27;
+//             int bb = map.containsKey(b) ? map.get(b) : 27;
+//             return aa - bb;
+//         });
+
+//         StringBuilder result = new StringBuilder();
+//         for (char c : ss) {
+//             result.append(c);
+//         }
+
+//         return result.toString();
+//     }
+// }
