7fb3e2b83eb6604a1df8221965edd7a90073080c
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-03-11 11:29:59 +0530

    Time: 1 ms (75.87%), Space: 41.5 MB (62.59%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,29 +1,49 @@
+// O(n)
+
 class Solution {
     public String customSortString(String order, String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < order.length(); ++i) {
-            map.put(order.charAt(i), i);
-        }
-
-        List<Character> charList = new ArrayList<>();
-        for (char c : s.toCharArray()) {
-            charList.add(c);
-        }
-
-        Collections.sort(charList, (a, b) -> {
-            int aa = map.containsKey(a) ? map.get(a) : 27;
-            int bb = map.containsKey(b) ? map.get(b) : 27;
-            return aa - bb;
-        });
+        int[] freq = new int[26];
+        for(int i=0; i<s.length(); ++i) ++freq[s.charAt(i)-'a'];
 
         StringBuilder sb = new StringBuilder();
-        for (char c : charList) {
-            sb.append(c);
+        for(int i=0; i<order.length(); ++i){
+            while(freq[order.charAt(i)-'a']-->0) sb.append(order.charAt(i));
         }
+        
+        for(int i=0; i<26; ++i)
+            while(freq[i]-->0) sb.append((char)(i+'a'));
 
         return sb.toString();
     }
 }
+
+
+
+
+
+// class Solution {
+//     public String customSortString(String order, String s) {
+//         HashMap<Character, Integer> map = new HashMap<>();
+//         for (int i = 0; i < order.length(); ++i) {
+//             map.put(order.charAt(i), i);
+//         }
+
+//         List<Character> charList = new ArrayList<>();
+//         for (char c : s.toCharArray()) {
+//             charList.add(c);
+//         }
+
+//         Collections.sort(charList, (a, b) -> 
+//                         map.getOrDefault(a, 26) - map.getOrDefault(b, 26));
+
+//         StringBuilder sb = new StringBuilder();
+//         for (char c : charList) {
+//             sb.append(c);
+//         }
+
+//         return sb.toString();
+//     }
+// }
 
 
 
@@ -40,11 +60,7 @@ class Solution {
 //             ss[i] = s.charAt(i);
 //         }
 
-//         Arrays.sort(ss, (a, b) -> {
-//             int aa = map.containsKey(a) ? map.get(a) : 27;
-//             int bb = map.containsKey(b) ? map.get(b) : 27;
-//             return aa - bb;
-//         });
+//         Collections.sort(charList, (a, b) -> map.getOrDefault(a, 26) - map.getOrDefault(b, 26));
 
 //         StringBuilder result = new StringBuilder();
 //         for (char c : ss) {
