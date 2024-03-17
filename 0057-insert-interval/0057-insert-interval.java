734cc9db58e93fcf43fa755381f42e912cf5b389
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-03-17 15:25:00 +0530

    Time: 2 ms (44.45%), Space: 44.6 MB (74.25%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,18 +1,17 @@
+// O(logn + n)
+
 class Solution {
     public int[][] insert(int[][] intvl, int[] newIntvl) {
         List<int[]> ans = new ArrayList<>();
         int idx = bs(intvl, newIntvl[0]);
         
         for(int i=0; i<idx; ++i) ans.add(intvl[i]);
-        System.out.println(idx);
         int i = ans.size();
         
         if(i!=0 && ans.get(i-1)[1] >= newIntvl[0])
             ans.get(i-1)[1] = Math.max(newIntvl[1], ans.get(i-1)[1]);
         else
             ans.add(newIntvl);
-
-        // System.out.println(ans.size());
         
         for(; i<intvl.length; ++i){
             if(ans.get(ans.size()-1)[1] >= intvl[i][0])
@@ -23,17 +22,6 @@ class Solution {
         int[][] res = new int[ans.size()][2];
         for(int j=0; j<ans.size(); ++j) res[j] = ans.get(j);
         return res;
-        // while(idx-->0) ans.add(intvl[]);
-        
-        
-        
-//         boolean baki = true;
-//         if(intvl[0][0] < newIntvl[0]) ans.add(intvl[0]);
-//         else {ans.add(newIntvl); baki=false};
-        
-//         for(int i=1; i<intvl.length; ++i){
-//             if(baki && ans.get(i-1)[1])
-//         }
     }
     
     int bs(int[][] arr, int x){
@@ -41,7 +29,6 @@ class Solution {
         int j=arr.length;
         while(i<j){
             int mid = i+(j-i)/2;
-            // if(arr[mid][0]==x) return mid;
             if(arr[mid][0] < x) i=mid + 1;
             else j = mid;
         }
