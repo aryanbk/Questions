83a696ee97db4f975d54b596ba91d396c894eff1
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-04-06 13:25:01 +0530

    Time: 24 ms (42.39%), Space: 45.4 MB (41.40%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -2,8 +2,8 @@ class Solution {
     public String minRemoveToMakeValid(String s) {
         Stack<Character> stk = new Stack<>();
         int open = 0;
-        int total = 0;
         int[] close = new int[s.length()+1];
+        
         for(int i=s.length()-1; i>=0; --i)
             close[i] = close[i+1] + (s.charAt(i)==')' ? 1 : 0);
         
@@ -13,32 +13,16 @@ class Solution {
                     stk.push(')');
                     --open;
                 }
-                continue;
             }
             else if(s.charAt(i) == '('){
-                ++open;
-                ++total;
-                if(close[i] - open >= 0)
-                    stk.push('(');
+                if(close[i] - open++ > 0) stk.push('(');
             }
             else
                 stk.push(s.charAt(i));
-            // System.out.println(stk+" "+open);
         }
+        
         char[] ans = new char[stk.size()];
-        int idx = ans.length-1;
-        System.out.println(stk+" "+open+" "+total+" "+idx+" "+stk.size());
-        while(idx >= 0){
-            char ch = stk.pop();
-            ans[idx--] = ch;
-            // if(ch == '(' && total > open){
-            //     ans[idx--] = ch;
-            //     --total;
-            // }
-            // else{
-            //     ans[idx--] = ch;
-            // }
-        }
+        for(int i=ans.length-1; i>=0; --i) ans[i] = stk.pop();
         
         return new String(ans);
     }
