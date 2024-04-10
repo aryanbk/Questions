9f967016f58d6b37389185620022a41f3c2e8b1c
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-04-10 11:28:33 +0530

    Time: 4 ms (86.41%), Space: 44.1 MB (15.53%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -3,15 +3,35 @@ class Solution {
         int n = deck.length;
         int[] ans = new int[n];
         Arrays.sort(deck);
-        Deque<Integer> dq = new LinkedList<>();
-        for(int i=0; i<n; ++i) dq.offerLast(i);
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<n; ++i) q.offer(i);
         
         for(int i=0;; ++i){
-            int polled = dq.pollFirst();
-            ans[polled] = deck[i];
+            ans[q.poll()] = deck[i];
             
-            if(dq.isEmpty()) return ans;
-            dq.offerLast(dq.pollFirst());
+            if(q.isEmpty()) return ans;
+            q.offer(q.poll());
         }
     }
 }
+
+
+
+
+// class Solution {
+//     public int[] deckRevealedIncreasing(int[] deck) {
+//         int n = deck.length;
+//         int[] ans = new int[n];
+//         Arrays.sort(deck);
+//         Deque<Integer> dq = new LinkedList<>();
+//         for(int i=0; i<n; ++i) dq.offerLast(i);
+        
+//         for(int i=0;; ++i){
+//             int polled = dq.pollFirst();
+//             ans[polled] = deck[i];
+            
+//             if(dq.isEmpty()) return ans;
+//             dq.offerLast(dq.pollFirst());
+//         }
+//     }
+// }
