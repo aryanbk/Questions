82244238e1c0a1a87cc8bd883103830c5834ba89
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-04-14 10:02:12 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (14.30%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -13,14 +13,28 @@
  *     }
  * }
  */
+
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
         return dfs(root, false);
     }
     int dfs(TreeNode root, boolean isLeft){
-        if(root == null) return 0;
-        if(root.left==null && root.right==null)
-            return isLeft ? root.val : 0;
+        if(root==null || (root.left==null && root.right==null))
+            return isLeft && root!=null? root.val : 0;
         return dfs(root.left, true) + dfs(root.right, false);
     }
 }
+
+
+
+// class Solution {
+//     public int sumOfLeftLeaves(TreeNode root) {
+//         return dfs(root, false);
+//     }
+//     int dfs(TreeNode root, boolean isLeft){
+//         if(root == null) return 0;
+//         if(root.left==null && root.right==null)
+//             return isLeft ? root.val : 0;
+//         return dfs(root.left, true) + dfs(root.right, false);
+//     }
+// }
