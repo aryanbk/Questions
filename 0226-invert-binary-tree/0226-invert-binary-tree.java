61bccc04e695a4d32a24371ed4b3402c3fe6183a
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-04-20 20:12:00 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (32.44%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -13,12 +13,35 @@
  *     }
  * }
  */
+
+
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if(root==null) return root;
-        TreeNode temp = root.left;
-        root.left = invertTree(root.right);
-        root.right = invertTree(temp);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        
+        while(!q.isEmpty()){
+            TreeNode node = q.poll();
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            if(node.left != null) q.offer(node.left);
+            if(node.right != null) q.offer(node.right);
+        }
+        
         return root;
     }
 }
+
+
+
+// class Solution {
+//     public TreeNode invertTree(TreeNode root) {
+//         if(root==null) return root;
+//         TreeNode temp = root.left;
+//         root.left = invertTree(root.right);
+//         root.right = invertTree(temp);
+//         return root;
+//     }
+// }
