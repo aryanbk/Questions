d1592294b8fd4c1aae2240fc3e4ee1d3c09afa25
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-04-22 22:23:18 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (24.53%) - LeetHub

---
+++ 0991-broken-calculator/0991-broken-calculator.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int brokenCalc(int st, int trgt) {
+        if(st >= trgt) return st-trgt;
+        return trgt%2==0 ? brokenCalc(st, trgt/2)+1 : brokenCalc(st, (trgt+1)/2)+2;
+    }
+}
+
+
+
+
+
+
+
+// class Solution {
+//     public int brokenCalc(int st, int trgt) {
+//         if(st >= trgt) return trgt-st;
+//         Queue<Integer> q = new LinkedList<>();
+//         q.offer(st);
+//         Set<Integer> vis = new HashSet<>();
+        
+//         for(int ans=0; !q.isEmpty(); ++ans){
+//             int l = q.size();
+//             for(int i=0; i<l; ++i){
+//                 int cur = q.poll();
+//                 if(cur==trgt) return ans;
+//                 if(vis.contains(cur)) continue;
+//                 vis.add(cur);
+//                 if(cur*2 <= trgt+1) q.offer(cur*2);
+//                 else{
+                    
+//                 }
+//                 q.offer(cur-1);
+//             }
+//         }
+//         return -1;
+//     }
+// }
