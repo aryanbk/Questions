a4dc5495c09bdc5c999f9ee4cef9bd5b5a045dd5
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-04-22 18:58:30 +0530

    Time: 83 ms (78.98%), Space: 55.1 MB (10.31%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.java
@@ -0,0 +1,77 @@
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        Set<String> vis = new HashSet<>();
+        for(String d: deadends) vis.add(d);
+        
+        Queue<String> q = new LinkedList<>();
+        q.offer("0000");
+        
+        int ans = 0;
+        
+        for(; !q.isEmpty(); ++ans){
+            int l = q.size();
+            // System.out.println(q);
+            
+            for(int i=0; i<l; ++i){
+                String cur = q.poll();
+                char[] ch = cur.toCharArray();
+                
+                if(cur.equals(target)) return ans;
+                if(vis.contains(cur)) continue;
+                vis.add(cur);
+                
+                for(int j=0; j<4; ++j){
+                    // int dig = ch[j]-'0';
+                    char prev = ch[j]=='0' ? '9' : (char)(ch[j]-1);
+                    char nx = ch[j]=='9' ? '0' : (char)(ch[j]+1);
+                    ch[j] = prev;
+                    // if(!vis.contains(String.valueOf(ch)))
+                        q.offer(String.valueOf(ch));
+                    ch[j] = nx;
+                    // if(!vis.contains(String.valueOf(ch)))
+                        q.offer(String.valueOf(ch));
+                    ch[j] = cur.charAt(j);
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
+
+
+// // StackOverFlow Error
+//
+// class Solution {
+//     public int openLock(String[] deadends, String target) {
+//         char[] ch = new char[]{'0', '0', '0', '0'};
+//         Set<String> set = new HashSet<>();
+//         for(String d: deadends) set.add(d);
+        
+//         return bk(ch, set, target.toCharArray(), new HashSet<>());
+//     }
+    
+//     int bk(char[] ch, Set<String> set, char[] trgt, Set<String> vis){
+//         String cur = String.valueOf(ch);
+//         if(Arrays.equals(ch, trgt)) return 0;
+//         if(set.contains(cur)) return -10000000;
+//         vis.add(cur);
+        
+//         int ans = 1000000000;
+        
+//         for(int i=0; i<4; ++i){
+//             int dig = ch[i]-'0';
+//             int prev = (dig+9)%10;
+//             int nx = (dig+1)%10;
+//             ch[i] = (char)(prev+'0');
+//             if(!set.contains(String.valueOf(ch)))
+//                 ans = Math.min(ans, bk(ch, set, trgt, vis));
+//             ch[i] = (char)(nx+'0');
+//             if(!set.contains(String.valueOf(ch)))
+//                 ans = Math.min(ans, bk(ch, set, trgt, vis));
+//             ch[i] = (char)(dig+'0');
+//         }
+        
+//         return ans;
+//     }
+// }
