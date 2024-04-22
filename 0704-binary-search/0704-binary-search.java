218bb93340da99290d5367547cf9d81b28cf257d
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-04-22 14:23:13 +0530

    Time: 0 ms (100.00%), Space: 45.5 MB (71.52%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,9 +1,17 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int ans = Arrays.binarySearch(nums, target);
-        return ans < 0 ? -1 : ans;
+        return Math.max(Arrays.binarySearch(nums, target), -1);
     }
 }
+
+
+
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int ans = Arrays.binarySearch(nums, target);
+//         return ans < 0 ? -1 : ans;
+//     }
+// }
 
 
 // class Solution {
