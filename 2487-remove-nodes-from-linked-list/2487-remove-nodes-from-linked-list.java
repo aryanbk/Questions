6251a20b21f0ad2160e215df36edc7a91d5fa483
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-05-06 08:57:11 +0530

    Time: 6 ms (93.91%), Space: 69 MB (45.04%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -8,24 +8,19 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode removeNodes(ListNode head) {
         ListNode tail = rev(head);
-        Stack<ListNode> stk = new Stack<>();
-        stk.add(tail);
-        ListNode ptr = tail.next;
-        tail.next = null;
-        // System.out.println(ptr.val);
+        ListNode ptr = tail;
+        ListNode prev = new ListNode(-1);
         
         while(ptr != null){
-            // while(!stk.isEmpty() && stk.peek().val < ptr.val)
-            //     stk.pop();
-            // System.out.println(ptr.val);
             ListNode nx = ptr.next;
-            if(stk.peek().val <= ptr.val){
-                stk.peek().next = ptr;
+            if(prev.val <= ptr.val){
+                prev.next = ptr;
                 ptr.next = null;
-                stk.push(ptr);
+                prev = ptr;
             }
             ptr = nx;
         }
@@ -45,3 +40,51 @@ class Solution {
         return prev;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public ListNode removeNodes(ListNode head) {
+//         ListNode tail = rev(head);
+//         Stack<ListNode> stk = new Stack<>();
+//         stk.add(tail);
+//         ListNode ptr = tail.next;
+//         tail.next = null;
+//         // System.out.println(ptr.val);
+        
+//         while(ptr != null){
+//             // while(!stk.isEmpty() && stk.peek().val < ptr.val)
+//             //     stk.pop();
+//             // System.out.println(ptr.val);
+//             ListNode nx = ptr.next;
+//             if(stk.peek().val <= ptr.val){
+//                 stk.peek().next = ptr;
+//                 ptr.next = null;
+//                 stk.push(ptr);
+//             }
+//             ptr = nx;
+//         }
+        
+//         return rev(tail);
+//     }
+    
+//     ListNode rev(ListNode head){
+//         ListNode prev = null;
+//         ListNode cur = head;
+//         while(cur != null){
+//             ListNode nx = cur.next;
+//             cur.next = prev;
+//             prev = cur;
+//             cur = nx;
+//         }
+//         return prev;
+//     }
+// }
