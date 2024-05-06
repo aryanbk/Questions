6bd737e25afa6f58ac03e4c27204736066b76b9e
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-05-06 09:40:14 +0530

    Time: 41 ms (29.65%), Space: 45.2 MB (71.24%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -4,24 +4,53 @@ class Solution {
         int[] aSuff = new int[n+1];
         for(int i=n-1; i>=0; --i)
             aSuff[i] = aSuff[i+1] + (s.charAt(i)=='a' ? 1 : 0);
-        // System.out.println(Arrays.toString(aSuff)+" "+n);
         int as = aSuff[0];
         int bs = n - as;
         
         if(as == n || as == 0) return 0;
         
         int bPre = 0;
-        int ans = 1000000000;
+        int ans = Math.min(as, bs);
         
         for(int i=0; i<n; ++i){
             bPre += s.charAt(i)=='b' ? 1 : 0;
-            if(aSuff[i+1]==as || bPre==bs)
-                ans = Math.min(ans, Math.min(as, bs));
-            else
-                ans = Math.min(ans, bPre + aSuff[i+1]);
-            // System.out.println(ans);
+            ans = Math.min(ans, bPre + aSuff[i+1]);
         }
         
         return ans;
     }
 }
+
+
+
+
+
+
+
+// class Solution {
+//     public int minimumDeletions(String s) {
+//         int n = s.length();
+//         int[] aSuff = new int[n+1];
+//         for(int i=n-1; i>=0; --i)
+//             aSuff[i] = aSuff[i+1] + (s.charAt(i)=='a' ? 1 : 0);
+//         // System.out.println(Arrays.toString(aSuff)+" "+n);
+//         int as = aSuff[0];
+//         int bs = n - as;
+        
+//         if(as == n || as == 0) return 0;
+
+//         int bPre = 0;
+//         int ans = 1000000000;
+        
+//         for(int i=0; i<n; ++i){
+//             bPre += s.charAt(i)=='b' ? 1 : 0;
+//             if(aSuff[i+1]==as || bPre==bs)
+//                 ans = Math.min(ans, Math.min(as, bs));
+//             else
+//                 ans = Math.min(ans, bPre + aSuff[i+1]);
+//             // System.out.println(ans);
+//         }
+        
+//         return ans;
+//     }
+// }
