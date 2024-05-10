ca2e1d0ed8ae2596506e41cd87af60d580096215
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-05-10 13:42:23 +0530

    Time: 399 ms (48.03%), Space: 69.4 MB (41.92%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -1,41 +1,63 @@
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
         int n = arr.length;
-        PriorityQueue<Curr> pq = new PriorityQueue<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) ->
+        Double.compare((double)b[0]/(double)b[1], (double)a[0]/(double)a[1]));
+        // reverse double compare
         
         for(int i=0; i<n; ++i){
             for(int j=i+1; j<n; ++j){
-                Curr cur = new Curr(arr[i], arr[j]);
-                pq.offer(cur);
+                pq.offer(new int[]{arr[i], arr[j]});
                 while(pq.size() > k) pq.poll();
             }
         }
-        // System.out.println(pq);
-        Curr ans = pq.poll();
-        return new int[]{ans.i, ans.j};
+        return pq.poll();
         
     }
 }
 
 
-class Curr implements Comparable<Curr>{
-    int i;
-    int j;
-    public Curr(int ii, int jj){
-        i = ii;
-        j = jj;
-    }
-    
-    @Override
-    public int compareTo(Curr two){
-        double fir = (double)this.i / (double) this.j;
-        double sec = (double)two.i / (double) two.j;
+
+
+
+// class Solution {
+//     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+//         int n = arr.length;
+//         PriorityQueue<Curr> pq = new PriorityQueue<>();
         
-        return Double.compare(sec, fir);
-    }
+//         for(int i=0; i<n; ++i){
+//             for(int j=i+1; j<n; ++j){
+//                 Curr cur = new Curr(arr[i], arr[j]);
+//                 pq.offer(cur);
+//                 while(pq.size() > k) pq.poll();
+//             }
+//         }
+//         // System.out.println(pq);
+//         Curr ans = pq.poll();
+//         return new int[]{ans.i, ans.j};
+        
+//     }
+// }
+
+
+// class Curr implements Comparable<Curr>{
+//     int i;
+//     int j;
+//     public Curr(int ii, int jj){
+//         i = ii;
+//         j = jj;
+//     }
     
-    @Override
-    public String toString(){
-        return this.i+" "+this.j;
-    }
-}
+//     @Override
+//     public int compareTo(Curr two){
+//         double fir = (double)this.i / (double) this.j;
+//         double sec = (double)two.i / (double) two.j;
+        
+//         return Double.compare(sec, fir);
+//     }
+    
+//     @Override
+//     public String toString(){
+//         return this.i+" "+this.j;
+//     }
+// }
