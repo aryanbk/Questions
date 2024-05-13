801918c2192bf3bf08ad8a7b6907b91c4619151d
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-05-13 12:14:45 +0530

    Time: 0 ms (100.00%), Space: 41 MB (86.06%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.java
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.java
@@ -19,8 +19,8 @@ class Solution {
         
         int ans = 0;
         for(int i=0; i<m; ++i){
-            String rowVal = rowFind(grid, r, c, i);
-            ans += Integer.parseInt(rowVal, 2);
+            int rowVal = rowFind(grid, r, c, i);
+            ans += rowVal;
         }
         
         return ans;
@@ -33,10 +33,64 @@ class Solution {
         return ans;
     }
     
-    String rowFind(int[][] grid, int[] r, int[] c, int i){
-        StringBuilder ans = new StringBuilder();
-        for(int j=0; j<grid[i].length; ++j)
-            ans.append((grid[i][j] ^ (r[i] ^ c[j]))==1 ? '1' : '0');
-        return ans.toString();
+    int rowFind(int[][] grid, int[] r, int[] c, int i){
+        int ans = 0;
+        int two = 1;
+        for(int j = grid[i].length-1; j >= 0; --j, two *= 2)
+            ans += (grid[i][j] ^ (r[i] ^ c[j])) * two;
+        return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int matrixScore(int[][] grid) {
+//         int m = grid.length;
+//         int n = grid[0].length;
+        
+//         int[] r = new int[m];
+//         int[] c = new int[n];
+        
+//         for(int i=0; i<m; ++i){
+//             if(grid[i][0] == 0)
+//                 r[i] = 1;
+//         }
+        
+//         for(int j=1; j<n; ++j){
+//             int colVal = colFind(grid, r, c, j);
+//             if(colVal < (m+1)/2)
+//                 c[j] = 1;
+//         }
+        
+//         int ans = 0;
+//         for(int i=0; i<m; ++i){
+//             String rowVal = rowFind(grid, r, c, i);
+//             ans += Integer.parseInt(rowVal, 2);
+//         }
+        
+//         return ans;
+//     }
+    
+//     int colFind(int[][] grid, int[] r, int[] c, int j){
+//         int ans = 0;
+//         for(int i=0; i<grid.length; ++i)
+//             ans += (grid[i][j] ^ (r[i] ^ c[j]));
+//         return ans;
+//     }
+    
+//     String rowFind(int[][] grid, int[] r, int[] c, int i){
+//         StringBuilder ans = new StringBuilder();
+//         for(int j=0; j<grid[i].length; ++j)
+//             ans.append((grid[i][j] ^ (r[i] ^ c[j]))==1 ? '1' : '0');
+//         return ans.toString();
+//     }
+// }
