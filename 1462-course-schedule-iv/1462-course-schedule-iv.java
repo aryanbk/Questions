2e0dd8a6a0cec2e0481bd267d0c32d9d495f7596
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-05-19 01:59:34 +0530

    Time: 19 ms (82.20%), Space: 49 MB (31.00%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.java
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -1,34 +1,29 @@
 class Solution {
     List<Integer>[] map;
-    // Map<List<Integer>, Boolean> cache;
     Boolean[][] cache;
-    public List<Boolean> checkIfPrerequisite(int n, int[][] pre, int[][] q) {
-        map = new ArrayList[n];
-        // cache = new HashMap<>();
+    
+    public List<Boolean> checkIfPrerequisite(int n, int[][] pre, int[][] que) {
         cache = new Boolean[n][n];
-        
-        Arrays.setAll(map, (i) -> {
-            List<Integer> list = new ArrayList<>();
-            // list.add(i);
-            return list;
-        });
-        for(int[] p: pre)
-            union(p[0], p[1]);
-        
         List<Boolean> ans = new ArrayList<>();
-        for(int[] i: q){
-            ans.add(find(i[0], i[1]));
-        }
+        
+        map = new ArrayList[n];
+        for(int i=0; i<n; ++i) map[i] = new ArrayList<>();
+        
+        for(int[] p: pre) union(p[0], p[1]);
+        
+        for(int[] q: que) ans.add(find(q[0], q[1]));
+        
         return ans;
     }
     
     void union(int i, int j){
         map[i].add(j);
+        cache[i][j] = true;
     }
     boolean find(int i, int j){
-        // if()
         if(i==j) return true;
         if(cache[i][j] != null) return cache[i][j];
+        
         for(int nbr: map[i]){
             if(find(nbr, j)){
                 cache[i][j] = true;
