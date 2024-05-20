7ed460f64ff1a5a852b6bdd4e33729702ab5b0e1
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-05-20 16:18:32 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (96.23%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,17 +1,38 @@
 class Solution {
     int ans = 0;
     public int subsetXORSum(int[] nums) {
-        // return bk(0, nums) + nums[0];
         bk(0, nums, 0);
         return ans;
     }
-    int bk(int i, int[] nums, int cur){
-        if(i==nums.length){
+    void bk(int i, int[] nums, int cur){
+        if(i==nums.length)
             ans += cur;
-            return cur;
+        else{
+            bk(i+1, nums, cur);
+            bk(i+1, nums, cur^nums[i]);
         }
-        bk(i+1, nums, cur);
-        bk(i+1, nums, cur^nums[i]);
-        return cur;
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     int ans = 0;
+//     public int subsetXORSum(int[] nums) {
+//         // return bk(0, nums) + nums[0];
+//         bk(0, nums, 0);
+//         return ans;
+//     }
+//     int bk(int i, int[] nums, int cur){
+//         if(i==nums.length){
+//             ans += cur;
+//             return cur;
+//         }
+//         bk(i+1, nums, cur);
+//         bk(i+1, nums, cur^nums[i]);
+//         return cur;
+//     }
+// }
