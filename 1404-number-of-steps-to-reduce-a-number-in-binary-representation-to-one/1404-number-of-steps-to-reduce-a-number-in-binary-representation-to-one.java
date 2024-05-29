736c271b0166f4fa82d8f8eead0e03b24c4e60c9
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-05-29 13:06:37 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (57.23%) - LeetHub

---
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int numSteps(String s) {
+        int carry = 0;
+        int ans = 0;
+        for(int i=s.length()-1; i>0; --i, ++ans){
+            int num = s.charAt(i)-'0' + carry;
+            ans += num%2;
+            carry = num==0 ? 0 : 1;
+        }
+        ans += carry==0 ? 0 : 1;
+        return ans;
+    }
+}
+
+
+
+
+
+
+// class Solution {
+//     public int numSteps(String s) {
+//         int n = Integer.parseInt(s, 2);
+//         int ans = 0;
+//         while(n != 1){
+//             // ans += n%2==0 ? 1 : 2;
+//             n = n%2==0 ? n/2 : (n+1);
+//             ++ans;
+//         }
+//         return ans;
+//     }
+// }
