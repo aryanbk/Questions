2154dc4788f688332647eec78dffab7b8abb12ea
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-06-04 16:02:52 +0530

    Time: 6 ms (60.18%), Space: 41.8 MB (35.32%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,21 +1,39 @@
 class Solution {
     public int longestPalindrome(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        for(int i=0; i<s.length(); ++i)
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-        
+        Set<Character> set = new HashSet<>();
         int ans = 0;
-        boolean isOdd = false;
         
-        for(char i: map.keySet()){
-            int val = map.get(i);
-            if(val %2 ==0) ans += val;
-            else {
-                isOdd = true;
-                ans += val - 1;
-            }
+        for(int i=0; i<s.length(); ++i){
+            char ch = s.charAt(i);
+            if(set.contains(ch)){
+                ans += 2;
+                set.remove(ch);
+            }else
+                set.add(ch);
         }
         
-        return ans + (isOdd ? 1 : 0);
+        return ans + (!set.isEmpty() ? 1 : 0);
     }
 }
+
+// class Solution {
+//     public int longestPalindrome(String s) {
+//         Map<Character, Integer> map = new HashMap<>();
+//         for(int i=0; i<s.length(); ++i)
+//             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        
+//         int ans = 0;
+//         boolean isOdd = false;
+        
+//         for(char i: map.keySet()){
+//             int val = map.get(i);
+//             if(val %2 ==0) ans += val;
+//             else {
+//                 isOdd = true;
+//                 ans += val - 1;
+//             }
+//         }
+        
+//         return ans + (isOdd ? 1 : 0);
+//     }
+// }
