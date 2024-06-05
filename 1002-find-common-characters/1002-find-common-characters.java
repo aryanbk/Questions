bdaf3cc739ee109c646e364b55bc8569bc29c70d
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-06-05 18:24:51 +0530

    Time: 4 ms (60.81%), Space: 43 MB (65.61%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+        int n = words.length;
+        int[][] f = new int[n][26];
+        
+        // for(int x=0; x<n; ++x){
+        //     for(int i=0; i<words[x].length(); ++i)
+        //         f[x][words[x].charAt(i)-'a']++;
+        // }
+        
+        for(int i=0; i<words.length; ++i){
+            for(int j=0; j<words[i].length(); ++j){
+                ++f[i][words[i].charAt(j)-'a'];
+            }
+        }
+        
+        int[] minF = new int[26];
+        Arrays.fill(minF, 1000000000);
+        
+        for(int c=0; c<26; ++c){
+            for(int i=0; i<words.length; ++i){
+                minF[c] = Math.min(minF[c], f[i][c]);
+            }
+        }
+        
+        List<String> ans = new ArrayList<>();
+        
+        for(int i=0; i<26; ++i){
+            for(int x=0; x<minF[i]; ++x){
+                char ch = (char)(i+'a');
+                ans.add(String.valueOf(ch));
+            }
+        }
+        
+        return ans;
+    }
+}
