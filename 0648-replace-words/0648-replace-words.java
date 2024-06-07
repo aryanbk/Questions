d17993fa23deaae2276c673d0bfecddde53b9029
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-06-07 21:54:39 +0530

    Time: 8 ms (99.24%), Space: 55.3 MB (60.53%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -2,20 +2,14 @@ class Solution {
     Node head;
     public String replaceWords(List<String> dct, String s) {
         head = new Node();
-        for(String word: dct){
-            insert(word);
-        }
+        for(String word: dct) insert(word);
         
         String[] ss = s.split(" ");
-        for(int i=0; i<ss.length; ++i){
-            String word = ss[i];
-            ss[i] = search(word);
-        }
+        for(int i=0; i<ss.length; ++i) ss[i] = search(ss[i]);
         
         return String.join(" ", ss);
-        
-        
     }
+    
     void insert(String word){
         Node cur = head;
         for(int i=0; i<word.length(); ++i){
@@ -24,30 +18,30 @@ class Solution {
                 cur.map[idx] = new Node();
             cur = cur.map[idx];
         }
-        cur.word = word;
+        cur.end = true;
     }
     
     String search(String word){
         Node cur = head;
         int i=0;
-        while(cur.word == null && i<word.length()){
+        while(!cur.end && i<word.length()){
             int idx = word.charAt(i)-'a';
             if(cur.map[idx] == null)
                 return word;
             cur = cur.map[idx];
             ++i;
         }
-        return cur.word==null ? word : cur.word;
+        return cur.end ? word.substring(0, i) : word;
     }
     
 }
 
+// Trie Node
 class Node{
     Node[] map;
-    String word;
+    boolean end;
     public Node(){
         map = new Node[26];
-        word = null;
     }
 }
 
@@ -55,8 +49,8 @@ class Node{
 
 
 
-
-
+// // brute force
+//
 // class Solution {
 //     public String replaceWords(List<String> dct, String s) {
 //         String[] ss = s.split(" ");
