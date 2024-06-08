bfea57d1b3f96e529d2e5edfe322ddcb523c103e
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-06-08 18:46:28 +0530

    Time: 377 ms (84.00%), Space: 66.4 MB (10.90%) - LeetHub

--- 3164-find-the-number-of-good-pairs-ii/3164-find-the-number-of-good-pairs-ii.java
+++ 3164-find-the-number-of-good-pairs-ii/3164-find-the-number-of-good-pairs-ii.java
@@ -6,17 +6,47 @@ class Solution {
         long ans = 0;
         
         for(int i=0; i<n; ++i)
-            map.put(nums2[i]*k, map.getOrDefault(nums2[i]*k, 0)+1);
+            map.put(nums2[i], map.getOrDefault(nums2[i], 0)+1);
         
         for(int i=0; i<m; ++i){
-            for(int j=1; j*j<=nums1[i]; ++j)
-                if(nums1[i]%j == 0){
-                    ans += map.getOrDefault(j, 0);
-                    int op = nums1[i]/j;
-                    ans += j!=op ? map.getOrDefault(op, 0) : 0;
-                }
+            if(nums1[i] % k == 0){
+                int num = nums1[i]/k;
+                for(int j=1; j*j <= num; ++j)
+                    if(num % j == 0){
+                        ans += map.getOrDefault(j, 0);
+                        int op = num/j;
+                        ans += j!=op ? map.getOrDefault(op, 0) : 0;
+                    }
+            }
         }
         
         return ans;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public long numberOfPairs(int[] nums1, int[] nums2, int k) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         int m = nums1.length;
+//         int n = nums2.length;
+//         long ans = 0;
+        
+//         for(int i=0; i<n; ++i)
+//             map.put(nums2[i]*k, map.getOrDefault(nums2[i]*k, 0)+1);
+        
+//         for(int i=0; i<m; ++i){
+//             for(int j=1; j*j<=nums1[i]; ++j)
+//                 if(nums1[i]%j == 0){
+//                     ans += map.getOrDefault(j, 0);
+//                     int op = nums1[i]/j;
+//                     ans += j!=op ? map.getOrDefault(op, 0) : 0;
+//                 }
+//         }
+        
+//         return ans;
+//     }
+// }
