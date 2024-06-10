1d12974beb246069939969cdafb0f7cdd4e266c5
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-06-10 09:47:01 +0530

    Time: 4 ms (11.65%), Space: 41.3 MB (51.37%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -2,9 +2,8 @@ class Solution {
     public int heightChecker(int[] heights) {
         int[] sorted = heights.clone();
         Arrays.sort(sorted);
-        int ans = 0;
-        int n = sorted.length;
-        ans = (int) IntStream.range(0, n).filter(i -> sorted[i] != heights[i]).count();
-        return ans;
+        return (int) IntStream.range(0, heights.length)
+            .filter(i -> sorted[i] != heights[i])
+            .count();
     }
 }
