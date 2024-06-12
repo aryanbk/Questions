8997483b1fdda54f881aa4792d713e3ce0b4461e
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-06-12 10:08:16 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (42.89%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,22 +1,51 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        int i=0;
-        int j=n-1;
-        for(int x=0; x<=j; ++x){
-            if(nums[x] < 1)
-                swap(nums, x, i++);
-            else if(nums[x] > 1)
-                swap(nums, x--, j--);
+        int i=0; // pointer for 0
+        int j=0; // pointer for 1
+        int k = nums.length - 1; // pointer for 2
+        
+        while(j<=k){
+            if(nums[j] == 0){
+                swap(j, i, nums);
+                ++i;
+                ++j;
+            }
+            else if(nums[j] == 2){
+                swap(j, k, nums);
+                --k;
+            }
+            else if(nums[i] == 1)
+                ++j;
         }
     }
     
-    void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    void swap(int a, int b, int[] nums){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
+
+
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         int n = nums.length;
+//         int i=0;
+//         int j=n-1;
+//         for(int x=0; x<=j; ++x){
+//             if(nums[x] < 1)
+//                 swap(nums, x, i++);
+//             else if(nums[x] > 1)
+//                 swap(nums, x--, j--);
+//         }
+//     }
+    
+//     void swap(int[] nums, int i, int j){
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+// }
 
 
 
