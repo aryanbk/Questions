378630ae730143a13e8c7c01d4bf47fb66ff9824
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-06-13 16:42:55 +0530

    Time: 7 ms (5.22%), Space: 44.2 MB (34.48%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
@@ -1,11 +1,28 @@
+// Java streams
+//
 class Solution {
     public int minMovesToSeat(int[] seats, int[] std) {
         Arrays.sort(seats);
         Arrays.sort(std);
-        int ans = 0;
-        for(int i=0; i<seats.length; ++i)
-            ans += Math.abs(seats[i]-std[i]);
-        
-        return ans;
+        return IntStream.range(0, seats.length)
+            .map(i -> Math.abs(seats[i]-std[i]))
+            .reduce(0, Integer::sum);
     }
 }
+
+
+
+
+// // normal method
+//
+// class Solution {
+//     public int minMovesToSeat(int[] seats, int[] std) {
+//         Arrays.sort(seats);
+//         Arrays.sort(std);
+//         int ans = 0;
+//         for(int i=0; i<seats.length; ++i)
+//             ans += Math.abs(seats[i]-std[i]);
+        
+//         return ans;
+//     }
+// }
