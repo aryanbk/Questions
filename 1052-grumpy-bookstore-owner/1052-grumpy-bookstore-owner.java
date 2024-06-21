90892070d0c3cc946dc95203301355061ffd48e6
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-06-21 20:54:32 +0530

    Time: 5 ms (24.31%), Space: 47.4 MB (47.57%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,27 +1,31 @@
 class Solution {
     public int maxSatisfied(int[] cst, int[] gmp, int mnt) {
         int n = cst.length;
-        int sumt = 0;
-        int sumtGmp = 0;
+        int maxSatisfy = 0;
+        int maxUnsatisfy = 0;
         
         for(int i=0; i<n; ++i){
-            sumt += cst[i];
+            maxSatisfy += cst[i];
             cst[i] = cst[i]*gmp[i];
-            sumtGmp += cst[i];
+            maxUnsatisfy += cst[i];
         }
         
+        return maxSatisfy - maxUnsatisfy + maxSubarraySum(cst, mnt);
+    }
+    
+    int maxSubarraySum(int[] nums, int k){
         int curSumt = 0;
         int mx = 0;
         
-        for(int l=0, r=0; r<n; ++r){
-            curSumt += cst[r];
-            if(r-l+1 == mnt){
+        for(int l=0, r=0; r<nums.length; ++r){
+            curSumt += nums[r];
+            if(r-l+1 == k){
                 mx = Math.max(mx, curSumt);
-                curSumt -= cst[l];
+                curSumt -= nums[l];
                 ++l;
             }
         }
         
-        return sumt - sumtGmp + mx;
+        return mx;
     }
 }
