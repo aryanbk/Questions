06281aac8f17ed458d7181fe7a690cde5b0ff47b
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-06-28 14:38:40 +0530

    Time: 56 ms (60.98%), Space: 56.3 MB (38.24%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -4,8 +4,8 @@ class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
         root1 = new Node();
         root2 = new Node();
-        add(arr1, root1);
-        add(arr2, root2);
+        insert(arr1, root1);
+        insert(arr2, root2);
         
         return dfs(root1, root2, 0);
     }
@@ -14,8 +14,6 @@ class Solution {
     int dfs(Node root1, Node root2, int cur){
         if(root1==null || root2==null) return 0;
         int ans = 0;
-        if((root1.end || root2.end) && (root1.child>0 || root2.child>0))
-            ans = cur;
         
         for(int i=0; i<10; ++i){
             if(root1.arr[i]!=null && root2.arr[i]!=null){
@@ -27,7 +25,7 @@ class Solution {
         return ans;
     }
     
-    void add(int[] nums, Node root){
+    void insert(int[] nums, Node root){
         for(int num: nums){
             String str = Integer.toString(num);
             
@@ -36,7 +34,6 @@ class Solution {
                 int x = str.charAt(i)-'0';
                 if(cur.arr[x]==null)
                     cur.arr[x] = new Node();
-                cur.child = cur.child+1;
                 cur = cur.arr[x];
             }
             cur.end = true;
@@ -47,10 +44,8 @@ class Solution {
 class Node{
     Node[] arr;
     boolean end;
-    int child;
     public Node(){
         arr = new Node[10];
         end = false;
-        child = 0;
     }
 }
