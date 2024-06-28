9905fe039a723661d4c18469c14e342ca3f7d490
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-06-28 15:07:35 +0530

    Time: 30 ms (91.99%), Space: 57.1 MB (14.99%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -1,28 +1,28 @@
 class Solution {
-    Node root1;
-    Node root2;
+    Node root;
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        root1 = new Node();
-        root2 = new Node();
-        insert(arr1, root1);
-        insert(arr2, root2);
-        
-        return dfs(root1, root2, 0);
-    }
-    
-    
-    int dfs(Node root1, Node root2, int cur){
-        if(root1==null || root2==null) return 0;
+        root = new Node();
+        insert(arr1, root);
         int ans = 0;
         
-        for(int i=0; i<10; ++i){
-            if(root1.arr[i]!=null && root2.arr[i]!=null){
-                ans = Math.max(ans, cur+1); // Important
-                ans = Math.max(ans, dfs(root1.arr[i], root2.arr[i], cur+1));
-            }
+        
+        for(int a: arr2){
+            ans = Math.max(ans, matchPrefix(Integer.toString(a)));
         }
         
         return ans;
+        
+    }
+    
+    int matchPrefix(String str){
+        Node cur = root;
+        int i=0;
+        for(; i<str.length(); ++i){
+            int x = str.charAt(i)-'0';
+            if(cur.arr[x]==null) break;
+            else cur = cur.arr[x];
+        }
+        return i;
     }
     
     void insert(int[] nums, Node root){
@@ -49,3 +49,62 @@ class Node{
         end = false;
     }
 }
+
+
+
+
+
+
+// // 2 trie soolution - no need to use 2 tries
+//
+// class Solution {
+//     Node roo;
+//     Node root2;
+//     public int longestCommonPrefix(int[] arr1, int[] arr2) {
+//         roo= new Node();
+//         root2 = new Node();
+//         insert(arr1, roo);
+//         insert(arr2, root2);
+        
+//         return dfs(roo, root2, 0);
+//     }
+    
+    
+//     int dfs(Node roo, Node root2, int cur){
+//         if(root==null || root2==null) return 0;
+//         int ans = 0;
+        
+//         for(int i=0; i<10; ++i){
+//             if(roo.arr[i]!=null && root2.arr[i]!=null){
+//                 ans = Math.max(ans, cur+1); // Important
+//                 ans = Math.max(ans, dfs(roo.arr[i], root2.arr[i], cur+1));
+//             }
+//         }
+        
+//         return ans;
+//     }
+    
+//     void insert(int[] nums, Node root){
+//         for(int num: nums){
+//             String str = Integer.toString(num);
+            
+//             Node cur = root;
+//             for(int i=0; i<str.length(); ++i){
+//                 int x = str.charAt(i)-'0';
+//                 if(cur.arr[x]==null)
+//                     cur.arr[x] = new Node();
+//                 cur = cur.arr[x];
+//             }
+//             cur.end = true;
+//         }
+//     }
+// }
+
+// class Node{
+//     Node[] arr;
+//     boolean end;
+//     public Node(){
+//         arr = new Node[10];
+//         end = false;
+//     }
+// }
