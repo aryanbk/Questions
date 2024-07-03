65455e741508be1c586e1b1c9810a8d303c2dbc0
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-07-03 09:37:03 +0530

    Time: 15 ms (96.06%), Space: 57.7 MB (5.51%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -1,10 +1,15 @@
 class Solution {
     public int minDifference(int[] nums) {
         Arrays.sort(nums);
-        return mcm(0, 0, nums.length-1, nums);
+        return solve(0, 0, nums.length-1, nums);
     }
-    int mcm(int x, int i, int j, int[] nums){
-        if(x==3) return j>i ? Math.abs(nums[j]-nums[i]) : 0;
-        return Math.min(mcm(x+1, i+1, j, nums), mcm(x+1, i, j-1, nums));
+    
+    // only 3 moves so dp is not required
+    // x is dependant on i & j, x is not a state, i used it for convenience
+    // i & j are only state
+    
+    int solve(int x, int i, int j, int[] nums){
+        if(x==3) return j>i ? nums[j]-nums[i] : 0;
+        return Math.min(solve(x+1, i+1, j, nums), solve(x+1, i, j-1, nums));
     }
 }
