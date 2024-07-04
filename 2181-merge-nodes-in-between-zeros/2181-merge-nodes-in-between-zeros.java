1602d90b9824e00b6c90c06d546a3c8888668174
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-07-04 13:51:37 +0530

    Time: 34 ms (5.00%), Space: 96.1 MB (5.08%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -8,21 +8,41 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        var ans = new ListNode(0);
-        var ansPtr = ans;
-        var ptr = head.next;
-        
-        while(ptr.next != null){
-            if(ptr.val == 0){
-                ansPtr.next = new ListNode(0);
-                ansPtr = ansPtr.next;
-            }
-            ansPtr.val += ptr.val;
-            ptr = ptr.next;
+        if(head == null || head.next == null) return null;
+        head = head.next;
+        int sumt = 0;
+        while(head!=null && head.val!=0){
+            sumt += head.val;
+            head = head.next;
         }
-        
-        return ans;
+        return new ListNode(sumt, mergeNodes(head));
     }
 }
+
+
+
+
+// // iterative
+//
+// class Solution {
+//     public ListNode mergeNodes(ListNode head) {
+//         var ans = new ListNode(0);
+//         var ansPtr = ans;
+//         var ptr = head.next;
+        
+//         while(ptr.next != null){
+//             if(ptr.val == 0){
+//                 ansPtr.next = new ListNode(0);
+//                 ansPtr = ansPtr.next;
+//             }
+//             ansPtr.val += ptr.val;
+//             ptr = ptr.next;
+//         }
+        
+//         return ans;
+//     }
+// }
