f36c463b0f897499a4712cbdd87560aa0a49a04a
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-07-07 16:09:41 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (54.33%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.java
+++ 1518-water-bottles/1518-water-bottles.java
@@ -1,10 +1,28 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-        return solve(numBottles, numExchange, 0);
-    }
-    
-    int solve(int filled, int exch, int empty){
-        if(filled==0 && empty<exch) return 0;
-        return filled + solve((filled+empty)/exch, exch, (filled+empty)%exch);
+        int empty = 0;
+        int ans = 0;
+        
+        while(numBottles!=0){
+            ans += numBottles;
+            empty += numBottles;
+            numBottles = (empty)/numExchange;
+            empty = (empty)%numExchange;
+        }
+        
+        return ans;
     }
 }
+
+
+
+// class Solution {
+//     public int numWaterBottles(int numBottles, int numExchange) {
+//         return solve(numBottles, 0, numExchange);
+//     }
+    
+//     int solve(int filled, int empty, int rate){
+//         if(filled==0 && empty<rate) return 0;
+//         return filled + solve((filled+empty)/rate, (filled+empty)%rate, rate);
+//     }
+// }
