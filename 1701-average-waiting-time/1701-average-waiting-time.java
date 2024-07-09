1c1aa4b9ffa6aa596ac08b0562e018e4577e9685
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-07-09 08:44:24 +0530

    Time: 4 ms (42.86%), Space: 73 MB (22.04%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.java
+++ 1701-average-waiting-time/1701-average-waiting-time.java
@@ -1,19 +1,38 @@
 class Solution {
     public double averageWaitingTime(int[][] cust) {
-        int n = cust.length;
         int t = 0;
         double sumt = 0;
+        
         for(int[] c: cust){
-            if(t<=c[0]){
-                t = c[0] + c[1];
-            }
-            else{
-                sumt += t-c[0];
-                t += c[1];
-            }
-            sumt += c[1];
+            sumt += Math.max(t-c[0], 0) + c[1];
+            t = Math.max(t, c[0]) + c[1];
         }
         
-        return sumt/n;
+        return sumt/cust.length;
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     public double averageWaitingTime(int[][] cust) {
+//         int n = cust.length;
+//         int t = 0;
+//         double sumt = 0;
+//         for(int[] c: cust){
+//             if(t<=c[0]){
+//                 t = c[0] + c[1];
+//             }
+//             else{
+//                 sumt += t-c[0];
+//                 t += c[1];
+//             }
+//             sumt += c[1];
+//         }
+        
+//         return sumt/n;
+//     }
+// }
