faed3306bbe70a0fa4cf0abe9539abbfdfa5d244
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-07-14 23:57:26 +0530

    Time: 4 ms (86.88%), Space: 41.8 MB (45.70%) - LeetHub

--- 0726-number-of-atoms/0726-number-of-atoms.java
+++ 0726-number-of-atoms/0726-number-of-atoms.java
@@ -1,29 +1,9 @@
 class Solution {
     int n;
-    int[] bkt;
     public String countOfAtoms(String fml) {
         n = fml.length();
-        bkt = new int[n];
-        var stk = new ArrayDeque<Integer>();
-        for(int i=0; i<n; ++i){
-            char c = fml.charAt(i);
-            if(help(c) == 2) stk.offerLast(i);
-            if(help(c) == -2){
-                int open = stk.pollLast();
-                bkt[open] = i;
-                // bkt[i] = open;
-                // System.out.println(open+" "+i);
-            }
-        }
-        // System.out.println(Arrays.toString(bkt));
-        
-//         var map = new TreeMap<Character, Integer>();
-//         var sb = new StringBuilder();
-//         for(int i=0; i<n; ++i){
-//             char c = s.charAt(i);
-            
-//         }
         Map<String, Integer> map = solve(0, fml);
+        
         var sb = new StringBuilder();
         for(String key: map.keySet()){
             int val = map.get(key);
@@ -36,8 +16,6 @@ class Solution {
     }
     
     Map<String, Integer> solve(int i, String fml){
-        // System.out.println(i);
-        // var map = new TreeMap<String, Integer>(Collections.reverseOrder());
         var map = new TreeMap<String, Integer>();
         
         while(i<n){
@@ -48,19 +26,7 @@ class Solution {
             
             if(fml.charAt(i) == '('){
                 Map<String, Integer> res = solve(i+1, fml);
-                // System.out.println("res===="+res+" map===="+map);
-                // System.out.println(res);
-//                 int a = bkt[i]+1;
-//                 int mlt = 1;
-//                 newI = a;
                 
-//                 if(a<=j && help(fml.charAt(a)) == 0){
-//                     for(; a<=j && help(fml.charAt(a))==0; ++a);
-//                     mlt = Integer.parseInt(fml.substring(bkt[i]+1, a));
-//                     newI = a;
-//                 }
-                
-                // System.out.println(i+" "+bkt[i]+" "+mlt);
                 for(String key: res.keySet())
                     map.put(key, map.getOrDefault(key, 0) + res.get(key));
                 int a = i+1;
@@ -69,19 +35,16 @@ class Solution {
                     if(fml.charAt(a)=='(') ++open;
                     if(fml.charAt(a)==')') --open;
                 }
-                // System.out.println(a);
-                // ++a;
+                
                 for(; a<n && help(fml.charAt(a))==0; ++a);
                 newI = a;
-                // System.out.println(i+" "+a+" "+map);
             }
-            else if(fml.charAt(i) == ')'){
+            else if(fml.charAt(i) == ')'){ // if it is ) then add up return current map
                 int a = i+1;
                 for(; a<n && help(fml.charAt(a))==0; ++a);
                 int mlt = a==i+1 ? 1 : Integer.parseInt(fml.substring(i+1, a));
                 for(String key: map.keySet())
                     map.put(key, map.get(key)*mlt);
-                // System.out.println(i+" "+map+" "+mlt);
                 return map;
             }
             else{
@@ -90,8 +53,6 @@ class Solution {
                     int next = help(fml.charAt(i+1));
                     if(next != -1){ // next is not lower
                         cur = fml.substring(i, i+1);
-                        // System.out.println("cur--"+cur);
-                        // cur = Character.toString(fml.charAt(i));
                         if(next == 0){
                             int a = i+1;
                             for(; a<n && help(fml.charAt(a))==0; ++a);
@@ -113,10 +74,8 @@ class Solution {
                 }
                 map.put(cur, map.getOrDefault(cur, 0) + num);
             } 
-            // System.out.println("map----"+map);
             i = newI;
         }
-        // System.out.println(map);
         return map;
     }
     
