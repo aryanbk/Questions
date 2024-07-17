eb6d19eb1dc39aa0f36e97e3b4bea81ea50789a1
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-07-18 01:44:34 +0530

    Time: 1 ms (97.21%), Space: 44.9 MB (26.87%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
@@ -13,29 +13,58 @@
  *     }
  * }
  */
+
 class Solution {
     HashSet<Integer> s;
+    List<TreeNode> list;
     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
         s = new HashSet<>();
+        list = new ArrayList<>();
         for(int d: to_delete) s.add(d);
-        return dfs(root, null, false);
+        dfs(root, null, false);
+        return list;
     }
-    List<TreeNode> dfs(TreeNode root, TreeNode par, boolean left){
-        if(root==null) return new ArrayList<>();
-        var list = new ArrayList<TreeNode>();
+    void dfs(TreeNode root, TreeNode par, boolean left){
+        if(root==null) return;
+        TreeNode cur = s.contains(root.val) ? null : root;
         if(s.contains(root.val)){
             if(par!=null){
                 if(left) par.left = null;
                 else par.right = null;
             }
-            if(root.left!=null) list.addAll(dfs(root.left, null, true));
-            if(root.right!=null) list.addAll(dfs(root.right, null, false));
-            
-        }else{
-            if(par==null) list.add(root);
-            if(root.left!=null) list.addAll(dfs(root.left, root, true));
-            if(root.right!=null) list.addAll(dfs(root.right, root, false));
         }
-        return list;
+        else{
+            if(par==null) list.add(root);
+        }
+        dfs(root.left, cur, true);
+        dfs(root.right, cur, false);
     }
 }
+
+
+// class Solution {
+//     HashSet<Integer> s;
+//     List<TreeNode> list;
+//     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+//         s = new HashSet<>();
+//         list = new ArrayList<>();
+//         for(int d: to_delete) s.add(d);
+//         dfs(root, null, false);
+//         return list;
+//     }
+//     void dfs(TreeNode root, TreeNode par, boolean left){
+//         if(root==null) return;
+//         if(s.contains(root.val)){
+//             if(par!=null){
+//                 if(left) par.left = null;
+//                 else par.right = null;
+//             }
+//             dfs(root.left, null, true);
+//             dfs(root.right, null, false);
+//         }else{
+//             if(par==null) list.add(root);
+//             dfs(root.left, root, true);
+//             dfs(root.right, root, false);
+//         }
+//     }
+// }
