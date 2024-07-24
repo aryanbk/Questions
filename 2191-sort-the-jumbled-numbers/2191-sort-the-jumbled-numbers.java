65ee1d9874c0a872ba9c2a6bef4ad97362fc4d7e
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-07-25 00:02:20 +0530

    Time: 486 ms (11.76%), Space: 55.5 MB (59.80%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
@@ -1,17 +1,16 @@
 class Solution {
     public int[] sortJumbled(int[] mapping, int[] nums) {
-        Integer[] nums2 = new Integer[nums.length];
-        for(int i=0; i<nums.length; ++i)
-            nums2[i] = nums[i];
-        var map = new HashMap<Integer, Integer>();
-        for(int n: nums) map.putIfAbsent(n, val(n, mapping));
-        Arrays.sort(nums2, (a, b) -> map.get(a)-map.get(b));
-        for(int i=0; i<nums.length; ++i) nums[i] = nums2[i];
-        return nums;
-        
+        var idx = new Integer[nums.length];
+        Arrays.setAll(idx, (i)->i);
+        // sort indexed based on value after mapping
+        Arrays.sort(idx, (a, b) -> find(nums[a], mapping) - find(nums[b], mapping));
+        var ans = new int[nums.length];
+        for(int i=0; i<nums.length; ++i) ans[i] = nums[idx[i]];
+        return ans;
     }
     
-    int val(int n, int[] mapping){
+    // to find value of number after applying mapping
+    int find(int n, int[] mapping){
         String num = Integer.toString(n);
         int ans = 0;
         for(int i=0; i<num.length(); ++i){
@@ -21,3 +20,27 @@ class Solution {
         return ans;
     }
 }
+
+// class Solution {
+//     public int[] sortJumbled(int[] mapping, int[] nums) {
+//         Integer[] nums2 = new Integer[nums.length];
+//         for(int i=0; i<nums.length; ++i)
+//             nums2[i] = nums[i];
+//         var map = new HashMap<Integer, Integer>();
+//         for(int n: nums) map.putIfAbsent(n, val(n, mapping));
+//         Arrays.sort(nums2, (a, b) -> map.get(a)-map.get(b));
+//         for(int i=0; i<nums.length; ++i) nums[i] = nums2[i];
+//         return nums;
+        
+//     }
+    
+//     int val(int n, int[] mapping){
+//         String num = Integer.toString(n);
+//         int ans = 0;
+//         for(int i=0; i<num.length(); ++i){
+//             int cur = num.charAt(i)-'0';
+//             ans = (ans*10)+mapping[cur];
+//         }
+//         return ans;
+//     }
+// }
