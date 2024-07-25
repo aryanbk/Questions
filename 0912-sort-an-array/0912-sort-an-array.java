3bf43d54ab25f22025a00fca579117075dcad227
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-07-25 12:27:10 +0530

    Time: 25 ms (71.55%), Space: 57 MB (20.11%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,42 +1,36 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        return sort(0, nums.length-1, nums);
+        sort(0, nums.length-1, nums);
+        return nums;
     }
     
-    int[] sort(int i, int j, int[] nums){
-        if(i==j) return new int[]{nums[i]};
+    void sort(int i, int j, int[] nums){
+        if(i>=j) return;
         int mid = i + (j-i)/2;
-        int[] left = sort(i, mid, nums);
-        int[] right = sort(mid+1, j, nums);
-        
-        return merge(left, right, nums);
+        sort(i, mid, nums);
+        sort(mid+1, j, nums);
+        merge(i, j, nums);
     }
     
-    int[] merge(int[] left, int[] right, int[] nums){
-        int l = left.length;
-        int r = right.length;
-        int[] ans = new int[l+r];
-        int i = 0;
-        int j = 0;
+    void merge(int i, int j, int[] nums){
+        int mid = i + (j-i)/2;
+        int k = mid+1;
         int ptr = 0;
+        int[] tmp = new int[j-i+1];
         
-        
-        while(i<l && j<r){
-            if(left[i] > right[j])
-                ans[ptr] = right[j++];
-            else
-                ans[ptr] = left[i++];
+        while(i<=mid && k<=j){
+            if(nums[i] < nums[k]) tmp[ptr] = nums[i++];
+            else tmp[ptr] = nums[k++];
             ++ptr;
         }
-        while(i<l){
-            ans[ptr] = left[i++];
+        while(i<=mid){
+            tmp[ptr] = nums[i++];
             ++ptr;
         }
-        while(j<r){
-            ans[ptr] = right[j++];
+        while(k<=j){
+            tmp[ptr] = nums[k++];
             ++ptr;
         }
-        
-        return ans;
+        for(ptr=ptr-1; ptr>=0; --ptr, --j) nums[j] = tmp[ptr];
     }
 }
