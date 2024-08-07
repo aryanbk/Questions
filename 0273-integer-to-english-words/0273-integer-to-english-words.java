08a7be35b8e62fad9c9875d56ebafa1cc7012142
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-08-07 23:46:39 +0530

    Time: 2 ms (82.11%), Space: 41.7 MB (83.27%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,27 +1,28 @@
 class Solution {
-    String[] map = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty"};
-    
-    String[] map2 = {"", "Thousand", "Million", "Billion", "Trillion", "Quadrillion"};
-    
-    String[] map3 = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    String[] lessThanTwenty = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty"};
+    String[] Tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    String[] Thousands = {"", "Thousand", "Million", "Billion", "Trillion", "Quadrillion"};
     
     List<String> ans;
     
     public String numberToWords(int num) {
         if(num==0) return "Zero";
+        
         int i = 0;
         ans = new ArrayList<>();
+        
         while(num>0){
             int lastThree = num - ((num/1000)*1000);
-            if(lastThree!=0) ans.add(map2[i]);
-            getLastTwo(lastThree);
+            if(lastThree!=0) ans.add(Thousands[i]);
+            getLastTwoDigit(lastThree);
             if(lastThree/100 > 0){
                 ans.add("Hundred");
-                getOne(lastThree/100);
+                getOneDigit(lastThree/100);
             }
             num /= 1000;
             ++i;
         }
+        
         StringBuilder ansBuilder = new StringBuilder();
         for(int x=ans.size()-1; x>=0; --x){
             ansBuilder.append(ans.get(x));
@@ -33,19 +34,19 @@ class Solution {
         
     }
     
-    void getLastTwo(int num){
+    void getLastTwoDigit(int num){
         num = num - ((num/100)*100);
         if(num<=20){
-            ans.add(map[num]);
+            ans.add(lessThanTwenty[num]);
             return;
         }
-        getOne(num%10);
-        ans.add(map3[num/10]);
+        getOneDigit(num%10);
+        ans.add(Tens[num/10]);
         
     }
     
-    void getOne(int num){
+    void getOneDigit(int num){
         num = num - ((num/10)*10);
-        ans.add(map[num]);
+        ans.add(lessThanTwenty[num]);
     }
 }
