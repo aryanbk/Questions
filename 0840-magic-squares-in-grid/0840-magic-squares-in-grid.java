665ca7acdf19839bc360e50622277cfb651fe972
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-08-09 18:45:40 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (60.00%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
@@ -18,20 +18,21 @@ class Solution {
     }
     
     boolean valid(int i, int j, int[][] grid){
-        int[] row = new int[3];
+        if(grid[i+1][j+1] != 5) return false;
         int[] col = new int[3];
         boolean[] uniq = new boolean[9];
         
         for(int x=i; x<i+3; ++x){
+            int rowSum = 0;
             for(int y=j; y<j+3; ++y){
                 int val = grid[x][y];
-                row[x-i] += val;
+                rowSum += val;
                 col[y-j] += val;
                 if(val==0 || val>9 || uniq[val-1]) return false;
                 uniq[val-1] = true;
             }
+            if(rowSum!=15) return false;
         }
-        if(!(row[0]==row[1] && row[0]==row[2])) return false;
         if(!(col[0]==col[1] && col[0]==col[2])) return false;
         return grid[i][j]+grid[i+2][j+2] == grid[i][j+2]+grid[i+2][j];
     }
