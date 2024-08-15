3969ea53d0637d0017cfff168c3c307d59b7759a
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-08-15 15:10:16 +0530

    Time: 2 ms (95.27%), Space: 55.7 MB (62.98%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,28 +1,53 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int[] myBills = new int[3];
+        int five = 0;
+        int ten = 0;
         for(int i=0; i<bills.length; ++i){
-            if(bills[i] == 5) ++myBills[0];
-            
+            if(bills[i] == 5) ++five;
             else if(bills[i] == 10){
-                if(myBills[0] == 0) return false;
-                ++myBills[1];
-                --myBills[0];
+                if(five==0) return false;
+                --five;
+                ++ten;
             }
-            
             else{
-                ++myBills[2];
-                if(myBills[0]>0 && myBills[1]>0){
-                    --myBills[0];
-                    --myBills[1];
+                if(five>0 && ten>0){
+                    --five;
+                    --ten;
                 }
-                else if(myBills[0]>=3){
-                    myBills[0] -= 3;
-                }
+                else if(five>=3) five-=3;
                 else return false;
             }
         }
-        
         return true;
     }
 }
+
+
+// class Solution {
+//     public boolean lemonadeChange(int[] bills) {
+//         int[] myBills = new int[3]; // five, ten, twenty
+//         for(int i=0; i<bills.length; ++i){
+//             if(bills[i] == 5) ++myBills[0];
+            
+//             else if(bills[i] == 10){
+//                 if(myBills[0] == 0) return false;
+//                 ++myBills[1];
+//                 --myBills[0];
+//             }
+            
+//             else{
+//                 ++myBills[2];
+//                 if(myBills[0]>0 && myBills[1]>0){
+//                     --myBills[0];
+//                     --myBills[1];
+//                 }
+//                 else if(myBills[0]>=3){
+//                     myBills[0] -= 3;
+//                 }
+//                 else return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+// }
