86f0b2edaf6fd60140b53ecfe809c680d41776a4
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-08-23 10:32:59 +0530

    Time: 7 ms (54.59%), Space: 41.7 MB (70.18%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.java
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.java
@@ -1,22 +1,17 @@
 class Solution {
     public String fractionAddition(String exp) {
-        // List<String> exp = new ArrayList<>();
         int[] cur = new int[]{0, 1};
         String[] one = exp.split("\\+");
         for(String o: one){
             String[] two = o.split("-");
-            // int[] cur = new int[]{0, 1};
             if(two[0]!="") two[0] = "-"+two[0];
             for(String t : two){
-                // System.out.println(t);
                 if(t=="") continue;
                 String[] curNum = t.split("/");
                 int nume = Integer.parseInt(curNum[0]);
                 int deno = Integer.parseInt(curNum[1]);
-                System.out.println("numedeno "+nume+" "+deno);
                 cur = add(cur, new int[]{nume*-1, deno});
             }   
-            System.out.println(cur[0]+" "+cur[1]);
         }
         if(cur[0]==0) return "0/1";
         cur = reduce(cur);
@@ -24,24 +19,14 @@ class Solution {
     }
     
     int[] add(int[] i, int[] j){
-        System.out.println("start "+Arrays.toString(i)+" "+Arrays.toString(j));
-        // if(i[1]==j[1]) return new int[]{i[0]+j[0], i[0]};
         if(i[1] != j[1]){
             int[] temp = new int[]{j[0], j[1]};
-            // int[] temp = Arrays.copyOf(j);
             j[0] *= i[1];
             j[1] = Math.abs(j[1] * i[1]);
-            // System.out.println("spe "+i[0]+" "+temp[0]+" "+i[0]*temp[0]);
             i[0] *= temp[1];
             i[1] = j[1];
         }
-        // i[0] *= j[1];
-        // i[1] = Math.abs(i[1]*j[1]);
-        // j[0] *= i[1];
-        // j[1] = i[1];
-        System.out.println("mid "+Arrays.toString(i)+" "+Arrays.toString(j));
         int[] ans =  new int[]{i[0]+j[0], i[1]};
-        System.out.println("end "+Arrays.toString(ans));
         return ans;
     }
     
