93e53cf9e64e3aec4b22cd4f6b69ecfa30b326cd
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-09-04 20:16:32 +0530

    Time: 22 ms (65.94%), Space: 53.9 MB (19.21%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.java
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.java
@@ -1,22 +1,16 @@
 class Solution {
     int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    int x = 0;
-    int[] cur = {0, 0};
-    // Set<List<Integer>> obs;
+    int x = 0; // current direction
+    int[] cur = {0, 0}; // current position
+    
     public int robotSim(int[] cmd, int[][] obs) {
         Map<Integer, List<Integer>> obsx = new HashMap<>();
         Map<Integer, List<Integer>> obsy = new HashMap<>();
         for(int[] o: obs){
-            System.out.println(o[0]+" - "+o[1]);
-            var listy = obsy.getOrDefault(o[0], new ArrayList<Integer>());
-            listy.add(o[1]);
-            obsy.put(o[0], listy);
-            var listx = obsx.getOrDefault(o[1], new ArrayList<Integer>());
-            listx.add(o[0]);
-            obsx.put(o[1], listx);
+            obsy.computeIfAbsent(o[0], x -> new ArrayList<Integer>()).add(o[1]);
+            obsx.computeIfAbsent(o[1], x -> new ArrayList<Integer>()).add(o[0]);
         }
-        System.out.println(obsx+" "+obsy);
-        // obs = new HashSet<>();
+        
         int ans = 0;
         
         for(int i=0; i<cmd.length; ++i){
@@ -29,25 +23,25 @@ class Solution {
                 continue;
             }
             int[] newCur = {cur[0] + (dir[x%4][0]*cmd[i]), cur[1] + dir[x%4][1]*cmd[i]};
+            // new position = old + movement
+            
             if(cur[0] == newCur[0] && obsy.containsKey(cur[0])){
                 for(int k: obsy.get(cur[0])){
                     if((k-newCur[1]) * (k-cur[1]) <= 0 && k!=cur[1]){
-                        newCur[1] = k + (cur[1] > newCur[1] ? 1 : -1);
-                        // break;
+                        newCur[1] = k + (cur[1] > newCur[1] ? 1 : -1); // if obs, then stop it before
                     }
                 }
             }
             else if(cur[1] == newCur[1] && obsx.containsKey(cur[1])){
                 for(int k: obsx.get(cur[1])){
                     if((k-newCur[0]) * (k-cur[0]) <= 0 && k!=cur[0]){
-                        newCur[0] = k + (cur[0] > newCur[0] ? 1 : -1);
-                        // break;
+                        newCur[0] = k + (cur[0] > newCur[0] ? 1 : -1); // if obs then stop it before
                     }
                 }
             }
+            
             cur = newCur;
-            System.out.println(cur[0]+" "+cur[1]);
-            ans = Math.max(ans, (cur[0]*cur[0]) + (cur[1]*cur[1]));
+            ans = Math.max(ans, (cur[0]*cur[0]) + (cur[1]*cur[1])); // check max sq. ans every step
         }
         
         return ans;
