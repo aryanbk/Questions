2fd19c2fb93afc934c91446fa09450e319d049fc
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-09-08 15:21:17 +0530

    Time: 96 ms (61.13%), Space: 61.2 MB (58.34%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
@@ -1,34 +1,38 @@
 class Solution {
     
     int[] bit;
+    int n;
+    
     public List<Integer> countSmaller(int[] nums) {
-        bit = new int[nums.length+1];
-        int[][] nums2 = new int[nums.length][2];
-        for(int i=0; i<nums.length; ++i) nums2[i] = new int[]{nums[i], i};
-        Arrays.sort(nums2, (a, b) -> a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]);
+        n = nums.length;
+        bit = new int[n+1];
+        int[][] numsPair = new int[n][2]; // {num, idx} pair
         
-        Integer[] ans = new Integer[nums.length];
-        for(int i=0; i<nums.length; ++i){
-            update(nums2[i][1]+1, 1);
-            ans[nums2[i][1]] = query(nums.length) - query(nums2[i][1]+1);
+        for(int i=0; i<n; ++i) numsPair[i] = new int[]{nums[i], i};
+        Arrays.sort(numsPair, (a, b) -> a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]);
+        
+        Integer[] ans = new Integer[n];
+        for(int i=0; i<n; ++i){
+            update(numsPair[i][1]+1, 1);
+            ans[numsPair[i][1]] = sumQuery(n) - sumQuery(numsPair[i][1]+1);
         }
         
         return Arrays.asList(ans);
     }
     
+    
+    // Fenwick Tree
+    // index starts at 1
+    
     void update(int i, int val){
-        while(i<=bit.length-1){
+        for(; i<=n; i += (i & -i))
             bit[i] += val;
-            i += (i & -i);
-        }
     }
     
-    int query(int i){
+    int sumQuery(int i){
         int ans = 0;
-        while(i>0){
+        for(; i>0; i -= (i & -i))
             ans += bit[i];
-            i -= (i & -i);
-        }
         return ans;
     }
     
