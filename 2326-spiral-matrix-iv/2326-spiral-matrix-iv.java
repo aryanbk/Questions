71941375a3c1bd8f75fdc6c8bf963d9411741e83
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-09-10 03:52:20 +0530

    Time: 7 ms (87.52%), Space: 56.2 MB (98.95%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.java
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.java
@@ -12,47 +12,35 @@ class Solution {
     int[][] ans;
     int m;
     int n;
+    ListNode head;
+    
     public int[][] spiralMatrix(int m, int n, ListNode head) {
-        ans = new int[m][n];
         this.m = m;
         this.n = n;
-        fill(0, 0, head);
+        this.head = head;
+        
+        ans = new int[m][n];
+        for(var a: ans) Arrays.fill(a, -1);
+        
+        fill(0);
         return ans;
     }
-    void fill(int i, int j, ListNode head){
-        System.out.println("fill "+i+" "+j);
+    
+    void fill(int i){
         if(i>m-i-1 || i>n-i-1) return;
         
-        for(int x=j; x<n-j; ++x){
-            // if(head == null){
-            //     ans[i][x] = -1;
-            //     continue;
-            // }
-            // ans[i][x] = head.val;
-            // head = head.next;
-            head = add(i, x, head);
-        }
-        for(int x=i+1; x<m-i-1; ++x){
-            // if(head==null)
-            head = add(x, n-i-1, head);
-        }
-        for(int x=n-j-1; i!=m-j-1 && x>=j; --x){
-            head = add(m-i-1, x, head);
-        }
-        for(int x=m-i-2; i!=n-i-1 && x>i; --x)
-            head = add(x, i, head);
+        for(int x=i; x<n-i && head!=null; ++x, head = head.next)
+            ans[i][x] = head.val;
         
-        fill(i+1, i+1, head);
-    }
-    
-    ListNode add(int i, int j, ListNode head){
-        if(head==null){
-            System.out.println(i+" "+j+" null");
-            ans[i][j] = -1;
-            return head;
-        }
-        System.out.println(i+" "+j+" "+head.val);
-        ans[i][j] = head.val;
-        return head.next;
+        for(int x=i+1; x<m-i-1 && head!=null; ++x, head = head.next)
+            ans[x][n-i-1] = head.val;
+        
+        for(int x=n-i-1; i!=m-i-1 && x>=i && head!=null; --x, head = head.next)
+            ans[m-i-1][x] = head.val;
+        
+        for(int x=m-i-2; i!=n-i-1 && x>i && head!=null; --x, head=head.next)
+            ans[x][i] = head.val;
+        
+        fill(i+1);
     }
 }
