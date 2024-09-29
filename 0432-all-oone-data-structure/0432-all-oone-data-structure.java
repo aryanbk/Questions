467492088c9638c20709e5835860b1a0608618c5
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-09-29 18:30:07 +0530

    Time: 57 ms (33.73%), Space: 61 MB (35.94%) - LeetHub

---
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.java
@@ -0,0 +1,56 @@
+class AllOne {
+    
+    Map<String, Integer> map;
+    TreeMap<Integer, Set<String>> remap;
+    public AllOne() {
+        map = new HashMap<>();
+        remap = new TreeMap<>();
+    }
+    
+    public void inc(String key) {
+        int freq = map.getOrDefault(key, 0);
+        map.put(key, freq+1);
+        remap.putIfAbsent(freq+1, new HashSet<>());
+        remap.get(freq+1).add(key);
+        if(freq != 0){
+            remap.get(freq).remove(key);
+            if(remap.get(freq).size() == 0) remap.remove(freq);
+        }
+    }
+    
+    public void dec(String key) {
+        int freq = map.getOrDefault(key, 0);
+        if(freq != 1){
+            map.put(key, freq-1);
+            remap.putIfAbsent(freq-1, new HashSet<>());
+            remap.get(freq-1).add(key);
+        }
+        else map.remove(key);
+        
+        remap.get(freq).remove(key);
+        if(remap.get(freq).size() == 0) remap.remove(freq);
+    }
+    
+    public String getMaxKey() {
+        if(remap.size() == 0) return "";
+        int mxKey = remap.lastKey();
+        Iterator<String> iterator = remap.get(mxKey).iterator();
+        return iterator.next();
+    }
+    
+    public String getMinKey() {
+        if(remap.size() == 0) return "";
+        int mnKey = remap.firstKey();
+        Iterator<String> iterator = remap.get(mnKey).iterator();
+        return iterator.next();
+    }
+}
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne obj = new AllOne();
+ * obj.inc(key);
+ * obj.dec(key);
+ * String param_3 = obj.getMaxKey();
+ * String param_4 = obj.getMinKey();
+ */
