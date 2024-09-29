570e1ed18f4a09a109617218e1c70e389a186124
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-09-29 18:32:42 +0530

    Time: 62 ms (16.82%), Space: 60.8 MB (41.36%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.java
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.java
@@ -33,18 +33,66 @@ class AllOne {
     
     public String getMaxKey() {
         if(remap.size() == 0) return "";
-        int mxKey = remap.lastKey();
-        Iterator<String> iterator = remap.get(mxKey).iterator();
-        return iterator.next();
+        return remap.get(remap.lastKey()).iterator().next();
     }
     
     public String getMinKey() {
         if(remap.size() == 0) return "";
-        int mnKey = remap.firstKey();
-        Iterator<String> iterator = remap.get(mnKey).iterator();
-        return iterator.next();
+        return remap.get(remap.firstKey()).iterator().next();
     }
 }
+
+
+
+// // readable code============
+
+// class AllOne {
+    
+//     Map<String, Integer> map;
+//     TreeMap<Integer, Set<String>> remap;
+//     public AllOne() {
+//         map = new HashMap<>();
+//         remap = new TreeMap<>();
+//     }
+    
+//     public void inc(String key) {
+//         int freq = map.getOrDefault(key, 0);
+//         map.put(key, freq+1);
+//         remap.putIfAbsent(freq+1, new HashSet<>());
+//         remap.get(freq+1).add(key);
+//         if(freq != 0){
+//             remap.get(freq).remove(key);
+//             if(remap.get(freq).size() == 0) remap.remove(freq);
+//         }
+//     }
+    
+//     public void dec(String key) {
+//         int freq = map.getOrDefault(key, 0);
+//         if(freq != 1){
+//             map.put(key, freq-1);
+//             remap.putIfAbsent(freq-1, new HashSet<>());
+//             remap.get(freq-1).add(key);
+//         }
+//         else map.remove(key);
+        
+//         remap.get(freq).remove(key);
+//         if(remap.get(freq).size() == 0) remap.remove(freq);
+//     }
+    
+//     public String getMaxKey() {
+//         if(remap.size() == 0) return "";
+//         int mxKey = remap.lastKey();
+//         Iterator<String> iterator = remap.get(mxKey).iterator();
+//         return iterator.next();
+//     }
+    
+//     public String getMinKey() {
+//         if(remap.size() == 0) return "";
+//         int mnKey = remap.firstKey();
+//         Iterator<String> iterator = remap.get(mnKey).iterator();
+//         return iterator.next();
+//     }
+// }
 
 /**
  * Your AllOne object will be instantiated and called as such:
