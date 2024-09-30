a4e63459399a0d81e6bd8cf8a081fb0231de08a0
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-09-30 23:46:14 +0530

    Time: 5 ms (93.47%), Space: 45.3 MB (72.25%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
@@ -1,25 +1,55 @@
 class CustomStack {
-    List<Integer> list;
-    int mxSize;
+    Stack<Integer> stk;
+    int n;
+    int[] inc;
     
     public CustomStack(int maxSize) {
-        list = new ArrayList<>();
-        mxSize = maxSize;
+        stk = new Stack<>();
+        n = maxSize;
+        inc = new int[n+1];
     }
     
     public void push(int x) {
-        if(list.size() < mxSize) list.add(x);
+        if(stk.size() < n) stk.push(x);
     }
     
     public int pop() {
-        return list.size()==0 ? -1 : list.remove(list.size()-1);
+        if(stk.size() == 0) return -1;
+        int curInc = inc[stk.size()];
+        inc[stk.size()] = 0;
+        inc[stk.size()-1] += curInc;
+        return stk.pop() + curInc;
     }
     
     public void increment(int k, int val) {
-        for(int i=0; i<Math.min(k, list.size()); ++i)
-            list.set(i, list.get(i)+val);
+        inc[Math.min(k, stk.size())] += val;
     }
 }
+
+
+
+// class CustomStack {
+//     List<Integer> list;
+//     int mxSize;
+    
+//     public CustomStack(int maxSize) {
+//         list = new ArrayList<>();
+//         mxSize = maxSize;
+//     }
+    
+//     public void push(int x) {
+//         if(list.size() < mxSize) list.add(x);
+//     }
+    
+//     public int pop() {
+//         return list.size()==0 ? -1 : list.remove(list.size()-1);
+//     }
+    
+//     public void increment(int k, int val) {
+//         for(int i=0; i<Math.min(k, list.size()); ++i)
+//             list.set(i, list.get(i)+val);
+//     }
+// }
 
 /**
  * Your CustomStack object will be instantiated and called as such:
