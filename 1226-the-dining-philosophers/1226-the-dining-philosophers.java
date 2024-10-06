d528db3d3dddc60c483401ec43c47a05aba4ea10
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-10-06 21:01:41 +0530

    Time: 14 ms (49.89%), Space: 45.4 MB (67.74%) - LeetHub

--- 1226-the-dining-philosophers/1226-the-dining-philosophers.java
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.java
@@ -1,58 +1,3 @@
-// import java.util.concurrent.Semaphore;
-
-// class DiningPhilosophers {
-//     private final Semaphore[] forks;
-//     private final Semaphore maxDiners;
-
-//     public DiningPhilosophers() {
-//         forks = new Semaphore[5];
-//         for (int i = 0; i < 5; i++) {
-//             forks[i] = new Semaphore(1);
-//         }
-//         // Allow at most 4 philosophers to try to eat simultaneously
-//         maxDiners = new Semaphore(4);
-//     }
-
-//     public void wantsToEat(int philosopher,
-//                            Runnable pickLeftFork,
-//                            Runnable pickRightFork,
-//                            Runnable eat,
-//                            Runnable putLeftFork,
-//                            Runnable putRightFork) throws InterruptedException {
-        
-//         int leftFork = philosopher;
-//         int rightFork = (philosopher + 1) % 5;
-
-//         maxDiners.acquire(); // Limit the number of philosophers trying to eat
-
-//         try {
-//             // Always acquire the lower-numbered fork first
-//             if (leftFork < rightFork) {
-//                 forks[leftFork].acquire();
-//                 forks[rightFork].acquire();
-//             } else {
-//                 forks[rightFork].acquire();
-//                 forks[leftFork].acquire();
-//             }
-
-//             pickLeftFork.run();
-//             pickRightFork.run();
-//             eat.run();
-//             putLeftFork.run();
-//             putRightFork.run();
-//         } finally {
-//             // Always release the forks
-//             forks[leftFork].release();
-//             forks[rightFork].release();
-//             maxDiners.release(); // Allow another philosopher to try eating
-//         }
-//     }
-// }
-
-
-
-
-
 class DiningPhilosophers {
     
     Semaphore[] forks;
@@ -74,7 +19,6 @@ class DiningPhilosophers {
         
         eating.acquire();
         
-        try{
         forks[philosopher].acquire();
         forks[(philosopher+1)%5].acquire();
         
@@ -85,12 +29,11 @@ class DiningPhilosophers {
         
         putRightFork.run();
         putLeftFork.run();
-        }
-        finally{
+
         forks[philosopher].release();
         forks[(philosopher+1)%5].release();
         
         eating.release();
-        }
+        
     }
 }
