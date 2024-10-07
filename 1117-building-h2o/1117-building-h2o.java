ddf6beee9b52645ca087d35d7aa0ee9cf19ce0c9
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-10-07 18:19:20 +0530

    Time: 21 ms (20.69%), Space: 46.7 MB (5.05%) - LeetHub

---
+++ 1117-building-h2o/1117-building-h2o.java
@@ -0,0 +1,26 @@
+class H2O {
+    
+    Semaphore smpH;
+    Semaphore smpO;
+    
+    public H2O() {
+        smpH = new Semaphore(2);
+        smpO = new Semaphore(0);
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+		
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        smpH.acquire();
+        releaseHydrogen.run();
+        if(smpH.availablePermits()==0) smpO.release();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+        smpO.acquire();
+		releaseOxygen.run();
+        smpH.release(2);
+    }
+}
