6918d44d9c48dd36653c3486452d6ee8bfe8202b
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-10-12 15:49:58 +0530

    Time: 337 ms (5.16%), Space: 75.3 MB (94.57%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
@@ -1,32 +1,31 @@
 class Solution {
+    int n;
     public int minGroups(int[][] intv) {
-        int n = intv.length;
-        Arrays.sort(intv, (a, b) -> a[0]==b[0] ? b[1]-a[1] : a[0]-b[0]);
-        int st = intv[0][0];
-        int end = intv[0][1];
+        n = intv.length;
         int ans = 0;
+        
+        Arrays.sort(intv, (a, b) -> a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]);
         boolean[] added = new boolean[n];
-        // for(int[] x: intv) System.out.println(Arrays.toString(x));
-        for(int i=0; i<n; ++i){
+        
+        for(int i=0; i<n; ++i)
             ans += solve(i, intv, added) ? 1 : 0;
-            // System.out.println(Arrays.toString(added));
-        }
+        
         return ans;
     }
     
     boolean solve(int i, int[][] intv, boolean[] added){
-        if(i==intv.length || added[i]) return false;
+        if(i==n || added[i]) return false;
         added[i] = true;
         
         int nx = bs(intv, i+1, intv[i][1]);
-        while(nx<intv.length && added[nx]) ++nx;
-        if(nx != intv.length) solve(nx, intv, added);
+        while(nx<n && added[nx]) ++nx;
         
+        if(nx != n) solve(nx, intv, added);
         return true;
     }
     
     int bs(int[][] nums, int i, int trgt){
-        int j = nums.length-1;
+        int j = n-1;
         while(i<=j){
             int mid = i + (j-i)/2;
             if(nums[mid][0] > trgt) j = mid-1;
