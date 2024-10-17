cc18a27fc3dcaba2d75b39e5b2b0938c4debb78e
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-10-17 19:14:14 +0530

    Time: 1 ms (30.39%), Space: 40.3 MB (66.95%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -1,23 +1,25 @@
 class Solution {
     public int maximumSwap(int num) {
-        char[] c = Integer.toString(num).toCharArray();
-        // String[] ans = new String[c.length];
-        char mx = c[c.length-1];
-        int mxIdx = c.length-1;
+        char[] arr = Integer.toString(num).toCharArray();
+        int n = arr.length;
+        
+        char mx = arr[n-1];
+        int mxIdx = n-1;
         int ans = num;
         
-        for(int i=c.length-1; i>=0; --i){
-            char cur = c[i];
+        for(int i=n-1; i>=0; --i){
+            char cur = arr[i];
             if(cur > mx){
                 mx = cur;
                 mxIdx = i;
             }
             else{
-                c[i] = mx;
-                c[mxIdx] = cur;
-                ans = Math.max(ans, Integer.parseInt(String.valueOf(c)));
-                c[mxIdx] = mx;
-                c[i] = cur;
+                // swap, find new value, re-swap
+                arr[i] = mx;
+                arr[mxIdx] = cur;
+                ans = Math.max(ans, Integer.parseInt(String.valueOf(arr)));
+                arr[mxIdx] = mx;
+                arr[i] = cur;
             }
         }
         
