3285a6f5e188cddc7e03013b8d5eaa8eae09475d
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-10-19 02:39:12 +0530

    Time: 9 ms (67.33%), Space: 44.3 MB (35.23%) - LeetHub

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
@@ -0,0 +1,21 @@
+class Solution {
+    int or;
+    Map<List<Integer>, Integer> dp;
+    public int countMaxOrSubsets(int[] nums) {
+        or = 0;
+        dp = new HashMap<>();
+        for(int num: nums) or |= num;
+        return solve(0, 0, nums);
+    }
+    
+    int solve(int i, int cur, int[] nums){
+        if(i==nums.length) return cur==or ? 1 : 0;
+        var list = new ArrayList<Integer>();
+        list.add(i);
+        list.add(cur);
+        if(dp.containsKey(list)) return dp.get(list);
+        int ans = solve(i+1, cur, nums) + solve(i+1, cur|nums[i], nums);
+        dp.put(list, ans);
+        return ans;
+    }
+}
