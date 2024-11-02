0cec0483391312b72e10cf981f2ee738536ea6c0
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-11-02 18:54:22 +0530

    Time: 1 ms (96.81%), Space: 42.2 MB (25.89%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.java
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -2,11 +2,26 @@ class Solution {
     public boolean isCircularSentence(String sentence) {
         String[] sen = sentence.split(" ");
         int n = sen.length;
-        for(int i=0; i<n; ++i){
-            String cur = sen[i];
-            String nx = sen[(i+1)%n];
-            if(cur.charAt(cur.length()-1) != nx.charAt(0)) return false;
-        }
+        for(int i=0; i<n; ++i)
+            if(sen[i].charAt(sen[i].length()-1) != sen[(i+1)%n].charAt(0)) return false;
         return true;
     }
 }
+
+
+
+
+// // readable
+//
+// class Solution {
+//     public boolean isCircularSentence(String sentence) {
+//         String[] sen = sentence.split(" ");
+//         int n = sen.length;
+//         for(int i=0; i<n; ++i){
+//             String cur = sen[i];
+//             String nx = sen[(i+1)%n];
+//             if(cur.charAt(cur.length()-1) != nx.charAt(0)) return false;
+//         }
+//         return true;
+//     }
+// }
