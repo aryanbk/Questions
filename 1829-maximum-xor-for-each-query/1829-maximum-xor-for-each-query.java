a02fc0b1a99d6fbb4ce8357d6b699fde57fd2c8a
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-11-09 03:08:09 +0530

    Time: 66 ms (5.13%), Space: 63.7 MB (6.19%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] getMaximumXor(int[] nums, int mxBit) {
         int n = nums.length;
-        int[] xor = new int[n];
+        int[] xor = new int[n]; // to store reverse xor of the nums elements
         xor[n-1] = nums[0];
         for(int i=n-2; i>=0; --i) xor[i] = xor[i+1] ^ nums[n-i-1];
         
@@ -10,8 +10,8 @@ class Solution {
             int cur = xor[i];
             int res = 0;
             for(int x=0; x<mxBit; ++x, cur /= 2){
-                if(cur%2 == 0) res = res | (1 << x);
-            }
+                if(cur%2 == 0) res = res | (1 << x); 
+            } // if cur has unset bit then set bit in res
             ans[i] = res;
         }
         
