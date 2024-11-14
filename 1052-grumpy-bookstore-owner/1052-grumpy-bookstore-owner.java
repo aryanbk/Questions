00c4bc30792b0818a93081b3f9d2c9119b06c192
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-11-14 17:17:06 +0530

    Time: 4 ms (43.55%), Space: 47.4 MB (63.02%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,31 +1,59 @@
 class Solution {
-    public int maxSatisfied(int[] cst, int[] gmp, int mnt) {
-        int n = cst.length;
-        int maxSatisfy = 0;
-        int maxUnsatisfy = 0;
+    public int maxSatisfied(int[] cust, int[] grumpy, int min) {
+        int n = cust.length;
+        int ans = 0;
+        int total = 0;
+        int totalGrumpy = 0;
+        int saved = 0; // max customers saved from grumpy owner
+        int cur = 0;
         
-        for(int i=0; i<n; ++i){
-            maxSatisfy += cst[i];
-            cst[i] = cst[i]*gmp[i];
-            maxUnsatisfy += cst[i];
-        }
-        
-        return maxSatisfy - maxUnsatisfy + maxSubarraySum(cst, mnt);
-    }
-    
-    int maxSubarraySum(int[] nums, int k){
-        int curSumt = 0;
-        int mx = 0;
-        
-        for(int l=0, r=0; r<nums.length; ++r){
-            curSumt += nums[r];
-            if(r-l+1 == k){
-                mx = Math.max(mx, curSumt);
-                curSumt -= nums[l];
-                ++l;
+        for(int i=0, j=0; j<n; ++j){
+            total += cust[j];
+            totalGrumpy += cust[j] * grumpy[j];
+            cur += cust[j] * grumpy[j];
+            while(j-i+1 > min){
+                cur -= cust[i] * grumpy[i];
+                ++i;
             }
+            saved = Math.max(saved, cur);
         }
         
-        return mx;
+        return total - totalGrumpy + saved;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public int maxSatisfied(int[] cst, int[] gmp, int mnt) {
+//         int n = cst.length;
+//         int maxSatisfy = 0;
+//         int maxUnsatisfy = 0;
+        
+//         for(int i=0; i<n; ++i){
+//             maxSatisfy += cst[i];
+//             cst[i] = cst[i]*gmp[i];
+//             maxUnsatisfy += cst[i];
+//         }
+        
+//         return maxSatisfy - maxUnsatisfy + maxSubarraySum(cst, mnt);
+//     }
+    
+//     int maxSubarraySum(int[] nums, int k){
+//         int curSumt = 0;
+//         int mx = 0;
+        
+//         for(int l=0, r=0; r<nums.length; ++r){
+//             curSumt += nums[r];
+//             if(r-l+1 == k){
+//                 mx = Math.max(mx, curSumt);
+//                 curSumt -= nums[l];
+//                 ++l;
+//             }
+//         }
+        
+//         return mx;
+//     }
+// }
