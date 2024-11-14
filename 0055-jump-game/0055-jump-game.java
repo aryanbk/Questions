546dc7334f88cbf8d5e228d58e9206ce5648320a
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-11-14 18:35:44 +0530

    Time: 3 ms (47.37%), Space: 45.5 MB (72.63%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,17 +1,28 @@
 class Solution {
-    Boolean[] dp;
     public boolean canJump(int[] nums) {
-        dp = new Boolean[nums.length];
-        return solve(0, nums);
-    }
-    
-    boolean solve(int i, int[] nums){
-        if(i>=nums.length-1) return i==nums.length-1;
-        if(dp[i] != null) return dp[i];
-        boolean ans = false;
-        for(int j=i+1; j<Math.min(nums.length, i+nums[i]+1); ++j)
-            ans |= solve(j, nums);
-        dp[i] = ans;
-        return ans;
+        int mx = 0;
+        for(int i=0; i<nums.length && mx >= i; ++i)
+            mx = Math.max(mx, i + nums[i]);
+        return mx >= nums.length-1;
     }
 }
+
+
+
+// class Solution {
+//     Boolean[] dp;
+//     public boolean canJump(int[] nums) {
+//         dp = new Boolean[nums.length];
+//         return solve(0, nums);
+//     }
+    
+//     boolean solve(int i, int[] nums){
+//         if(i>=nums.length-1) return i==nums.length-1;
+//         if(dp[i] != null) return dp[i];
+//         boolean ans = false;
+//         for(int j=i+1; j<Math.min(nums.length, i+nums[i]+1); ++j)
+//             ans |= solve(j, nums);
+//         dp[i] = ans;
+//         return ans;
+//     }
+// }
