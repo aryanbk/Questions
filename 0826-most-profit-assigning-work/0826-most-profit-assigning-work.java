c8dc983ea0adca1bc6f7d9394943daf90438653f
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-11-14 20:16:55 +0530

    Time: 25 ms (40.17%), Space: 45.3 MB (91.81%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,20 +1,31 @@
 class Solution {
-    public int maxProfitAssignment(int[] diff, int[] pft, int[] wrkr) {
+    public int maxProfitAssignment(int[] diff, int[] pr, int[] wrk) {
         int n = diff.length;
-        int[][] cmb = new int[n][2];
         
-        for(int i=0; i<n; ++i) cmb[i] = new int[]{pft[i], diff[i]};
+        int[][] pair = new int[n][2];
+        for(int i=0; i<n; ++i) pair[i] = new int[]{diff[i], pr[i]};
+        Arrays.sort(pair, (a, b) -> a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]);
         
-        Arrays.sort(cmb, (a, b)-> b[0]-a[0]);
-        Arrays.sort(wrkr);
+        int[] mxpr = new int[n+1];
+        for(int i=1; i<=n; ++i) mxpr[i] = Math.max(mxpr[i-1], pair[i-1][1]);
+        
         int ans = 0;
-        
-        for(int i=0, j=wrkr.length-1; i<n && j>=0; ++i){
-            while(j>=0 && wrkr[j] >= cmb[i][1]){
-                ans += cmb[i][0];
-                --j;
-            }
+        for(int i=0; i<wrk.length; ++i){
+            int idx = bs(pair, wrk[i]);
+            ans += mxpr[idx+1];
         }
+        
         return ans;
+    }
+    
+    int bs(int[][] pair, int x){
+        int lo = 0;
+        int hi = pair.length-1;
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2;
+            if(pair[mid][0] <= x) lo = mid + 1;
+            else hi = mid -1;
+        }
+        return hi;
     }
 }
