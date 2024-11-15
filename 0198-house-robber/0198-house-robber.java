a1adb38016c470f3dc0871c7670063ffd6298ea1
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-11-15 18:25:42 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (49.73%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,12 +1,29 @@
+// class Solution {
+//     public int rob(int[] nums) {
+//         int last=0,secLast=0;
+//         for(int i: nums){
+//             int temp=Math.max(i+secLast, last);
+//             secLast=last;
+//             last=temp;
+//         }
+//         return last;
+//     }
+// }
+
+
+
+
 class Solution {
+    Integer[] dp;
     public int rob(int[] nums) {
-        int last=0,secLast=0;
-        for(int i: nums){
-            int temp=Math.max(i+secLast, last);
-            secLast=last;
-            last=temp;
-        }
-        return last;
+        dp = new Integer[nums.length+1];
+        return solve(0, nums);
+    }
+    int solve(int i, int[] nums){
+        if(i>=nums.length) return 0;
+        if(dp[i] == null)
+            dp[i] = Math.max(solve(i+1, nums), nums[i] + solve(i+2, nums));
+        return dp[i];
     }
 }
 
