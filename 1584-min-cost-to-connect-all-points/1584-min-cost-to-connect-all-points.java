fc5de3d06414bc46f584062b74630113e7f8c234
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-11-16 19:57:27 +0530

    Time: 79 ms (76.13%), Space: 57.7 MB (70.06%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,69 +1,30 @@
-// added seen in code, when seen == n while loop ends - it improves little bit
-// removed x, y from pq, only index is sufficient
-
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int ans = 0, n = points.length;
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
-        pq.offer(new int[]{0, 0}); // cost, index
-        boolean[] vis = new boolean[n];
-        
+        int n = points.length;
         int seen = 0;
+        int cost = 0;
         
-        while(!pq.isEmpty() && seen<n){
-            
-            int cost = pq.peek()[0];
-            int idx = pq.peek()[1];
-            pq.poll();
+        boolean[] vis = new boolean[n];
+        var pq = new PriorityQueue<int[]>((a, b) -> a[0]-b[0]);
+        pq.offer(new int[]{0, 0});
+        
+        while(!pq.isEmpty() && seen < n){
+            int[] cur = pq.poll();
+            int idx = cur[1];
             
             if(vis[idx]) continue;
             vis[idx] = true;
-            seen+=1;
-            ans += cost;
+            cost += cur[0]; 
+            ++seen;
             
-            for(int p=0; p<n; ++p){
-                if(vis[p]) continue;
-                int newCost = Math.abs(points[p][0]-points[idx][0]) 
-                    + Math.abs(points[p][1]-points[idx][1]);
-                pq.offer(new int[]{newCost, p});
+            for(int i=0; i<n; ++i){
+                if(i != idx && !vis[i]){
+                    int dist = Math.abs(points[i][0] - points[idx][0]) + Math.abs(points[i][1] - points[idx][1]);
+                    pq.offer(new int[]{dist, i});
+                }
             }
         }
         
-        return ans;
+        return cost;
     }
 }
-
-
-
-// class Solution {
-//     public int minCostConnectPoints(int[][] points) {
-//         int ans = 0, n = points.length;
-        
-//         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
-//         pq.offer(new int[]{0, points[0][0], points[0][1], 0}); // cost, x, y, index
-//         boolean[] vis = new boolean[n];
-        
-//         while(!pq.isEmpty()){
-            
-//             int cost = pq.peek()[0];
-//             int i = pq.peek()[1];
-//             int j = pq.peek()[2];
-//             int idx = pq.peek()[3];
-//             pq.poll();
-            
-//             if(vis[idx]) continue;
-//             vis[idx] = true;
-            
-//             ans += cost;
-            
-//             for(int p=0; p<n; ++p){
-//                 if(vis[p]) continue;
-//                 int newCost = Math.abs(points[p][0]-i)+Math.abs(points[p][1]-j);
-//                 pq.offer(new int[]{newCost, points[p][0], points[p][1], p});
-//             }
-//         }
-        
-//         return ans;
-//     }
-// }
