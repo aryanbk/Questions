c69e47271d0a704edf72cb579c491dee60f36f79
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-11-18 19:05:59 +0530

    Time: 1120 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -2,7 +2,14 @@
 
 
 select query_name, round(avg(rating/position), 2) as quality,
-round(sum(if(rating<3, 1, 0)) *100 / count(rating), 2) as poor_query_percentage
+round(avg(rating<3)*100, 2) as poor_query_percentage
 from queries
 where query_name is not null
 group by query_name
+
+
+# select query_name, round(avg(rating/position), 2) as quality,
+# round(sum(if(rating<3, 1, 0)) *100 / count(rating), 2) as poor_query_percentage
+# from queries
+# where query_name is not null
+# group by query_name
