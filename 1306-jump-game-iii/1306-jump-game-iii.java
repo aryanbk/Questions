0152fb9c56f6c22c6f2f8929d00df8d3ae4e09a0
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-11-20 03:34:54 +0530

    Time: 1 ms (99.58%), Space: 58.5 MB (43.91%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,9 +1,31 @@
 class Solution {
-    boolean[] vis = new boolean[50001];
     public boolean canReach(int[] arr, int start) {
-        if(start < 0 || start >= arr.length || vis[start]) return false;
-        if(arr[start]==0) return true;
-        vis[start] = true;
-        return canReach(arr, start-arr[start]) || canReach(arr, start+arr[start]);
+        if(start < 0 || start >= arr.length || arr[start]<0) return false;
+        arr[start] *= -1;
+        return arr[start]==0 || canReach(arr, start-arr[start]) || canReach(arr, start+arr[start]);
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean canReach(int[] arr, int start) {
+//         if(start < 0 || start >= arr.length || arr[start]<0) return false;
+//         if(arr[start]==0) return true;
+//         arr[start] *= -1;
+//         return canReach(arr, start-arr[start]) || canReach(arr, start+arr[start]);
+//     }
+// }
+
+
+
+// class Solution {
+//     boolean[] vis = new boolean[50001];
+//     public boolean canReach(int[] arr, int start) {
+//         if(start < 0 || start >= arr.length || vis[start]) return false;
+//         if(arr[start]==0) return true;
+//         vis[start] = true;
+//         return canReach(arr, start-arr[start]) || canReach(arr, start+arr[start]);
+//     }
+// }
