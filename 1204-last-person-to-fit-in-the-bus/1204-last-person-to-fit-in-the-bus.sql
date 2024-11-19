81079358288115cf7001801d0359545dd3923235
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-11-19 20:41:07 +0530

    Time: 1741 ms (27.47%), Space: 0B (100.00%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,10 +1,32 @@
 # Write your MySQL query statement below
 
-select f.person_name
-from queue f
-join queue s
-on f.turn >= s.turn
-group by f.person_id
-having sum(s.weight)<=1000
-order by sum(s.weight) desc
-limit 1
+
+# without using limit
+
+select person_name from queue
+where turn = 
+(
+    select max(turn) from 
+    (
+        select f.turn, sum(s.weight) as weight
+        from queue f
+        join queue s
+        on f.turn >= s.turn
+        group by f.turn
+        having sum(s.weight) <= 1000
+    ) as mxturn
+);
+
+
+
+
+# # using limit
+
+# select f.person_name
+# from queue f
+# join queue s
+# on f.turn >= s.turn
+# group by f.person_id
+# having sum(s.weight)<=1000
+# order by sum(s.weight) desc
+# limit 1
