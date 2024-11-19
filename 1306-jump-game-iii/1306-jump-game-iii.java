9aef26153b2e1ba77cac11fe32e5ff0a80078875
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-11-20 03:31:27 +0530

    Time: 1 ms (99.58%), Space: 58.9 MB (29.85%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,9 +1,20 @@
 class Solution {
-    boolean[] vis = new boolean[50001];
     public boolean canReach(int[] arr, int start) {
-        if(start < 0 || start >= arr.length || vis[start]) return false;
+        if(start < 0 || start >= arr.length || arr[start]<0) return false;
         if(arr[start]==0) return true;
-        vis[start] = true;
+        arr[start] *= -1;
         return canReach(arr, start-arr[start]) || canReach(arr, start+arr[start]);
     }
 }
+
+
+
+// class Solution {
+//     boolean[] vis = new boolean[50001];
+//     public boolean canReach(int[] arr, int start) {
+//         if(start < 0 || start >= arr.length || vis[start]) return false;
+//         if(arr[start]==0) return true;
+//         vis[start] = true;
+//         return canReach(arr, start-arr[start]) || canReach(arr, start+arr[start]);
+//     }
+// }
