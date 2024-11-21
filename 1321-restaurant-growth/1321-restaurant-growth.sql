4cdd773f23b930905bc9ed9e2c5ad1aaa5d46f60
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-11-21 16:12:39 +0530

    Time: 346 ms (88.58%), Space: 0B (100.00%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+
+# from customer f
+# join customer s
+# on f.visited_on >= s.visited_on 
+# and f.visited_on <= DATE_ADD(s.visited_on, interval 7 day)
+# group by f.visited_on
+
+
+select f.visited_on, sum(s.amount) as amount, round(avg(s.amount), 2) as average_amount
+from 
+(select distinct(visited_on) as visited_on from customer group by visited_on) f
+join 
+(select visited_on, sum(amount) as amount from customer group by visited_on) s
+on f.visited_on >= s.visited_on 
+and DATEDIFF(f.visited_on, s.visited_on) < 7
+group by f.visited_on
+having count(s.visited_on) = 7
+order by f.visited_on
